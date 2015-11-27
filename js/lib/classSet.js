9bc07d2a623f8de14e5de366333b9decc393af11
Brian R. Bondy <netzen@gmail.com>
2015-11-27 13:06:23 -0500

    Add class-set

---
+++ js/lib/classSet.js
@@ -0,0 +1,9 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/**
+ * Converts an object with boolean properties to a CSS string.
+ * Only the property names with truthy values are included.
+ */
+module.exports = obj => Object.keys(obj).filter(prop => obj[prop]).join(' ')
