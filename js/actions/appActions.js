bd4a11d1ca241360efd4416077af821ef9188476
Brian R. Bondy <netzen@gmail.com>
2015-11-27 13:48:32 -0500

    Add UrlUtil and prepend http when applicable

--- js/actions/appActions.js
+++ js/actions/appActions.js
@@ -1,8 +1,12 @@
-var AppDispatcher = require('../dispatcher/appDispatcher')
-var AppConstants = require('../constants/appConstants')
+const AppDispatcher = require('../dispatcher/appDispatcher')
+const AppConstants = require('../constants/appConstants')
+const UrlUtil = require('./../../node_modules/urlutil.js/dist/node-urlutil.js')
 
 const AppActions = {
   loadUrl: function (loc) {
+    if (UrlUtil.isURL(loc)) {
+      loc = UrlUtil.getUrlFromInput(loc)
+    }
     AppDispatcher.dispatch({
       actionType: AppConstants.APP_SET_URL,
       location: loc
