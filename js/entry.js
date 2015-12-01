8ea3a73ff48649642906b6d3b8a35228b5dbc584
Kevin Grandon <kevingrandon@yahoo.com>
2015-11-30 19:42:59 -0800

    Fix lint issue.

--- js/entry.js
+++ js/entry.js
@@ -1,9 +1,10 @@
+
 // Stylesheets are included here for webpack live reloading
 require('../less/browser.less')
 require('../less/main.less')
 require('../less/navigationBar.less')
 require('../less/tabs.less')
-require('../node_modules/font-awesome/css/font-awesome.css');
+require('../node_modules/font-awesome/css/font-awesome.css')
 
 const React = require('react')
 const ReactDOM = require('react-dom')
--- webpack.config.js
+++ webpack.config.js
@@ -42,11 +42,11 @@ module.exports = {
       // Loads font files for Font Awesome
       {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: "url-loader?limit=10000&minetype=application/font-woff"
+        loader: 'url-loader?limit=10000&minetype=application/font-woff'
       },
       {
         test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: "file-loader"
+        loader: 'file-loader'
       }
     ]
   },
