220df78e776e1434cdda82421dd8df06cea2b70d
Brian R. Bondy <netzen@gmail.com>
2015-12-06 19:04:25 -0500

    Set MPL-2.0 license. Fixes #9
    NPM package name from Brave to brave is because npm package names alwasy
    need lowercase.
    
    Auditors: @aekeus

--- js/stores/appStore.js
+++ js/stores/appStore.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 const AppDispatcher = require('../dispatcher/appDispatcher')
 const EventEmitter = require('events').EventEmitter
 const AppConstants = require('../constants/appConstants')
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 var webpack = require('webpack')
 
 module.exports = {
@@ -65,4 +69,3 @@ module.exports = {
     publicPath: 'http://localhost:8080/built/'
   }
 }
-
