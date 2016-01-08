a07752e0d9dc91d9dd49c5ca1be7857a81f47bc0
Brian R. Bondy <netzen@gmail.com>
2016-01-08 11:59:02 -0500

    Use stripped www for first party host lookups

--- app/trackingProtection.js
+++ app/trackingProtection.js
@@ -34,10 +34,14 @@ const startTrackingProtection = (wnd) => {
 
   Filtering.register(wnd, (details) => {
     const firstPartyUrl = URL.parse(details.firstPartyUrl)
+    let firstPartyUrlHost = firstPartyUrl.host || ''
+    if (firstPartyUrlHost.startsWith('www.')) {
+      firstPartyUrlHost = firstPartyUrlHost.substring(4)
+    }
     if (firstPartyUrl.protocol && firstPartyUrl.protocol.startsWith('http')) {
-      if (!cachedFirstParty[firstPartyUrl.host]) {
-        let firstPartyHosts = trackingProtection.findFirstPartyHosts(firstPartyUrl.host)
-        cachedFirstParty[firstPartyUrl.host] =
+      if (!cachedFirstParty[firstPartyUrlHost]) {
+        let firstPartyHosts = trackingProtection.findFirstPartyHosts(firstPartyUrlHost)
+        cachedFirstParty[firstPartyUrlHost] =
           firstPartyHosts && firstPartyHosts.split(',') || []
         ++cachedFirstPartyCount
       }
@@ -45,10 +49,10 @@ const startTrackingProtection = (wnd) => {
     const urlHost = URL.parse(details.url).host
     const shouldBlock = firstPartyUrl.protocol &&
       firstPartyUrl.protocol.startsWith('http') &&
-      cachedFirstParty[firstPartyUrl.host] &&
+      cachedFirstParty[firstPartyUrlHost] &&
       trackingProtection.matchesTracker(urlHost) &&
-      urlHost !== firstPartyUrl.host &&
-      !cachedFirstParty[firstPartyUrl.host].find((baseHost) =>
+      urlHost !== firstPartyUrlHost &&
+      !cachedFirstParty[firstPartyUrlHost].find((baseHost) =>
         !isThirdPartyHost(baseHost, urlHost))
 
     DataFile.debug(resourceName, details, shouldBlock)
