4de26359771766514abe30a8bc6c62a9b401000f
Brian R. Bondy <netzen@gmail.com>
2016-01-13 01:21:57 -0500

    Fix last window closing on Windows
    If app.quit is called sync then BrowserWindow.getAllWindows() returns 1 and it never closes
    
    Auditors: @bridiver

--- app/index.js
+++ app/index.js
@@ -22,7 +22,7 @@ app.on('window-all-closed', function () {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit()
+    setTimeout(app.quit, 0)
   }
 })
 
