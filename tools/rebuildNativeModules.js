2964be26e757feb37988749eb1e312393644a03e
Brian R. Bondy <netzen@gmail.com>
2016-01-13 23:26:38 -0500

    Rebuild sqlite3 for electron
    Auditors: @diracdeltas

--- tools/rebuildNativeModules.js
+++ tools/rebuildNativeModules.js
@@ -1,5 +1,23 @@
 var VersionInfo = require('./lib/versionInfo')
 var execute = require('./lib/execute')
+var fs = require('fs')
+
+console.log('Patching sqlite3 binding.gyp...')
+;(function patchSqliteBindingGYP (gypPath, moduleName, modulePath) {
+  var gypFileContents =  fs.readFileSync(gypPath, 'UTF8')
+  // Backup the old file
+  fs.writeFileSync(gypPath + '.old', gypFileContents)
+  // Replace some variables which aren't supported by the npm dist version
+  // we need to use.
+  gypFileContents = gypFileContents
+    .replace(/<\(module_name\)/g, moduleName)
+    .replace(/<\(module_path\)/g, modulePath)
+  // Write out the patched binding.gyp file
+  fs.writeFileSync(gypPath, gypFileContents)
+})('./node_modules/sqlite3/binding.gyp',
+  'node_sqlite3',
+  './lib/binding/node-v' + process.versions.modules +
+    '-' + process.platform + '-' + process.arch)
 
 console.log('Installing native modules, please wait...')
 var env = {
@@ -15,6 +33,12 @@ var cmds = [
     ' --dist-url=https://atom.io/download/atom-shell',
   'cd ../../',
   'cd ./node_modules/tracking-protection',
+  '"../../node_modules/.bin/node-gyp" rebuild' +
+    ' --target=' + VersionInfo.electronVersion +
+    ' --arch=x64' +
+    ' --dist-url=https://atom.io/download/atom-shell',
+  'cd ../../',
+  'cd ./node_modules/sqlite3',
   '"../../node_modules/.bin/node-gyp" rebuild' +
     ' --target=' + VersionInfo.electronVersion +
     ' --arch=x64' +
