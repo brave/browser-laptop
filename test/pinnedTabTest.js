54ad89a9e95d5354dced292c2d19ca8c185b5015
Robert Blasetti <rblasett@thoughtworks.com>
2016-02-09 21:51:36 +1100

    Add two pinned tab tests

--- test/pinnedTabTest.js
+++ test/pinnedTabTest.js
@@ -13,17 +13,52 @@ describe('pinnedTabs', function () {
       .waitForVisible(urlInput)
   }
 
-  describe('new tab signal', function () {
+  describe('new pinned tab', function () {
+    Brave.beforeAll(this)
+    before(function *() {
+      this.page1Url = Brave.server.url('page1.html')
+      yield setup(this.app.client)
+    })
+    it.skip('creates when signaled', function *() {
+      yield this.app.client
+        .ipcSend(messages.SHORTCUT_NEW_FRAME, 'http://www.brave.com', {isPinned: true})
+        .waitForExist('.tab.isPinned[data-frame-key="2"]')
+    })
+  })
+
+  describe('close pinned tab', function () {
+    Brave.beforeAll(this)
+    before(function *() {
+      this.page1Url = Brave.server.url('page1.html')
+      yield setup(this.app.client)
+    })
+    it.skip('should focus on next tab', function *() {
+      yield this.app.client
+        .ipcSend(messages.SHORTCUT_NEW_FRAME, 'http://www.brave.com')
+        .ipcSend(messages.SHORTCUT_NEW_FRAME, 'http://www.brave.com', {isPinned: true})
+        .rightClick('.tab.isPinned[data-frame-key="2"]').then(function () {
+          // need to implement close button on pinned tabs
+          this.click('#close')
+        })
+        .elementActive('.tab[data-frame-key="1"]').should.eventually.be(true)
+    })
+  })
+
+  describe('close last pinned tab', function () {
     Brave.beforeAll(this)
     before(function *() {
       this.page1Url = Brave.server.url('page1.html')
       yield setup(this.app.client)
     })
 
-    it.skip('creates a new pinned tab when signaled', function *() {
+    it.skip('should close the window if there are no other tabs', function *() {
       yield this.app.client
-        .ipcSend(messages.SHORTCUT_NEW_FRAME, this.page1Url, { isPinned: true })
-        .waitForExist('.tab.isPinned[data-frame-key="2"]')
+        .ipcSend(messages.SHORTCUT_NEW_FRAME, 'http://www.brave.com', {isPinned: true})
+        .rightClick('.tab[data-frame-key="2"]').then(function () {
+          // need to implement close button on pinned tabs
+          this.click('#close')
+        })
+        .isExisting('#window').should.eventually.equal(false)
     })
   })
 })
