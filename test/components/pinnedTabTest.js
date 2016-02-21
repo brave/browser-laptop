4a9aa677efe5c5fc65148c05af413c220bda91e4
Brian R. Bondy <netzen@gmail.com>
2016-02-20 22:52:19 -0500

    Test pinned tab navigating via urlbar
    Auditors: @bridiver

--- test/components/pinnedTabTest.js
+++ test/components/pinnedTabTest.js
@@ -6,7 +6,17 @@ const messages = require('../../js/constants/messages')
 const siteTags = require('../../js/constants/siteTags')
 const {urlInput} = require('../lib/selectors')
 
-describe('pinnedTabs', function () {
+function * loadUrl (client, url) {
+  yield client.ipcSend('shortcut-focus-url')
+    .moveToObject(urlInput)
+    .click(urlInput)
+    .waitForElementFocus(urlInput)
+    .setValue(urlInput, url)
+    // hit enter
+    .keys('\uE007')
+}
+
+describe.only('pinnedTabs', function () {
   function * setup (client) {
     yield client
       .waitUntilWindowLoaded()
@@ -57,6 +67,30 @@ describe('pinnedTabs', function () {
         // true for reverse
         .waitForExist('.tab.isPinned[data-frame-key="3"]', 3000, true)
         .waitForExist('.tab.isPinned[data-frame-key="2"]')
+    })
+  })
+
+  describe('Pinned tab navigation', function () {
+    Brave.beforeAll(this)
+    before(function *() {
+      yield setup(this.app.client)
+      const page1Url = Brave.server.url('page1.html')
+      yield this.app.client
+        .ipcSend(messages.SHORTCUT_NEW_FRAME, page1Url)
+        .waitForExist('.tab[data-frame-key="2"]')
+        .setPinned(2, true)
+    })
+    it('navigate within the same origin', function *() {
+      const page2Url = Brave.server.url('page2.html')
+      yield loadUrl(this.app.client, page2Url)
+      yield this.app.client.waitUntil(function () {
+        return this.getAttribute('webview[data-frame-key="2"]', 'src').then(src => src === page2Url)
+      })
+    })
+    it('navigating to a different origin opens a new tab', function *() {
+      const page2Url = Brave.server.url('page2.html').replace('localhost', '127.0.0.1')
+      yield loadUrl(this.app.client, page2Url)
+      this.app.client.waitForExist('webview[data-frame-key="3"]')
     })
   })
 })
