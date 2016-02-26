35efd0ecdcaf98deee1d29c1a319fbbfe04a0c59
yan <yan@mit.edu>
2016-02-25 21:00:54 -0800

    Add basic permissions dialog
    Adds a blocking permissions dialog for the 6 permission types currently
    supported in Electron.
    
    Auditors: @bbondy

--- app/filtering.js
+++ app/filtering.js
@@ -15,6 +15,7 @@ const getBaseDomain = require('../js/lib/baseDomain').getBaseDomain
 const getSetting = require('../js/settings').getSetting
 const settings = require('../js/constants/settings')
 const ipcMain = electron.ipcMain
+const dialog = electron.dialog
 
 const beforeSendHeadersFilteringFns = []
 const beforeRequestFilteringFns = []
@@ -112,6 +113,70 @@ function registerForBeforeSendHeaders (session) {
   })
 }
 
+/**
+ * Register permission request handler
+ * @param {Object} session to add permission request handler on
+ */
+function registerPermissionHandler (session) {
+  // Keep track of per-site permissions granted for this session.
+  // TODO: Localize strings
+  let permissions = {
+    media: {
+      action: 'use your camera and/or microphone',
+      hosts: {}
+    },
+    geolocation: {
+      action: 'see your location',
+      hosts: {}
+    },
+    notifications: {
+      action: 'show you notifications',
+      hosts: {}
+    },
+    midiSysex: {
+      action: 'use web MIDI',
+      hosts: {}
+    },
+    pointerLock: {
+      action: 'disable your mouse cursor',
+      hosts: {}
+    },
+    fullscreen: {
+      action: 'be fullscreen',
+      hosts: {}
+    }
+  }
+  session.setPermissionRequestHandler((webContents, permission, cb) => {
+    let host = urlParse(webContents.getURL()).host
+    let isAllowed = permissions[permission].hosts[host]
+    if (isAllowed !== undefined) {
+      cb(isAllowed)
+    } else {
+      // TODO: Add option to remember decision between restarts.
+      let result = dialog.showMessageBox({
+        type: 'question',
+        message: host + ' is requesting permission to ' + permissions[permission].action,
+        buttons: ['Deny', 'Allow'],
+        defaultId: 0,
+        cancelId: 0
+      })
+      let isTemp = dialog.showMessageBox({
+        type: 'question',
+        title: 'Remember this decision?',
+        message: 'Would you like to remember this decision on ' + host + ' until Brave closes?',
+        buttons: ['Yes', 'No'],
+        defaultId: 0,
+        cancelId: 0
+      })
+      result = !!(result)
+      cb(result)
+      if (!isTemp) {
+        permissions[permission].hosts[host] = result
+      }
+    }
+  })
+}
+
 module.exports.isThirdPartyHost = (baseContextHost, testHost) => {
   // TODO: Always return true if these are IP addresses that aren't the same
   if (!testHost || !baseContextHost) {
@@ -126,7 +191,7 @@ module.exports.isThirdPartyHost = (baseContextHost, testHost) => {
 }
 
 function initForPartition (partition) {
-  [registerForBeforeRequest, registerForBeforeSendHeaders].forEach(fn => {
+  [registerPermissionHandler, registerForBeforeRequest, registerForBeforeSendHeaders].forEach(fn => {
     fn(session.fromPartition(partition))
   })
 }
