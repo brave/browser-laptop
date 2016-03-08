ae4370b41ce8390c7f36a386ae5716698b5d69eb
bradleyrichter <bradr@Bradleys-MacBook-Pro-2.local>
2016-03-08 09:29:42 -0800

    reduced chrome height and adjusted alignments

--- js/components/urlBar.js
+++ js/components/urlBar.js
@@ -240,7 +240,7 @@ class UrlBar extends ImmutableComponent {
               urlbarIcon: true,
               'fa': true,
               'fa-lock': this.isHTTPPage && this.secure && !this.props.urlbar.get('active'),
-              'fa-unlock': this.isHTTPPage && !this.secure && !this.props.urlbar.get('active') && !this.props.titleMode,
+              'fa-unlock-alt': this.isHTTPPage && !this.secure && !this.props.urlbar.get('active') && !this.props.titleMode,
               'fa fa-search': this.props.searchSuggestions && this.props.urlbar.get('active') && this.props.loading === false,
               'fa fa-file': !this.props.searchSuggestions && this.props.urlbar.get('active') && this.props.loading === false,
               extendedValidation: this.extendedValidationSSL
