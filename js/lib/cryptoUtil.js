dd0fd86c2073ef9232be4c38698b633efd422307
yan <yan@mit.edu>
2016-03-10 18:24:38 -0800

    Refactor CryptoUtil and add more unit tests

--- js/lib/cryptoUtil.js
+++ js/lib/cryptoUtil.js
@@ -7,6 +7,8 @@
 const crypto = require('crypto')
 
 const AES_256_GCM = 'aes-256-gcm'
+const DEFAULT_ENCODING = 'binary' // encoding of ciphertexts, iv's, keys, etc.
+const PLAINTEXT_ENCODING = 'utf8' // encoding of encryption input and decryption output
 
 /**
  * Encrypts and integrity-protects some text using AES GCM 256
@@ -15,16 +17,18 @@ const AES_256_GCM = 'aes-256-gcm'
  * @return {{content: string, tag: string, iv: string}}
  */
 module.exports.encryptAuthenticate = function (text, key) {
-  var ivBuffer = module.exports.getRandomBytes(12)
-  var iv = ivBuffer.toString('binary')
-  var cipher = crypto.createCipheriv(AES_256_GCM, key, iv)
-  cipher.setAAD(ivBuffer)
-  var encrypted = cipher.update(text, 'utf8', 'binary')
-  encrypted += cipher.final('binary')
+  var iv = module.exports.getRandomBytes(12)
+  var encrypted = module.exports.encryptAuthenticateInternal(
+    new Buffer(text, PLAINTEXT_ENCODING),
+    new Buffer(key, DEFAULT_ENCODING),
+    iv,
+    iv,
+    DEFAULT_ENCODING
+  )
   return {
-    content: encrypted,
-    tag: cipher.getAuthTag().toString('binary'),
-    iv: iv
+    content: encrypted.ciphertext,
+    tag: encrypted.tag,
+    iv: iv.toString(DEFAULT_ENCODING)
   }
 }
 
@@ -38,12 +42,15 @@ module.exports.encryptAuthenticate = function (text, key) {
  */
 module.exports.decryptVerify = function (encrypted, authTag, key, iv) {
   try {
-    let decipher = crypto.createDecipheriv(AES_256_GCM, key, iv)
-    decipher.setAAD(new Buffer(iv, 'binary'))
-    decipher.setAuthTag(new Buffer(authTag, 'binary'))
-    let decrypted = decipher.update(encrypted, 'binary', 'utf8')
-    decrypted += decipher.final('utf8')
-    return decrypted
+    let ivBuffer = new Buffer(iv, DEFAULT_ENCODING)
+    return module.exports.decryptVerifyInternal(
+      new Buffer(encrypted, DEFAULT_ENCODING),
+      new Buffer(key, DEFAULT_ENCODING),
+      ivBuffer,
+      ivBuffer,
+      new Buffer(authTag, DEFAULT_ENCODING),
+      PLAINTEXT_ENCODING
+    )
   } catch (e) {
     // node throws error if authentication fails
     console.log('got decryption failure', e)
@@ -58,4 +65,48 @@ module.exports.decryptVerify = function (encrypted, authTag, key, iv) {
  */
 module.exports.getRandomBytes = function (size) {
   return crypto.randomBytes(size)
+}
+
+/**
+ * Encrypts and integrity-protects some data using AES GCM 256
+ * @param {Buffer} pdata - plaintext data to encrypt
+ * @param {Buffer} key - secret key
+ * @param {Buffer} iv - initialization vector
+ * @param {Buffer|undefined} adata - additional authenticated data
+ * @param {string} outputEncoding - 'hex', 'binary', or 'base64'
+ * @return {{ciphertext: string, tag: string}}
+ */
+module.exports.encryptAuthenticateInternal = function (pdata, key, iv, adata, outputEncoding) {
+  // Note that Node's GCM implementation only accepts 12 byte IV's
+  var cipher = crypto.createCipheriv(AES_256_GCM, key, iv)
+  if (adata) {
+    cipher.setAAD(adata)
+  }
+  var encrypted = cipher.update(pdata, undefined, outputEncoding)
+  encrypted += cipher.final(outputEncoding)
+  return {
+    ciphertext: encrypted,
+    tag: cipher.getAuthTag().toString(outputEncoding)
+  }
+}
+
+/**
+ * Decrypts and verifies some data using AES GCM 256
+ * @param {Buffer} ciphertext - ciphertext
+ * @param {Buffer} key - secret key
+ * @param {Buffer} iv - initialization vector
+ * @param {Buffer|undefined} adata - additional authenticated data
+ * @param {Buffer} tag - authentication tag
+ * @param {string} outputEncoding - 'binary', 'ascii', 'utf8'
+ * @return {string}
+ */
+module.exports.decryptVerifyInternal = function (ciphertext, key, iv, adata, tag, outputEncoding) {
+  let decipher = crypto.createDecipheriv(AES_256_GCM, key, iv)
+  if (adata) {
+    decipher.setAAD(adata)
+  }
+  decipher.setAuthTag(tag)
+  let decrypted = decipher.update(ciphertext, undefined, outputEncoding)
+  decrypted += decipher.final(outputEncoding)
+  return decrypted
 }
--- test/unit/cryptoUtilTest.js
+++ test/unit/cryptoUtilTest.js
@@ -25,4 +25,84 @@ describe('crypto util test', function () {
     let wrongEncrypted = CryptoUtil.encryptAuthenticate(tamperedMessage, key)
     assert.equal(null, CryptoUtil.decryptVerify(encrypted.content, wrongEncrypted.tag, key, encrypted.iv))
   })
+  it('decryption fails with wrong IV', function () {
+    let message = 'hello world'
+    let key = '3zTvzr3p67VC61jmV54rIYu1545x4TlY'
+    let encrypted = CryptoUtil.encryptAuthenticate(message, key)
+    assert.equal(null, CryptoUtil.decryptVerify(encrypted.content, encrypted.tag, key,
+                                                CryptoUtil.getRandomBytes(12)))
+  })
+  // Try some test vectors from
+  // http://csrc.nist.gov/groups/ST/toolkit/BCM/documents/proposedmodes/gcm/gcm-revised-spec.pdf
+  it('passes NIST Test Case 13', function () {
+    let K = '0000000000000000000000000000000000000000000000000000000000000000'
+    let P = ''
+    let IV = '000000000000000000000000'
+    let encrypted = CryptoUtil.encryptAuthenticateInternal(
+      new Buffer(P, 'hex'),
+      new Buffer(K, 'hex'),
+      new Buffer(IV, 'hex'),
+      undefined,
+      'hex'
+    )
+    let decrypted = CryptoUtil.decryptVerifyInternal(
+      new Buffer('', 'hex'), new Buffer(K, 'hex'), new Buffer(IV, 'hex'), undefined,
+      new Buffer('530f8afbc74536b9a963b4f1c4cb738b', 'hex'), 'binary'
+    )
+    assert.equal(encrypted.ciphertext, '')
+    assert.equal(encrypted.tag, '530f8afbc74536b9a963b4f1c4cb738b')
+    assert.equal(decrypted, P)
+  })
+  it('passes NIST Test Case 14', function () {
+    let K = '0000000000000000000000000000000000000000000000000000000000000000'
+    let P = '00000000000000000000000000000000'
+    let IV = '000000000000000000000000'
+    let encrypted = CryptoUtil.encryptAuthenticateInternal(
+      new Buffer(P, 'hex'),
+      new Buffer(K, 'hex'),
+      new Buffer(IV, 'hex'),
+      undefined,
+      'hex'
+    )
+    assert.equal(encrypted.ciphertext, 'cea7403d4d606b6e074ec5d3baf39d18')
+    assert.equal(encrypted.tag, 'd0d1c8a799996bf0265b98b5d48ab919')
+  })
+  it('passes NIST Test Case 15', function () {
+    let K = 'feffe9928665731c6d6a8f9467308308feffe9928665731c6d6a8f9467308308'
+    let P = 'd9313225f88406e5a55909c5aff5269a86a7a9531534f7da2e4c303d8a318a721c3c0c95956809532fcf0e2449a6b525b16aedf5aa0de657ba637b391aafd255'
+    let IV = 'cafebabefacedbaddecaf888'
+    let encrypted = CryptoUtil.encryptAuthenticateInternal(
+      new Buffer(P, 'hex'),
+      new Buffer(K, 'hex'),
+      new Buffer(IV, 'hex'),
+      undefined,
+      'hex'
+    )
+    assert.equal(encrypted.ciphertext, '522dc1f099567d07f47f37a32a84427d' +
+                 '643a8cdcbfe5c0c97598a2bd2555d1aa' +
+                 '8cb08e48590dbb3da7b08b1056828838' +
+                 'c5f61e6393ba7a0abcc9f662898015ad')
+    assert.equal(encrypted.tag, 'b094dac5d93471bdec1a502270e3cc6c')
+  })
+  it('passes NIST Test Case 16', function () {
+    let K = 'feffe9928665731c6d6a8f9467308308feffe9928665731c6d6a8f9467308308'
+    let P = 'd9313225f88406e5a55909c5aff5269a86a7a9531534f7da2e4c303d8a318a721c3c0c95956809532fcf0e2449a6b525b16aedf5aa0de657ba637b39'
+    let IV = 'cafebabefacedbaddecaf888'
+    let A = 'feedfacedeadbeeffeedfacedeadbeefabaddad2'
+    let encrypted = CryptoUtil.encryptAuthenticateInternal(
+      new Buffer(P, 'hex'),
+      new Buffer(K, 'hex'),
+      new Buffer(IV, 'hex'),
+      new Buffer(A, 'hex'),
+      'hex'
+    )
+    let decrypted = CryptoUtil.decryptVerifyInternal(
+      new Buffer('522dc1f099567d07f47f37a32a84427d643a8cdcbfe5c0c97598a2bd2555d1aa8cb08e48590dbb3da7b08b1056828838c5f61e6393ba7a0abcc9f662', 'hex'),
+      new Buffer(K, 'hex'), new Buffer(IV, 'hex'), new Buffer(A, 'hex'),
+      new Buffer('76fc6ece0f4e1768cddf8853bb2d551b', 'hex'), 'binary'
+    )
+    assert.equal(encrypted.ciphertext, '522dc1f099567d07f47f37a32a84427d643a8cdcbfe5c0c97598a2bd2555d1aa8cb08e48590dbb3da7b08b1056828838c5f61e6393ba7a0abcc9f662')
+    assert.equal(encrypted.tag, '76fc6ece0f4e1768cddf8853bb2d551b')
+    assert.equal(P, new Buffer(decrypted, 'binary').toString('hex'))
+  })
 })
