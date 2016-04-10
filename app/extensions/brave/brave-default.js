dd9d418a8acc114756cb473e1e3e25838284f5f8
yan <yan@mit.edu>
2016-04-09 20:44:54 -0700

    Ensure that passwords' form action is a string
    Fix #1305
    
    Auditors: @bbondy

--- app/extensions/brave/brave-default.js
+++ app/extensions/brave/brave-default.js
@@ -362,6 +362,9 @@ if (typeof this.KeyEvent === 'undefined') {
    * @return {string}
    */
   function normalizeURL (url) {
+    if (typeof url !== 'string') {
+      return ''
+    }
     var a = document.createElement('a')
     a.href = url
     return [a.protocol, a.host].join('//') + a.pathname
