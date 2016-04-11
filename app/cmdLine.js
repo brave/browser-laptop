aeb6be4faa762441e6f5d9bed4a5eed3e26a1c0d
bridiver <github@brianjohnson.cc>
2016-04-10 21:02:13 -0700

    don't try to open a window unless the app is initialized fixes #1298

--- app/cmdLine.js
+++ app/cmdLine.js
@@ -12,28 +12,56 @@ const BrowserWindow = electron.BrowserWindow
 const appActions = require('../js/actions/appActions')
 let appInitialized = false
 
+const focusOrOpenWindow = function (url) {
+  // don't try to do anything if the app hasn't been initialized
+  if (!appInitialized) {
+    return false
+  }
+
+  let win = BrowserWindow.getFocusedWindow()
+  if (!win) {
+    win = BrowserWindow.getAllWindows()[0]
+    if (win) {
+      if (win.isMinimized()) {
+        win.restore()
+      }
+      win.focus()
+    }
+  }
+
+  if (!win) {
+    appActions.newWindow(Immutable.fromJS({
+      location: url
+    }))
+  } else if (url) {
+    win.webContents.send(messages.SHORTCUT_NEW_FRAME, url)
+  }
+
+  return true
+}
+
+if (process.env.NODE_ENV !== 'development' && process.env.NODE_ENV !== 'test') {
+  const appAlreadyStartedShouldQuit = app.makeSingleInstance((commandLine, workingDirectory) => {
+    // Someone tried to run a second instance, we should focus our window.
+    focusOrOpenWindow()
+  })
+  if (appAlreadyStartedShouldQuit) {
+    app.exit(0)
+  }
+}
+
 app.on('will-finish-launching', () => {
+  app.on('activate', () => {
+    // (OS X) open a new window when the user clicks on the app icon if there aren't any open
+    focusOrOpenWindow()
+  })
+
   // User clicked a link when w were the default or via command line like:
   // open -a Brave http://www.brave.com
   app.on('open-url', (event, path) => {
     event.preventDefault()
 
-    if (appInitialized) {
-      let wnd = BrowserWindow.getFocusedWindow()
-      if (!wnd) {
-        const wnds = BrowserWindow.getAllWindows()
-        if (wnds.length > 0) {
-          wnd = wnds[0]
-        }
-      }
-      if (wnd) {
-        wnd.webContents.send(messages.SHORTCUT_NEW_FRAME, path)
-      } else {
-        appActions.newWindow(Immutable.fromJS({
-          location: path
-        }))
-      }
-    } else {
+    if (!focusOrOpenWindow(path)) {
       module.exports.newWindowURL = path
     }
   })
