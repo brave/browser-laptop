f9f3363e196fa64a2e6135bbdf753f900f515629
yan <yan@mit.edu>
2016-04-12 21:20:58 -0700

    Make flow pass on app/index.js

--- decls/globals.js
+++ decls/globals.js
@@ -8,3 +8,10 @@ declare class IpcRenderer {
     sendToHost(msg: string, ...args: any): void;
 }
 declare var KeyEvent: any;
+declare module electron {
+  declare var BrowserWindow: any;
+  declare var ipcMain: any;
+  declare var dialog: any;
+  declare var clipboard: any;
+  declare var app: any;
+}
