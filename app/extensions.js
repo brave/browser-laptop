18d12b63691b764dfeb080e4dbfd51be5764b8f4
bridiver <github@brianjohnson.cc>
2016-05-05 20:50:07 -0700

    use extension urls in dev

--- app/extensions.js
+++ app/extensions.js
@@ -1,10 +1,92 @@
 const electron = require('electron')
+const fs = require('fs')
+const path = require('path')
 const BrowserWindow = electron.BrowserWindow
 const AppStore = require('../js/stores/appStore')
 const getExtensionsPath = require('../js/lib/appUrlUtil').getExtensionsPath
 const getSetting = require('../js/settings').getSetting
 const messages = require('../js/constants/messages')
 const settings = require('../js/constants/settings')
+const config = require('../js/constants/config')
+
+let generateBraveManfiest = () => {
+  return new Promise((resolve, reject) => {
+    let baseManifest = {
+      name: 'brave',
+      manifest_version: 2,
+      version: '1.0',
+      content_scripts: [
+        {
+          all_frames: true,
+          matches: ['http://*/*', 'https://*/*'],
+          run_at: 'document_start',
+          js: [
+            'brave-default.js'
+          ],
+          css: [
+            'brave-default.css'
+          ]
+        }
+      ],
+      web_accessible_resources: [
+        'about-*.html',
+        'img/favicon.ico'
+      ],
+      incognito: 'spanning',
+      key: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAupOLMy5Fd4dCSOtjcApsAQOnuBdTs+OvBVt/3P93noIrf068x0xXkvxbn+fpigcqfNamiJ5CjGyfx9zAIs7zcHwbxjOw0Uih4SllfgtK+svNTeE0r5atMWE0xR489BvsqNuPSxYJUmW28JqhaSZ4SabYrRx114KcU6ko7hkjyPkjQa3P+chStJjIKYgu5tWBiMJp5QVLelKoM+xkY6S7efvJ8AfajxCViLGyDQPDviGr2D0VvIBob0D1ZmAoTvYOWafcNCaqaejPDybFtuLFX3pZBqfyOCyyzGhucyCmfBXJALKbhjRAqN5glNsUmGhhPK87TuGATQfVuZtenMvXMQIDAQAB'
+    }
+
+    let cspDirectives = {
+      'default-src': '\'self\'',
+      'form-action': '\'none\'',
+      'referrer': 'no-referrer',
+      'style-src': '\'self\' \'unsafe-inline\''
+    }
+
+    let aboutContentScripts = {
+      run_at: 'document_start',
+      js: [
+        'brave-about.js'
+      ]
+    }
+
+    if (process.env.NODE_ENV === 'development') {
+      aboutContentScripts.matches = fs.readdirSync(path.join(getExtensionsPath(), 'brave'))
+        .filter((filename) => (filename.match(/about-*.html/) && filename !== 'about-blank.html'))
+        .map((filename) => 'http://localhost:*/' + filename)
+      // allow access to webpack dev server resources
+      let devServer = 'localhost:' + process.env.npm_package_config_port
+      cspDirectives['default-src'] = '\'self\' http://' + devServer
+      cspDirectives['connect-src'] = '\'self\' http://' + devServer + ' ws://' + devServer
+      cspDirectives['style-src'] = '\'self\' \'unsafe-inline\' http://' + devServer
+    } else {
+      aboutContentScripts.matches = [
+        '<all_urls>'
+      ]
+      aboutContentScripts.include_globs = [
+        'chrome-extension://' + config.braveExtensionId + '/about-*.html'
+      ]
+      aboutContentScripts.exclude_globs = [
+        'chrome-extension://' + config.braveExtensionId + '/about-blank.html'
+      ]
+    }
+
+    baseManifest.content_scripts.push(aboutContentScripts)
+    var csp = ''
+    for (var directive in cspDirectives) {
+      csp += directive + ' ' + cspDirectives[directive] + '; '
+    }
+    baseManifest.content_security_policy = csp.trim()
+
+    fs.writeFile(path.join(getExtensionsPath(), 'brave', 'manifest.json'), JSON.stringify(baseManifest), (err) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve()
+      }
+    })
+  })
+}
 
 module.exports.init = () => {
   process.on('chrome-browser-action-popup', function (extensionId, name, props, popup) {
@@ -24,7 +106,7 @@ module.exports.init = () => {
     installedExtensions[installInfo.name] = installInfo
   }
 
-  let installExtension = function (name, path, options = {}) {
+  let installExtension = (name, path, options = {}) => {
     process.emit('load-extension', name, path, options, extensionInstalled)
   }
 
@@ -42,7 +124,7 @@ module.exports.init = () => {
     }
   }
 
-  let enableExtensions = function () {
+  let enableExtensions = () => {
     installExtension('brave', getExtensionsPath(), {manifest_location: 'component'})
 
     if (getSetting(settings.ONE_PASSWORD_ENABLED)) {
@@ -60,8 +142,15 @@ module.exports.init = () => {
     }
   }
 
-  AppStore.addChangeListener(() => {
+  generateBraveManfiest().catch((err) => { console.log(err) }).then((err) => {
+    if (err) {
+      console.log(err)
+    }
+
     enableExtensions()
+
+    AppStore.addChangeListener(() => {
+      enableExtensions()
+    })
   })
-  enableExtensions()
 }
--- app/extensions/brave/js/about.js
+++ app/extensions/brave/js/about.js
@@ -1,7 +1,15 @@
+var queryString = window.location.href.split('?')[1]
+var devServerPort = queryString.match(/devServerPort=([^&]*)/)[1]
+
+let aboutEntryPage = 'gen/aboutPages.entry.js'
+if (devServerPort) {
+  aboutEntryPage = 'http://localhost:' + devServerPort + '/' + aboutEntryPage
+}
+
 window.addEventListener('load', function () {
   var po = document.createElement('script')
   po.async = true
-  po.src = 'gen/aboutPages.entry.js'
+  po.src = aboutEntryPage
   var s = document.getElementsByTagName('script')[0]
   s.parentNode.insertBefore(po, s)
 })
