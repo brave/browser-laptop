bcec95cf9e1db33dac424eefeed171b12402190a
bridiver <github@brianjohnson.cc>
2016-05-05 20:50:07 -0700

    use extension urls in dev

--- app/extensions/brave/js/about.js
+++ app/extensions/brave/js/about.js
@@ -1,3 +1,11 @@
+var queryString = window.location.href.split('?')[1]
+var devServerPort = queryString.match(/devServerPort=([^&]*)/)[1]
+
+let aboutEntryPage = 'gen/aboutPages.entry.js'
+if (devServerPort) {
+  aboutEntryPage = 'http://localhost:' + devServerPort + '/' + aboutEntryPage
+}
+
 window.addEventListener('language', function (evt) {
   document.l10n.requestLanguages([evt.detail.langCode])
   document.getElementsByName('availableLanguages')[0].content = evt.detail.languageCodes.join(', ')
@@ -7,7 +15,7 @@ window.addEventListener('language', function (evt) {
 window.addEventListener('load', function () {
   var po = document.createElement('script')
   po.async = true
-  po.src = 'gen/aboutPages.entry.js'
+  po.src = aboutEntryPage
   var s = document.getElementsByTagName('script')[0]
   s.parentNode.insertBefore(po, s)
   window.dispatchEvent(new CustomEvent('request-language'))
