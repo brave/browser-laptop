e8c534a9250335e7a8839f1915e9c3a9edfa42c2
bridiver <github@brianjohnson.cc>
2016-05-19 18:57:17 -0700

    temporary workaround for #1817 auditors: @bbondy, @diracdeltas

--- js/components/frame.js
+++ js/components/frame.js
@@ -24,7 +24,7 @@ const settings = require('../constants/settings')
 const adInfo = require('../data/adInfo.js')
 const FindBar = require('./findbar.js')
 const consoleStrings = require('../constants/console')
-const { aboutUrls, isSourceAboutUrl, getTargetAboutUrl } = require('../lib/appUrlUtil')
+const { aboutUrls, isSourceAboutUrl, isTargetAboutUrl, getTargetAboutUrl } = require('../lib/appUrlUtil')
 const { isFrameError } = require('../lib/errorUtil')
 
 class Frame extends ImmutableComponent {
@@ -460,13 +460,27 @@ class Frame extends ImmutableComponent {
       this.webview = false
     })
     this.webview.addEventListener('did-fail-load', (e) => {
-      if (e.isMainFrame && isFrameError(e.errorCode)) {
-        windowActions.setFrameError(this.props.frame, {
-          event_type: 'did-fail-load',
-          errorCode: e.errorCode,
-          url: e.validatedURL
-        })
-        windowActions.loadUrl(this.props.frame, 'about:error')
+      // temporary workaround for https://github.com/brave/browser-laptop/issues/1817
+      if (e.validatedURL === aboutUrls.get('about:newtab') ||
+          e.validatedURL === aboutUrls.get('about:blank') ||
+          e.validatedURL === aboutUrls.get('about:certerror')) {
+        // this will just display a blank page for cert errors
+        // but we don't want to take the user out of the private tab
+        return
+      } else if (isTargetAboutUrl(e.validatedURL)) {
+        // open a new tab for other about urls
+        // and send this tab back to wherever it came from
+        this.goBack()
+        windowActions.newFrame({location: e.validatedURL}, true)
+      } else {
+        if (e.isMainFrame && isFrameError(e.errorCode)) {
+          windowActions.setFrameError(this.props.frame, {
+            event_type: 'did-fail-load',
+            errorCode: e.errorCode,
+            url: e.validatedURL
+          })
+          windowActions.loadUrl(this.props.frame, 'about:error')
+        }
       }
     })
     this.webview.addEventListener('did-finish-load', () => {
