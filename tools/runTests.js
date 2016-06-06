e0aee7533e2fe312bc3458dd0673344757aa16b8
Brian R. Bondy <netzen@gmail.com>
2016-06-05 23:03:47 -0400

    Make test command compatible with windows shell

--- tools/runTests.js
+++ tools/runTests.js
@@ -1,7 +1,24 @@
-var execute = require('./lib/execute')
+var glob = require('glob')
+process.env.NODE_ENV = 'test'
 
-var env = {
-  NODE_ENV: 'test'
-}
+var Mocha = require('mocha')
+var mocha = new Mocha()
 
-execute('mocha --compilers js:babel-register --recursive $(find test -name \'*Test.js\')', env)
+glob('test/**/*Test.js', function (err, files) {
+  if (err) {
+    console.error(err)
+    return
+  }
+  files.forEach(mocha.addFile.bind(mocha))
+  mocha.run(function (failures) {
+    process.on('exit', function () {
+      process.exit(failures)
+    })
+  })
+})
+
+process.on('SIGINT', function () {
+  console.log('Caught interrupt signal')
+  mocha = null
+  process.exit(1)
+})
