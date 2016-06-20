37f040441f4dc0a07d059d8e0eb078cf5821d6b9
Brian R. Bondy <netzen@gmail.com>
2016-06-20 14:31:13 -0400

    Fix scrolling and overflow for urlbar suggestions
    Auditors: @diracdeltas

--- js/components/urlBarSuggestions.js
+++ js/components/urlBarSuggestions.js
@@ -117,12 +117,14 @@ class UrlBarSuggestions extends ImmutableComponent {
       }
       items = items.concat(suggestions.map((suggestion, i) => {
         const currentIndex = index + i
+        const selected = this.activeIndex === currentIndex + 1 || currentIndex === 0 && hasUrlSuffix
         return <li data-index={currentIndex + 1}
           onMouseOver={this.onMouseOver.bind(this)}
           onClick={suggestion.onClick}
           key={suggestion.title}
+          ref={(node) => { selected && (this.selectedElement = node) }}
           className={cx({
-            selected: this.activeIndex === currentIndex + 1 || currentIndex === 0 && hasUrlSuffix,
+            selected,
             suggestionItem: true,
             [suggestion.type]: true
           })}>
@@ -145,8 +147,10 @@ class UrlBarSuggestions extends ImmutableComponent {
     addToItems(historySuggestions, 'historyTitle', locale.translation('historySuggestionTitle'), 'fa-clock-o')
     addToItems(searchSuggestions, 'searchTitle', locale.translation('searchSuggestionTitle'), 'fa-search')
     addToItems(topSiteSuggestions, 'topSiteTitle', locale.translation('topSiteSuggestionTitle'), 'fa-link')
-
-    return <ul className='urlBarSuggestions'>
+    const documentHeight = Number.parseInt(window.getComputedStyle(document.querySelector(':root')).getPropertyValue('--navbar-height'), 10)
+    return <ul className='urlBarSuggestions' style={{
+      maxHeight: document.documentElement.offsetHeight - documentHeight - 2
+    }}>
       {items}
     </ul>
   }
@@ -156,6 +160,9 @@ class UrlBarSuggestions extends ImmutableComponent {
   }
 
   componentDidUpdate (prevProps) {
+    if (this.selectedElement) {
+      this.selectedElement.scrollIntoView()
+    }
     if (this.props.urlLocation === prevProps.urlLocation) {
       return
     }
