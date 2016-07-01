195fa2a7cd079740c20a15ea6138b9185721621f
Ludable <github@ludable.com>
2016-06-30 22:06:42 -0700

    Hide downloads bar when the current frame is about:downloads.
    Fix #2012

--- js/components/main.js
+++ js/components/main.js
@@ -829,7 +829,7 @@ class Main extends ImmutableComponent {
         </div>
       </div>
       {
-        this.props.windowState.getIn(['ui', 'downloadsToolbar', 'isVisible']) && this.props.appState.get('downloads') && this.props.appState.get('downloads').size > 0
+        this.props.windowState.getIn(['ui', 'downloadsToolbar', 'isVisible']) && this.props.appState.get('downloads') && this.props.appState.get('downloads').size > 0 && activeFrame.get('location') !== 'about:downloads'
         ? <DownloadsBar
           windowWidth={this.props.appState.get('defaultWindowWidth')}
           downloads={this.props.appState.get('downloads')} />
