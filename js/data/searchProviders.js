688fa232d283bd1f72e2759ea9514022241d01b1
Anthony Tseng <darkdh@gmail.com>
2016-08-02 13:01:17 +0800

    Replace &amp; with &

--- js/data/searchProviders.js
+++ js/data/searchProviders.js
@@ -8,34 +8,34 @@ module.exports = { "providers" :
       "name" : "Amazon",
       "image" : "http://www.amazon.com/favicon.ico",
       "search" : "http://www.amazon.com/exec/obidos/external-search/?field-keywords={searchTerms}&mode=blended",
-      "autocomplete" : "http://completion.amazon.com/search/complete?method=completion&amp;q={searchTerms}&amp;search-alias=aps&amp;client=amazon-search-ui&amp;mkt=1",
+      "autocomplete" : "http://completion.amazon.com/search/complete?method=completion&q={searchTerms}&search-alias=aps&client=amazon-search-ui&mkt=1",
       "shortcut" : "a"
     },
     {
       "name" : "Bing",
       "image" : "https://www.bing.com/favicon.ico",
       "search" : "https://www.bing.com/search?q={searchTerms}",
-      "autocomplete" : "http://api.bing.com/osjson.aspx?query={searchTerms}&amp;language={language}&amp;form=OSDJAS",
+      "autocomplete" : "http://api.bing.com/osjson.aspx?query={searchTerms}&language={language}&form=OSDJAS",
       "shortcut" : "b"
     },
     {
       "name" : "DuckDuckGo",
       "image" : "https://duckduckgo.com/favicon.ico",
-      "search" : "https://duckduckgo.com/?q={searchTerms}&amp;t=brave",
-      "autocomplete" : "https://ac.duckduckgo.com/ac/?q={searchTerms}&amp;type=list",
+      "search" : "https://duckduckgo.com/?q={searchTerms}&t=brave",
+      "autocomplete" : "https://ac.duckduckgo.com/ac/?q={searchTerms}&type=list",
       "shortcut" : "d"
     },
     {
       "name" : "Google",
       "image" : "https://www.google.com/favicon.ico",
       "search" : "https://www.google.com/search?q={searchTerms}",
-      "autocomplete" : "https://suggestqueries.google.com/complete/search?client=chrome&amp;q={searchTerms}",
+      "autocomplete" : "https://suggestqueries.google.com/complete/search?client=chrome&q={searchTerms}",
       "shortcut" : "g"
     },
     {
       "name" : "Twitter",
       "image" : "https://twitter.com/favicon.ico",
-      "search" : "https://twitter.com/search?q={searchTerms}&amp;source=desktop-search",
+      "search" : "https://twitter.com/search?q={searchTerms}&source=desktop-search",
       "autocomplete" : "https://api.twitter.com/1.1/search/tweets.json?q={searchTerms}",
       "shortcut" : "t"
     },
@@ -49,15 +49,15 @@ module.exports = { "providers" :
     {
       "name" : "Yahoo",
       "image" : "https://search.yahoo.com/favicon.ico",
-      "search" : "https://search.yahoo.com/search?p={searchTerms}&amp;fr=opensearch",
-      "autocomplete": "https://search.yahoo.com/sugg/os?command={searchTerms}&amp;output=fxjson&amp;fr=opensearch",
+      "search" : "https://search.yahoo.com/search?p={searchTerms}&fr=opensearch",
+      "autocomplete": "https://search.yahoo.com/sugg/os?command={searchTerms}&output=fxjson&fr=opensearch",
       "shortcut" : "y"
     },
     {
       "name" : "Youtube",
       "image" : "https://www.youtube.com/favicon.ico",
-      "search" : "https://www.youtube.com/results?search_type=search_videos&amp;search_query={searchTerms}&amp;search_sort=relevance&amp;search_category=0&amp;page=",
-      "autocomplete": "http://suggestqueries.google.com/complete/search?output=chrome&amp;client=chrome&amp;hl=it&amp;q={searchTerms}&amp;ds=yt",
+      "search" : "https://www.youtube.com/results?search_type=search_videos&search_query={searchTerms}&search_sort=relevance&search_category=0&page=",
+      "autocomplete": "http://suggestqueries.google.com/complete/search?output=chrome&client=chrome&hl=it&q={searchTerms}&ds=yt",
       "shortcut" : "yt"
     }
   ]
