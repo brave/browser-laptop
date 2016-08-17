5cadc80a3c85f68a0523d6c51643568edc2fb872
Willy Bruns <willy@entropi.info>
2016-08-17 01:18:10 -0700

    Adding test for #3012 fix
    - added test to urlBarSuggestionsTest.js to check that mousing over autocomplete suggestions does not interrupt typing of URL

--- js/actions/windowActions.js
+++ js/actions/windowActions.js
@@ -489,8 +489,8 @@ const windowActions = {
   setUrlBarSuggestions: function (suggestionList, selectedIndex) {
     dispatch({
       actionType: WindowConstants.WINDOW_SET_URL_BAR_SUGGESTIONS,
-      suggestionList: suggestionList,
-      selectedIndex: selectedIndex
+      suggestionList,
+      selectedIndex
     })
   },
 
