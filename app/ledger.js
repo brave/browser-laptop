55f22884053da20bc3c34e256e0a21c10f0eb519
Marshall Rose <mrose17@gmail.com>
2016-08-24 12:44:32 -0700

    Revert "don't let slow network connectivity result in an error"
    This reverts commit 943c6e3178469ee34cafee208fd9ac483c9a10ae.

--- app/ledger.js
+++ app/ledger.js
@@ -867,11 +867,7 @@ var run = (delayTime) => {
   }
 
   if (delayTime === 0) {
-    try {
-      delayTime = client.timeUntilReconcile()
-    } catch (ex) {
-      delayTime = random.randomInt({ min: 1, max: 10 * msecs.minute })
-    }
+    delayTime = client.timeUntilReconcile()
     if (delayTime === false) delayTime = 0
   }
   if (delayTime > 0) {
