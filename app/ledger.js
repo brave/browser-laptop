60f3957aaab474f5cb9c650bec1eac6e2317ecc1
Marshall Rose <mrose17@gmail.com>
2016-08-25 16:27:00 -0700

    Revert "remove redundant line"
    This reverts commit 4396d6460fab1280c8792b918dbac3a2b2afbdce.

--- app/ledger.js
+++ app/ledger.js
@@ -130,6 +130,7 @@ var boot = () => {
 
     if (err.code !== 'ENOENT') console.log('statePath read error: ' + err.toString())
 
+    ledgerInfo.creating = true
     appActions.updateLedgerInfo({ creating: true })
     try {
       client = (require('ledger-client'))(null, underscore.extend({ roundtrip: roundtrip }, clientOptions), null)
