df392c387a04d6957af42ead1a2aca0f1e9312dc
Scott King <kingsc850@gmail.com>
2016-08-27 18:39:40 -0400

    Scaffolding for deafult search engine appearing as new tab

--- js/data/searchProviders.js
+++ js/data/searchProviders.js
@@ -6,58 +6,65 @@ module.exports = { "providers" :
   [
     {
       "name" : "Amazon",
-      "image" : "https://www.amazon.com/favicon.ico",
-      "search" : "https://www.amazon.com/exec/obidos/external-search/?field-keywords={searchTerms}&mode=blended",
+      "base" : "https://www.amazon.com",
+      "image" : `${this.base}/favicon.ico`,
+      "search" : `${this.base}/exec/obidos/external-search/?field-keywords={searchTerms}&mode=blended`,
       "autocomplete" : "https://completion.amazon.com/search/complete?method=completion&q={searchTerms}&search-alias=aps&client=amazon-search-ui&mkt=1",
       "shortcut" : "a"
     },
     {
       "name" : "Bing",
-      "image" : "https://www.bing.com/favicon.ico",
-      "search" : "https://www.bing.com/search?q={searchTerms}",
+      "base" : "https://www.bing.com",
+      "image" : `${this.base}/favicon.ico`,
+      "search" : `${this.base}/search?q={searchTerms}`,
       "autocomplete" : "https://api.bing.com/osjson.aspx?query={searchTerms}&language={language}&form=OSDJAS",
       "shortcut" : "b"
     },
     {
       "name" : "DuckDuckGo",
-      "image" : "https://duckduckgo.com/favicon.ico",
-      "search" : "https://duckduckgo.com/?q={searchTerms}&t=brave",
+      "base" : "https://duckduckgo.com",
+      "image" : `${this.base}/favicon.ico`,
+      "search" : `${this.base}/?q={searchTerms}&t=brave`,
       "autocomplete" : "https://ac.duckduckgo.com/ac/?q={searchTerms}&type=list",
       "shortcut" : "d"
     },
     {
       "name" : "Google",
-      "image" : "https://www.google.com/favicon.ico",
-      "search" : "https://www.google.com/search?q={searchTerms}",
+      "base" : "https://www.google.com",
+      "image" : `${this.base}/favicon.ico`,
+      "search" : `${this.base}/search?q={searchTerms}`,
       "autocomplete" : "https://suggestqueries.google.com/complete/search?client=chrome&q={searchTerms}",
       "shortcut" : "g"
     },
     {
       "name" : "Twitter",
-      "image" : "https://twitter.com/favicon.ico",
-      "search" : "https://twitter.com/search?q={searchTerms}&source=desktop-search",
+      "base" : "https://twitter.com",
+      "image" : `${this.base}/favicon.ico`,
+      "search" : `${this.base}/search?q={searchTerms}&source=desktop-search`,
       "shortcut" : "t"
     },
     {
       "name" : "Wikipedia",
-      "image" : "https://en.wikipedia.org/favicon.ico",
+      "base" : "https://en.wikipedia.org",
+      "image" : `${this.base}/favicon.ico`,
       "search" : "https://en.wikipedia.org/wiki/Special:Search?search={searchTerms}",
       "shortcut" : "w"
     },
     {
       "name" : "Yahoo",
-      "image" : "https://search.yahoo.com/favicon.ico",
+      "base" : "https://search.yahoo.com",
+      "image" : `${this.base}/favicon.ico`,
       "search" : "https://search.yahoo.com/search?p={searchTerms}&fr=opensearch",
       "autocomplete": "https://search.yahoo.com/sugg/os?command={searchTerms}&output=fxjson&fr=opensearch",
       "shortcut" : "y"
     },
     {
       "name" : "Youtube",
-      "image" : "https://www.youtube.com/favicon.ico",
-      "search" : "https://www.youtube.com/results?search_type=search_videos&search_query={searchTerms}&search_sort=relevance&search_category=0&page=",
+      "base" : "https://www.youtube.com",
+      "image" : `${this.base}/favicon.ico`,
+      "search" : `${this.base}/results?search_type=search_videos&search_query={searchTerms}&search_sort=relevance&search_category=0&page=`,
       "autocomplete": "https://suggestqueries.google.com/complete/search?output=chrome&client=chrome&hl=it&q={searchTerms}&ds=yt",
       "shortcut" : "yt"
     }
   ]
 }
-
