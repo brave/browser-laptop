f6f9ac5cc81c178fc033dbadfc9d5422d0a3eb66
bridiver <github@brianjohnson.cc>
2016-09-08 17:56:05 -0700

    update dashlane to 4.2.3 auditors: @bbondy

--- app/extensions/dashlane/content/setup_cws.js
+++
@@ -1,2 +0,0 @@
-var KW__localTime=0;KWController.init();chrome.browserAction.onClicked.addListener(function(a){KW__ORDER_SENDER.sendOrderToCpp({action:"askLoginPopup",status:"loggedout"},KW__CONFIG.defaultTabId,"fromExtensionToCpp")
-});
