0086c21ef05dd54e0bf018bef3272a37725cf67f
bridiver <github@brianjohnson.cc>
2016-09-21 18:34:09 -0700

    update node modules to electron 1.4.0 related https://github.com/brave/electron/commit/072c6eb5c3b333b90582b7b9b57bbf6eaca92b4e fix #4153

--- tools/cibuild.py
+++ tools/cibuild.py
@@ -5,7 +5,7 @@ import subprocess
 import sys
 import os.path
 
-UPSTREAM_ELECTRON = '1.3.3'
+UPSTREAM_ELECTRON = '1.4.0'
 SOURCE_ROOT = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 TARGET_ARCH= os.environ['TARGET_ARCH'] if os.environ.has_key('TARGET_ARCH') else 'x64'
 os.environ['npm_config_arch'] = TARGET_ARCH
