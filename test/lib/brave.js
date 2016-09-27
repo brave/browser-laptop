373df5796bb2842760467b98632cceebd62646c7
bridiver <github@brianjohnson.cc>
2016-09-27 10:59:42 -0700

    fix test cleanup issue auditors: @bbondy

--- test/lib/brave.js
+++ test/lib/brave.js
@@ -28,10 +28,14 @@ const rmDir = (dirPath) => {
   if (files.length > 0) {
     for (var i = 0; i < files.length; i++) {
       var filePath = path.join(dirPath, files[i])
-      if (fs.statSync(filePath).isFile()) {
-        fs.unlinkSync(filePath)
-      } else {
-        rmDir(filePath)
+      try {
+        if (fs.statSync(filePath).isFile()) {
+          fs.unlinkSync(filePath)
+        } else {
+          rmDir(filePath)
+        }
+      } catch (e) {
+        console.warn(e)
       }
     }
   }
