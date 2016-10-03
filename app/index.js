1e91bb41908e53490e277ca64643b5d8913eb470
bridiver <github@brianjohnson.cc>
2016-10-02 20:06:57 -0700

    wip tabs api cc @jkup

--- app/index.js
+++ app/index.js
@@ -80,6 +80,7 @@ const contentSettings = require('../js/state/contentSettings')
 const privacy = require('../js/state/privacy')
 const basicAuth = require('./browser/basicAuth')
 const async = require('async')
+const tabs = require('./browser/tabs')
 
 // temporary fix for #4517, #4518 and #4472
 app.commandLine.appendSwitch('enable-use-zoom-for-dsf', 'false')
@@ -415,6 +416,7 @@ app.on('ready', () => {
     Menu.init(initialState, null)
     return loadedPerWindowState
   }).then((loadedPerWindowState) => {
+    tabs.init()
     basicAuth.init()
     contentSettings.init()
     privacy.init()
