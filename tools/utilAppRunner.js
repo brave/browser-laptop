d771fdfe5fa44f057cc7d62bd6ab6918bf96604e
Willy Bruns <willy@entropi.info>
2016-10-09 18:34:45 -0700

    Add tools for generating a simulated Payment History for testing
    - `tools/lib/transactionHelpers.js`: Tools for generating and validating (w/ Joi) Ledger transactions
    - `npm run add-simulated-payment-history`: new npm script which generates a simulated payment history, allowing testing related features without spending BTC
    
    fixes #4199

---
+++ tools/utilAppRunner.js
@@ -0,0 +1,22 @@
+const path = require('path')
+const proc = require('child_process')
+
+function runUtilApp (cmd, file, stdioOptions) {
+  console.log('runUtilApp: ')
+
+  process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+  const utilAppDir = path.join(__dirname, 'lib', 'utilApp')
+  const options = {
+    env: process.env,
+    cwd: utilAppDir,
+    stdio: stdioOptions
+  }
+  cmd = cmd.split(' ')
+  const utilApp = proc.spawnSync('electron', [utilAppDir].concat(cmd), options)
+  if (utilApp.error) {
+    console.log('Could not run utilApp - run `npm install electron-prebuilt` and try again', utilApp.error)
+  }
+  return utilApp
+}
+
+module.exports = runUtilApp
