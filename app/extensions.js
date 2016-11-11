5ba15b57634be73619085b23c2d0909cde46409b
Feross Aboukhadijeh <feross@feross.org>
2016-11-10 18:58:33 -0800

    Add torrent view extension key for deterministic extension ID

--- app/extensions.js
+++ app/extensions.js
@@ -195,7 +195,8 @@ let generateTorrentManifest = () => {
     web_accessible_resources: [
       'img/favicon.ico'
     ],
-    incognito: 'split'
+    incognito: 'split',
+    key: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAyWl+wMvL0wZX3JUs7GeZAvxMP+LWEh2bwMV1HyuBra/lGZIq3Fmh0+AFnvFPXz1NpQkbLS3QWyqhdIn/lepGwuc2ma0glPzzmieqwctUurMGSGManApGO1MkcbSPhb+R1mx8tMam5+wbme4WoW37PI3oATgOs2NvHYuP60qol3U7b/zB3IWuqtwtqKe2Q1xY17btvPuz148ygWWIHneedt0jwfr6Zp+CSLARB9Heq/jqGXV4dPSVZ5ebBHLQ452iZkHxS6fm4Z+IxjKdYs3HNj/s8xbfEZ2ydnArGdJ0lpSK9jkDGYyUBugq5Qp3FH6zV89WqBvoV1dqUmL9gxbHsQIDAQAB'
   }
 }
 
