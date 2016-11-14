4fe8a6eca08bd6c81849681eaf0fc8d4f355878b
Brian Clifton <brian@clifton.me>
2016-11-14 01:21:09 -0700

    Adding tests for aboutNewTabState (follow up for https://github.com/brave/browser-laptop/commit/d2b45af872ad52fce8dadef396885f4f7fe8a2ec)
    Auditors: @bbondy

--- test/unit/common/state/aboutNewTabStateTest.js
+++ test/unit/common/state/aboutNewTabStateTest.js
@@ -42,103 +42,97 @@ describe('aboutNewTabState', function () {
 
   describe('setSites', function () {
     const site1 = Immutable.fromJS({
-      location: 'https://example1.com/',
-      title: 'sample 1',
-      parentFolderId: 0,
-      count: 10
+      location: 'https://example1.com/', title: 'sample 1', parentFolderId: 0, count: 10
     })
     const site2 = Immutable.fromJS({
-      location: 'https://example2.com',
-      title: 'sample 2',
-      parentFolderId: 0,
-      count: 5
+      location: 'https://example2.com', title: 'sample 2', parentFolderId: 0, count: 5
     })
     const site3 = Immutable.fromJS({
-      location: 'https://example3.com',
-      title: 'sample 3',
-      parentFolderId: 0,
-      count: 23
+      location: 'https://example3.com', title: 'sample 3', parentFolderId: 0, count: 23, lastAccessedTime: 123
     })
     const site4 = Immutable.fromJS({
-      location: 'https://example4.com',
-      title: 'sample 4',
-      parentFolderId: 0,
-      count: 0
+      location: 'https://example4.com', title: 'sample 4', parentFolderId: 0, count: 0
+    })
+    const site5 = Immutable.fromJS({
+      location: 'https://example4.com', title: 'sample 5', parentFolderId: 0, count: 23, lastAccessedTime: 456
     })
     const folder1 = Immutable.fromJS({
-      customTitle: 'folder1',
-      parentFolderId: 0,
-      tags: [siteTags.BOOKMARK_FOLDER]
+      customTitle: 'folder1', parentFolderId: 0, tags: [siteTags.BOOKMARK_FOLDER]
     })
-    it('returns a list of the top sites', function () {
-      const stateWithSites = defaultAppState.set('sites',
-        Immutable.List().push(site1).push(site2).push(site3).push(folder1))
-      let expectedState = stateWithSites.setIn(['about', 'newtab', 'sites'],
-        Immutable.List().push(site3).push(site1).push(site2))
 
-      // account for timestamp being updated in results
-      const actualState = aboutNewTabState.setSites(stateWithSites)
-      const ts = assertTimeUpdated(actualState)
-      expectedState = expectedState.setIn(['about', 'newtab', 'updatedStamp'], ts)
+    describe('when fetching unpinned results', function () {
+      it('does not include bookmark folders', function () {
+        const stateWithSites = defaultAppState.set('sites',
+          Immutable.List().push(site1).push(folder1))
+        const expectedSites = Immutable.List().push(site1)
+        const actualState = aboutNewTabState.setSites(stateWithSites)
+        assert.deepEqual(actualState.getIn(['about', 'newtab', 'sites']).toJS(), expectedSites.toJS())
+      })
 
-      // checks:
-      // - sorts by count DESC
-      // - no folders included
-      assert.deepEqual(actualState.toJS(), expectedState.toJS())
+      it('sorts results by `count` DESC', function () {
+        const stateWithSites = defaultAppState.set('sites',
+          Immutable.List().push(site1).push(site2).push(site3).push(site4))
+        const expectedSites = Immutable.List().push(site3).push(site1).push(site2).push(site4)
+        const actualState = aboutNewTabState.setSites(stateWithSites)
+        assert.deepEqual(actualState.getIn(['about', 'newtab', 'sites']).toJS(), expectedSites.toJS())
+      })
+
+      it('sorts results by `lastAccessedTime` DESC if `count` is the same', function () {
+        const stateWithSites = defaultAppState.set('sites',
+          Immutable.List().push(site1).push(site3).push(site5))
+        const expectedSites = Immutable.List().push(site5).push(site3).push(site1)
+        const actualState = aboutNewTabState.setSites(stateWithSites)
+        assert.deepEqual(actualState.getIn(['about', 'newtab', 'sites']).toJS(), expectedSites.toJS())
+      })
+
+      it('only returns the last `maxSites` results', function () {
+        const maxSites = aboutNewTabState.maxSites
+        let tooManySites = Immutable.List()
+        for (let i = 0; i < maxSites + 1; i++) {
+          tooManySites = tooManySites.push(
+            site1.set('location', 'https://example' + i + '.com')
+              .set('title', 'sample ' + i)
+              .set('count', i))
+        }
+        const stateWithTooManySites = defaultAppState.set('sites', tooManySites)
+        const actualState = aboutNewTabState.setSites(stateWithTooManySites)
+        const actualSites = actualState.getIn(['about', 'newtab', 'sites'])
+        assert.equal(actualSites.size, maxSites)
+        assert.equal(actualSites.getIn([0, 'title']), 'sample ' + aboutNewTabState.maxSites)
+      })
+
+      it('does not include items marked as ignored', function () {
+        const ignoredSites = Immutable.List().push(site1).push(site3)
+        const stateWithIgnoredSites = defaultAppState
+          .set('sites', Immutable.List().push(site1).push(site2).push(site3).push(site4))
+          .setIn(['about', 'newtab', 'ignoredTopSites'], ignoredSites)
+        const expectedState = Immutable.List().push(site2).push(site4)
+        const actualState = aboutNewTabState.setSites(stateWithIgnoredSites)
+        assert.deepEqual(actualState.getIn(['about', 'newtab', 'sites']).toJS(), expectedState.toJS())
+      })
     })
-    it('respect position of pinned items', function () {
-      let stateWithPinnedSites = defaultAppState.set('sites',
-        Immutable.List().push(site1).push(site2).push(site3).push(folder1).push(site4))
 
+    it('respects position of pinned items when populating results', function () {
       const allPinned = Immutable.fromJS([null, null, site1, null, null, null, null, null, site4])
-
-      stateWithPinnedSites = stateWithPinnedSites.setIn(['about', 'newtab', 'pinnedTopSites'],
-        allPinned)
-      let expectedState = stateWithPinnedSites.setIn(['about', 'newtab', 'sites'],
-        Immutable.List().push(site3).push(site2).push(site1).push(site4))
-
-      // account for timestamp being updated in results
+      const stateWithPinnedSites = defaultAppState
+        .set('sites', Immutable.List().push(site1).push(site2).push(site3).push(folder1).push(site4))
+        .setIn(['about', 'newtab', 'pinnedTopSites'], allPinned)
+      const expectedSites = Immutable.List().push(site3).push(site2).push(site1).push(site4)
       const actualState = aboutNewTabState.setSites(stateWithPinnedSites)
-      const ts = assertTimeUpdated(actualState)
-      expectedState = expectedState.setIn(['about', 'newtab', 'updatedStamp'], ts)
-
       // checks:
       // - pinned item are in their expected order
       // - unpinned items fill the rest of the spots (starting w/ highest # visits first)
-      assert.deepEqual(actualState.toJS(), expectedState.toJS())
-    })
-    it('only returns `maxSites` results', function () {
-      const maxSites = aboutNewTabState.maxSites
-      let tooManySites = Immutable.List()
-      for (let i = 0; i < maxSites + 1; i++) {
-        tooManySites = tooManySites.push(
-            site1.set('location', 'https://example' + i + '.com').set('title', 'sample ' + i))
-      }
-      const stateWithTooManySites = defaultAppState.set('sites', tooManySites)
-      const actualState = aboutNewTabState.setSites(stateWithTooManySites)
-      assert.equal(actualState.getIn(['about', 'newtab', 'sites']).size, maxSites)
+      assert.deepEqual(actualState.getIn(['about', 'newtab', 'sites']).toJS(), expectedSites.toJS())
     })
 
-    it('does not include items marked as ignored', function () {
-      let stateWithIgnoredSites = defaultAppState.set('sites',
-        Immutable.List().push(site1).push(site2).push(site3).push(site4))
-
-      const ignoredSites = Immutable.List().push(site1).push(site3)
-      stateWithIgnoredSites = stateWithIgnoredSites.setIn(['about', 'newtab', 'ignoredTopSites'], ignoredSites)
-
-      let expectedState = stateWithIgnoredSites.setIn(['about', 'newtab', 'sites'],
-        Immutable.List().push(site2).push(site4))
-
-      // account for timestamp being updated in results
-      const actualState = aboutNewTabState.setSites(stateWithIgnoredSites)
-      const ts = assertTimeUpdated(actualState)
-      expectedState = expectedState.setIn(['about', 'newtab', 'updatedStamp'], ts)
-
-      // checks:
-      // - ignored items are not included
-      assert.deepEqual(actualState.toJS(), expectedState.toJS())
+    it('only includes one result for a domain (the one with the highest count)', function () {
+      const stateWithDuplicateDomains = defaultAppState.set('sites', Immutable.List()
+        .push(site1.set('location', 'https://example1.com/test').set('count', 12))
+        .push(site1.set('location', 'https://example1.com/about').set('count', 7)))
+      const expectedSites = Immutable.List().push(site1.set('location', 'https://example1.com/test').set('count', 12))
+      const actualState = aboutNewTabState.setSites(stateWithDuplicateDomains)
+      assert.deepEqual(actualState.getIn(['about', 'newtab', 'sites']).toJS(), expectedSites.toJS())
     })
-    // TODO(bsclifton): test de-duping
   })
 
   describe('mergeDetails', function () {
