7c3bd51ff05dd3e99c69d78424dd090dd6e0d168
Brian Clifton <brian@clifton.me>
2016-11-16 12:10:15 -0700

    Add new "empty new tab" option in preferences (for what new tab does)
    *includes tests* :)
    
    Fixes https://github.com/brave/browser-laptop/issues/5682
    
    Please note: choosing this option does NOT put focus in the URL bar. Is this OK?
    cc: @diracdeltas
    
    Auditors: @bradleyrichter, @bbondy
    
    Test Plan:
    1. Launch Brave and go to preferences
    2. Change "A new tab shows" to "empty new tab"
    3. Open a new tab; notice it's about:blank

---
+++ test/unit/lib/appUrlUtilTest.js
@@ -0,0 +1,94 @@
+/* global describe, it, afterEach */
+const mockery = require('mockery')
+const assert = require('assert')
+const settings = require('../../../js/constants/settings')
+const {newTabMode} = require('../../../app/common/constants/settingsEnums')
+let appUrlUtil = require('../../../js/lib/appUrlUtil')
+
+require('../braveUnit')
+
+describe('appUrlUtil test', function () {
+  const loadMocks = (settings) => {
+    mockery.enable({ warnOnReplace: false, warnOnUnregistered: false, useCleanCache: true })
+    mockery.registerMock('../settings', settings)
+  }
+
+  afterEach(function () {
+    mockery.disable()
+  })
+
+  describe('newFrameUrl', function () {
+    describe('when NEWTAB_MODE = HOMEPAGE', function () {
+      it('returns the configured home page', function () {
+        loadMocks({ getSetting: (settingKey, settingsCollection) => {
+          if (settingKey === settings.NEWTAB_MODE) {
+            return newTabMode.HOMEPAGE
+          } else if (settingKey === settings.HOMEPAGE) {
+            return 'https://brave.com/'
+          }
+        }})
+
+        const url = appUrlUtil.newFrameUrl()
+        assert.equal(url, 'https://brave.com/')
+      })
+      it('returns about:blank when home page is falsey', function () {
+        loadMocks({ getSetting: (settingKey, settingsCollection) => {
+          if (settingKey === settings.NEWTAB_MODE) {
+            return newTabMode.HOMEPAGE
+          }
+        }})
+
+        const url = appUrlUtil.newFrameUrl()
+        assert.equal(url, 'about:blank')
+      })
+    })
+
+    describe('when NEWTAB_MODE = DEFAULT_SEARCH_ENGINE', function () {
+      it('returns the default search engine URL', function () {
+        loadMocks({ getSetting: (settingKey, settingsCollection) => {
+          if (settingKey === settings.NEWTAB_MODE) {
+            return newTabMode.DEFAULT_SEARCH_ENGINE
+          } else if (settingKey === settings.DEFAULT_SEARCH_ENGINE) {
+            return 'DuckDuckGo'
+          }
+        }})
+
+        const url = appUrlUtil.newFrameUrl()
+        assert.equal(url, 'https://duckduckgo.com')
+      })
+    })
+
+    describe('when NEWTAB_MODE = EMPTY_NEW_TAB', function () {
+      it('returns about:blank', function () {
+        loadMocks({ getSetting: (settingKey, settingsCollection) => {
+          if (settingKey === settings.NEWTAB_MODE) {
+            return newTabMode.EMPTY_NEW_TAB
+          }
+        }})
+
+        const url = appUrlUtil.newFrameUrl()
+        assert.equal(url, 'about:blank')
+      })
+    })
+
+    describe('when NEWTAB_MODE = NEW_TAB_PAGE', function () {
+      it('returns about:newtab', function () {
+        loadMocks({ getSetting: (settingKey, settingsCollection) => {
+          if (settingKey === settings.NEWTAB_MODE) {
+            return newTabMode.NEW_TAB_PAGE
+          }
+        }})
+
+        const url = appUrlUtil.newFrameUrl()
+        assert.equal(url, 'about:newtab')
+      })
+    })
+
+    describe('when NEWTAB_MODE is null or has other values', function () {
+      it('returns about:newtab', function () {
+        const url = appUrlUtil.newFrameUrl()
+        assert.equal(url, 'about:newtab')
+      })
+    })
+  })
+})
