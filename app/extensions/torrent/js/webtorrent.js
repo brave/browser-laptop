eb19c308523b145a2962fc7d64fb44f1e1504d7f
bridiver <github@brianjohnson.cc>
2016-11-27 23:10:55 -0700

    fix webtorrent entry

--- app/extensions/torrent/js/webtorrent.js
+++ app/extensions/torrent/js/webtorrent.js
@@ -1,6 +1,22 @@
 (function () {
   let webtorrentEntryPage = 'gen/webtorrentPage.entry.js'
 
+  var queryString = window.location.search
+  var portMatch = queryString && queryString.match(/devServerPort=([0-9]+)/)
+  var devServerPort
+  if (portMatch) {
+    devServerPort = portMatch[1]
+  }
+
+  let webtorrentEntryPage = 'gen/webtorrentPage.entry.js'
+  if (devServerPort) {
+    var baseHref = 'http://localhost:' + devServerPort
+    webtorrentEntryPage = baseHref + '/' + webtorrentEntryPage
+    var baseNode = document.createElement('base')
+    baseNode.href = baseHref
+    document.getElementsByTagName('head')[0].appendChild(baseNode)
+  }
+
   chrome.ipc.on('language', (e, detail) => {
     document.l10n.requestLanguages([detail.langCode])
     document.getElementsByName('availableLanguages')[0].content = detail.languageCodes.join(', ')
