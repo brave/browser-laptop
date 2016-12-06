brave/browser-laptop

commit 46ee335509920c0a569a9da29d38bb3d3d4ee1e6

Make button styles consistent (part 3)
- Paddings of buttons on about:preferences were increased
- Removed unnecessary class

Auditors:

Test Plan:

js/about/preferences.js
@@ -927,7 +927,7 @@ class PaymentsTab extends ImmutableComponent {
     const onButtonClick = this.props.ledgerData.get('created')
       ? this.props.showOverlay.bind(this, 'addFunds')
       : (this.props.ledgerData.get('creating') ? () => {} : this.createWallet)
-    return <Button l10nId={buttonText} className='primaryButton addFunds' onClick={onButtonClick.bind(this)} disabled={this.props.ledgerData.get('creating')} />
+    return <Button l10nId={buttonText} className='primaryButton wideButton' onClick={onButtonClick.bind(this)} disabled={this.props.ledgerData.get('creating')} />
   }
 
   get paymentHistoryButton () {

less/about/preferences.less
@@ -655,9 +655,7 @@ div.nextPaymentSubmission {
       width: auto;
       min-width: 235px;
     }
-    span.browserButton.primaryButton.addFunds {
-      margin-right: 0;
-    }
+
     .fundsAmount {
       display: inline;
       margin: 0;

less/button.less
@@ -158,9 +158,10 @@ span.buttonSeparator {
 // for about:preferences
 .prefBody {
   .settingsList > .settingItem + button,
-  .settingItem > span + button {
+  .settingItem > span + button,
+  #paymentsContainer button:not(.close) {
     font-size: 0.9em;
-    padding: 5px 20px;
+    padding: 8px 20px;
   }
 
   .settingsList > .settingItem + button {
@@ -170,4 +171,11 @@ span.buttonSeparator {
   .settingItem > span + button {
     margin-top: 5px;
   }
+
+  #paymentsContainer {
+    .browserButton + .browserButton {
+      margin-left: 8px;
+    }
+  }
+
 }

commit 9bdb37ae13f4fe139861aa66f16b1086b8f4a5bb

Fix broken tests (selector related)
Fixes https://github.com/brave/browser-laptop/issues/5951
(was broke with https://github.com/brave/browser-laptop/commit/46ee335509920c0a569a9da29d38bb3d3d4ee1e6)

Auditors: @luixxiul

Test Plan:
1. Window 1: run `npm run watch-all`
2. Window 2: run `npm run uitest -- --grep="Payment Panel"
3. Watch tests pass

js/about/preferences.js
@@ -927,7 +927,7 @@ class PaymentsTab extends ImmutableComponent {
     const onButtonClick = this.props.ledgerData.get('created')
       ? this.props.showOverlay.bind(this, 'addFunds')
       : (this.props.ledgerData.get('creating') ? () => {} : this.createWallet)
-    return <Button l10nId={buttonText} className='primaryButton wideButton' onClick={onButtonClick.bind(this)} disabled={this.props.ledgerData.get('creating')} />
+    return <Button l10nId={buttonText} className='primaryButton wideButton addFunds' onClick={onButtonClick.bind(this)} disabled={this.props.ledgerData.get('creating')} />
   }
 
   get paymentHistoryButton () {

test/lib/selectors.js
@@ -60,7 +60,7 @@ module.exports = {
   addFundsButton: '.addFunds',
   advancedSettings: '.advancedSettings',
   fundsSelectBox: '#fundsSelectBox',
-  paymentsStatus: '#walletStatus',
+  paymentsStatus: '.walletStatus',
   ledgerTable: '#ledgerTable',
   bitcoinDashboard: '#bitcoinDashboard',
   modalCloseButton: 'button.close',