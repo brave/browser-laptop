a4af7606017dc4a04f2bbed7836eb16355ba51bc
Brian R. Bondy <netzen@gmail.com>
2016-12-21 08:58:52 -0500

    Use npm config for electron version when packaging
    Auditors: @bridiver
    
    Fix #6363

--- tools/lib/versionInfo.js
+++ tools/lib/versionInfo.js
@@ -2,4 +2,4 @@ var fs = require('fs')
 var pack = JSON.parse(fs.readFileSync('package.json', 'utf-8'))
 var electronPrebuiltPack = JSON.parse(fs.readFileSync('./node_modules/electron-prebuilt/package.json', 'utf-8'))
 module.exports.braveVersion = pack.version
-module.exports.electronVersion = electronPrebuiltPack.version
+module.exports.electronVersion = (process.env.npm_config_brave_electron_version || electronPrebuiltPack.version).replace(/-.*/, '')
