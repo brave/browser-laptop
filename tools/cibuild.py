0c7a64d6917b6e7e72d074b05e1d7488466418a9
Brian R. Bondy <netzen@gmail.com>
2016-12-29 18:34:20 -0500

    upgrade to brave/muon 2.0.8
    Auditors: @bridiver

--- tools/cibuild.py
+++ tools/cibuild.py
@@ -5,8 +5,8 @@ import subprocess
 import sys
 import os.path
 
-BRAVE_ELECTRON = '2.0.5'
-UPSTREAM_ELECTRON = '2.0.5'
+BRAVE_ELECTRON = '2.0.8'
+UPSTREAM_ELECTRON = '2.0.8'
 SOURCE_ROOT = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 TARGET_ARCH= os.environ['TARGET_ARCH'] if os.environ.has_key('TARGET_ARCH') else 'x64'
 os.environ['npm_config_arch'] = TARGET_ARCH
