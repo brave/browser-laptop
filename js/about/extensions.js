860eacc9dd299316f463fe4b1253e548ccd34321
Jonathan Sampson <jjdsampson@gmail.com>
2017-01-08 03:26:32 -0600

    Minor improvements to about:extensions

--- js/about/extensions.js
+++ js/about/extensions.js
@@ -27,6 +27,7 @@ class ExtensionItem extends ImmutableComponent {
   }
   render () {
     const icon = this.props.extension.getIn(['manifest', 'icons', '48'])
+    const permissions = this.props.extension.getIn(['manifest', 'permissions'])
     return <div role='listitem'
       disabled={!this.props.extension.get('enabled')}
       className='listItem'
@@ -46,7 +47,11 @@ class ExtensionItem extends ImmutableComponent {
         }
         <div className='extensionPath'><span data-l10n-id='extensionPathLabel' /> <span>{this.props.extension.get('base_path')}</span></div>
         <div className='extensionID'><span data-l10n-id='extensionIdLabel' /> <span>{this.props.extension.get('id')}</span></div>
-        <div className='extensionPermissions'><span data-l10n-id='extensionPermissionsLabel' /> <span>{this.props.extension.getIn(['manifest', 'permissions']).join(', ')}</span></div>
+        {
+          permissions
+          ? <div className='extensionPermissions'><span data-l10n-id='extensionPermissionsLabel' /> <span>{permissions.join(', ')}</span></div>
+          : null
+        }
       </div>
     </div>
   }
