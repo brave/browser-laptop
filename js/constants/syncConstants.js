daf8adb766555e6182bcf5fe2d978df300a63fe5
yan <yan@mit.edu>
2017-01-10 01:56:36 +0000

    sync site settings, add SYNC_CLEAR HISTORY
    also refactors reusable sync methods from app/sync.js to js/state/syncUtil.js
    
    Auditors: @ayumi
    
    Test Plan:
    1. start Brave
    2. go to bing.com
    3. disable shields
    4. you should see the console log a setting with 'shieldsUp: false', indicating the sync-client sent the record.
    5. enable shields
    6. same as 4 but with 'shieldsUp: true'

--- js/constants/syncConstants.js
+++ js/constants/syncConstants.js
@@ -8,7 +8,12 @@ const _ = null
 const syncConstants = {
   SYNC_ADD_SITE: _, /** @param {Immutable.Map} item */
   SYNC_UPDATE_SITE: _, /** @param {Immutable.Map} item */
-  SYNC_REMOVE_SITE: _  /** @param {Immutable.Map} item */
+  SYNC_REMOVE_SITE: _,  /** @param {Immutable.Map} item */
+  SYNC_CLEAR_HISTORY: _,
+  SYNC_CLEAR_SITE_SETTINGS: _,
+  SYNC_ADD_SITE_SETTING: _, /** @param {string} hostPattern, @param {Immutable.Map} item */
+  SYNC_UPDATE_SITE_SETTING: _, /** @param {string} hostPattern, @param {Immutable.Map} item */
+  SYNC_REMOVE_SITE_SETTING: _  /** @param {string} hostPattern, @param {Immutable.Map} item */
 }
 
 module.exports = mapValuesByKeys(syncConstants)
