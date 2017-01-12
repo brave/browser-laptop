brave/browser-laptop

commit 7abab4ba6235b12cafdc826a2a6202537c468562

Sync: Update appState with Sync bookmark records

js/state/siteUtil.js
@@ -290,9 +290,10 @@ module.exports.isMoveAllowed = (sites, sourceDetail, destinationDetail) => {
  * @param prepend Whether the destination detail should be prepended or not, not used if destinationIsParent is true
  * @param destinationIsParent Whether the item should be moved inside of the destinationDetail.
  * @param disallowReparent If set to true, parent folder will not be set
+ * @param {Function=} syncCallback
  * @return The new sites Immutable object
  */
-module.exports.moveSite = function (sites, sourceDetail, destinationDetail, prepend, destinationIsParent, disallowReparent) {
+module.exports.moveSite = function (sites, sourceDetail, destinationDetail, prepend, destinationIsParent, disallowReparent, syncCallback) {
   if (!module.exports.isMoveAllowed(sites, sourceDetail, destinationDetail)) {
     return sites
   }
@@ -323,6 +324,9 @@ module.exports.moveSite = function (sites, sourceDetail, destinationDetail, prep
     } else if (destinationSite.get('parentFolderId') !== sourceSite.get('parentFolderId')) {
       sourceSite = sourceSite.set('parentFolderId', destinationSite.get('parentFolderId'))
     }
+  }
+  if (syncCallback) {
+    syncCallback(sourceSite)
   }
   return sites.splice(newIndex, 0, sourceSite)
 }

js/state/syncUtil.js
@@ -4,6 +4,26 @@
 'use strict'
 
 const Immutable = require('immutable')
+const writeActions = require('../constants/sync/proto').actions
+
+const CATEGORY_MAP = {
+  bookmark: {
+    categoryName: 'BOOKMARKS',
+    settingName: 'SYNC_TYPE_BOOKMARK'
+  },
+  historySite: {
+    categoryName: 'HISTORY_SITES',
+    settingName: 'SYNC_TYPE_HISTORY'
+  },
+  siteSetting: {
+    categoryName: 'PREFERENCES',
+    settingName: 'SYNC_TYPE_SITE_SETTING'
+  },
+  device: {
+    categoryName: 'PREFERENCES'
+  }
+}
+module.exports.CATEGORY_MAP = CATEGORY_MAP
 
 const siteSettingDefaults = {
   hostPattern: '',
@@ -17,6 +37,134 @@ const siteSettingDefaults = {
   fingerprintingProtection: false,
   ledgerPayments: true,
   ledgerPaymentsShown: true
+}
+
+/**
+ * Apply a bookmark or historySite SyncRecord to the browser data store.
+ * @param {Object} record
+ * @param {Array.<string>} tags
+ */
+const applySiteRecord = (record, siteDetail, tag) => {
+  const appActions = require('../actions/appActions')
+  const objectId = new Immutable.List(record.objectId)
+  const category = CATEGORY_MAP[record.objectData].categoryName
+  const existingObject = this.getObjectById(objectId, category)
+  const existingObjectData = existingObject && existingObject[1]
+
+  switch (record.action) {
+    case writeActions.CREATE:
+      appActions.addSite(siteDetail, tag, undefined, undefined, true)
+      break
+    case writeActions.UPDATE:
+      appActions.addSite(siteDetail, tag, existingObjectData, null, true)
+      break
+    case writeActions.DELETE:
+      appActions.removeSite(siteDetail, tag, true)
+      break
+  }
+}
+
+/**
+ * Given a SyncRecord, apply it to the browser data store.
+ * @param {Object} record
+ */
+module.exports.applySyncRecord = (record) => {
+  const objectId = new Immutable.List(record.objectId)
+  switch (record.objectData) {
+    case 'bookmark':
+      const siteTags = require('../constants/siteTags')
+      const tag = record.bookmark.isFolder
+        ? siteTags.BOOKMARK_FOLDER
+        : siteTags.BOOKMARK
+      let bookmarkProps = Object.assign({}, record.bookmark, record.bookmark.site, {objectId})
+      delete bookmarkProps.bookmark
+      const bookmarkSite = new Immutable.Map(bookmarkProps)
+      applySiteRecord(record, bookmarkSite, tag)
+      break
+    case 'historySite':
+      const historyProps = Object.assign({}, record.historySite, {objectId})
+      const historySite = new Immutable.Map(historyProps)
+      applySiteRecord(record, historySite, null)
+      break
+    case 'siteSetting':
+      // TODO
+      break
+    case 'device':
+      // TODO
+      break
+    default:
+      throw new Error(`Invalid record objectData: ${record.objectData}`)
+  }
+}
+
+/**
+ * Given a category and SyncRecord, get an existing browser object.
+ * Used to respond to IPC GET_EXISTING_OBJECTS.
+ * @param {string} categoryName
+ * @param {Object} syncRecord
+ * @returns {Object=}
+ */
+module.exports.getExistingObject = (categoryName, syncRecord) => {
+  const AppStore = require('../stores/appStore')
+  const appState = AppStore.getState()
+  const objectId = new Immutable.List(syncRecord.objectId)
+  const existingObject = this.getObjectById(objectId, categoryName)
+  if (!existingObject) { return null }
+
+  const existingObjectData = existingObject[1].toJS()
+  let item
+  switch (categoryName) {
+    case 'BOOKMARKS':
+    case 'HISTORY_SITES':
+      item = this.createSiteData(existingObjectData)
+      break
+    case 'PREFERENCES':
+      const hostPattern = existingObject[0]
+      item = this.createSiteSettingsData(hostPattern, existingObjectData)
+      break
+    default:
+      throw new Error(`Invalid category: ${categoryName}`)
+  }
+  if (!item) {
+    throw new Error(`Can't create JS from existingObject! ${existingObjectData}`)
+  }
+  return {
+    action: writeActions.CREATE,
+    deviceId: appState.getIn(['sync', 'deviceId']),
+    objectData: item.name,
+    objectId: item.objectId,
+    [item.name]: item.value
+  }
+}
+
+/**
+ * Given an objectId and category, return the matching browser object.
+ * @param {Immutable.List} objectId
+ * @param {string} category
+ * @returns {Array} [<Array>, <Immutable.Map>] array is AppStore searchKeyPath e.g. ['sites', 10] for use with updateIn
+ */
+module.exports.getObjectById = (objectId, category) => {
+  if (!(objectId instanceof Immutable.List)) {
+    throw new Error('objectId must be an Immutable.List')
+  }
+
+  const AppStore = require('../stores/appStore')
+  const appState = AppStore.getState()
+  switch (category) {
+    case 'BOOKMARKS':
+    case 'HISTORY_SITES':
+      return appState.get('sites').findEntry((site, index) => {
+        const itemObjectId = site.get('objectId')
+        return (itemObjectId && itemObjectId.equals(objectId))
+      })
+    case 'PREFERENCES':
+      return appState.get('siteSettings').findEntry((siteSetting, hostPattern) => {
+        const itemObjectId = siteSetting.get('objectId')
+        return (itemObjectId && itemObjectId.equals(objectId))
+      })
+    default:
+      throw new Error(`Invalid object category: ${category}`)
+  }
 }
 
 /**
@@ -119,6 +267,7 @@ module.exports.createSiteData = (site, siteIndex) => {
       value: siteData
     }
   }
+  console.log(`Warning: Can't create site data: ${site}`)
 }
 
 /**
@@ -154,4 +303,30 @@ module.exports.createSiteSettingsData = (hostPattern, setting) => {
     objectId: setting.objectId || module.exports.newObjectId(['siteSettings', hostPattern]),
     value
   }
+}
+
+/**
+ * Deep modify object Uint8Array into Array.<Number> because IPC can't send
+ * Uint8Array (see brave/sync issue #17). Returns a copy.
+ */
+const deepArrayify = (sourceObject) => {
+  let object = Object.assign({}, sourceObject)
+  const has = Object.prototype.hasOwnProperty.bind(object)
+  for (let k in object) {
+    if (!has(k) || object[k] instanceof Array) { continue }
+    if (object[k] instanceof Uint8Array) {
+      object[k] = Array.from(object[k])
+    } else if (typeof object[k] === 'object') {
+      object[k] = deepArrayify(Object.assign({}, object[k]))
+    }
+  }
+  return object
+}
+
+/**
+ * @param {Object}
+ * @returns {Object}
+ */
+module.exports.ipcSafeObject = (object) => {
+  return deepArrayify(object)
 }

js/stores/appStore.js
@@ -485,12 +485,13 @@ const handleAppAction = (action) => {
       break
     case appConstants.APP_ADD_SITE:
       const oldSiteSize = appState.get('sites').size
+      const addSiteSyncCallback = action.skipSync ? undefined : syncActions.updateSite
       if (action.siteDetail.constructor === Immutable.List) {
         action.siteDetail.forEach((s) => {
-          appState = appState.set('sites', siteUtil.addSite(appState.get('sites'), s, action.tag, undefined, syncActions.updateSite))
+          appState = appState.set('sites', siteUtil.addSite(appState.get('sites'), s, action.tag, undefined, addSiteSyncCallback))
         })
       } else {
-        appState = appState.set('sites', siteUtil.addSite(appState.get('sites'), action.siteDetail, action.tag, action.originalSiteDetail, syncActions.updateSite))
+        appState = appState.set('sites', siteUtil.addSite(appState.get('sites'), action.siteDetail, action.tag, action.originalSiteDetail, addSiteSyncCallback))
       }
       if (action.destinationDetail) {
         appState = appState.set('sites', siteUtil.moveSite(appState.get('sites'), action.siteDetail, action.destinationDetail, false, false, true))
@@ -503,12 +504,13 @@ const handleAppAction = (action) => {
       appState = aboutHistoryState.setHistory(appState, action)
       break
     case appConstants.APP_REMOVE_SITE:
-      appState = appState.set('sites', siteUtil.removeSite(appState.get('sites'), action.siteDetail, action.tag, syncActions.removeSite))
+      const removeSiteSyncCallback = action.skipSync ? undefined : syncActions.removeSite
+      appState = appState.set('sites', siteUtil.removeSite(appState.get('sites'), action.siteDetail, action.tag, removeSiteSyncCallback))
       appState = aboutNewTabState.setSites(appState, action)
       appState = aboutHistoryState.setHistory(appState, action)
       break
     case appConstants.APP_MOVE_SITE:
-      appState = appState.set('sites', siteUtil.moveSite(appState.get('sites'), action.sourceDetail, action.destinationDetail, action.prepend, action.destinationIsParent, false))
+      appState = appState.set('sites', siteUtil.moveSite(appState.get('sites'), action.sourceDetail, action.destinationDetail, action.prepend, action.destinationIsParent, false, syncActions.updateSite))
       break
     case appConstants.APP_CLEAR_HISTORY:
       appState = appState.set('sites',

app/sync.js
@@ -19,23 +19,12 @@ const syncUtil = require('../js/state/syncUtil')
 const getSetting = require('../js/settings').getSetting
 const settings = require('../js/constants/settings')
 
-const categoryNames = Object.keys(categories)
-const categoryMap = {
-  bookmark: {
-    categoryName: 'BOOKMARKS',
-    settingName: 'SYNC_TYPE_BOOKMARK'
-  },
-  historySite: {
-    categoryName: 'HISTORY_SITES',
-    settingName: 'SYNC_TYPE_HISTORY'
-  },
-  siteSetting: {
-    categoryName: 'PREFERENCES',
-    settingName: 'SYNC_TYPE_SITE_SETTING'
-  },
-  device: {
-    categoryName: 'PREFERENCES'
-  }
+const CATEGORY_MAP = syncUtil.CATEGORY_MAP
+const CATEGORY_NAMES = Object.keys(categories)
+
+const log = (message) => {
+  if (!config.debug) { return }
+  console.log(`sync ${new Date().getTime()}:`, message)
 }
 
 let deviceId = null /** @type {Array|null} */
@@ -55,7 +44,7 @@ const sendSyncRecords = (sender, action, data) => {
   if (!data || !data.length) {
     return
   }
-  const category = categoryMap[data[0].name]
+  const category = CATEGORY_MAP[data[0].name]
   if (!category ||
     (category.settingName && !getSetting(settings[category.settingName]))) {
     return
@@ -84,7 +73,7 @@ const doAction = (sender, action) => {
   }
   // Only accept items who have an objectId set already
   if (!action.item.get('objectId')) {
-    console.log('Missing object ID!', action.item.toJS())
+    log(`Missing object ID! ${action.item.toJS()}`)
     return
   }
   switch (action.actionType) {
@@ -101,7 +90,7 @@ const doAction = (sender, action) => {
         [syncUtil.createSiteData(action.item.toJS())])
       break
     case syncConstants.SYNC_CLEAR_HISTORY:
-      sender.send(messages.DELETE_SYNC_CATEGORY, categoryMap.historySite.categoryName)
+      sender.send(messages.DELETE_SYNC_CATEGORY, CATEGORY_MAP.historySite.categoryName)
       break
     case syncConstants.SYNC_ADD_SITE_SETTING:
       if (syncUtil.isSyncable('siteSetting', action.item)) {
@@ -173,18 +162,30 @@ module.exports.onSyncReady = (isFirstRun, e) => {
     if (getSetting(settings.SYNC_ENABLED) !== true) {
       return
     }
-    if (categoryNames.includes(categoryName) || !records || !records.length) {
+    log(`getting existing objects for ${records.length} ${categoryName}`)
+    if (!CATEGORY_NAMES.includes(categoryName) || !records || !records.length) {
       return
     }
-    // TODO (Ayumi): get existing objects with objectId, send
-    // RESOLVE_SYNC_RECORDS, handle RESOLVED_SYNC_RECORDS
+    const recordsAndExistingObjects = records.map((record) => {
+      const safeRecord = syncUtil.ipcSafeObject(record)
+      const existingObject = syncUtil.getExistingObject(categoryName, record)
+      return [safeRecord, existingObject]
+    })
+    e.sender.send(messages.RESOLVE_SYNC_RECORDS, categoryName, recordsAndExistingObjects)
+  })
+  ipcMain.on(messages.RESOLVED_SYNC_RECORDS, (event, categoryName, records) => {
+    if (!records || !records.length) {
+      return
+    }
+    log(`applying resolved ${records.length} ${categoryName}.`)
+    for (let record of records) { syncUtil.applySyncRecord(record) }
   })
   // Periodically poll for new records
   let startAt = appState.getIn(['sync', 'lastFetchTimestamp']) || 0
   const poll = () => {
     let categoryNames = []
-    for (let type in categoryMap) {
-      let item = categoryMap[type]
+    for (let type in CATEGORY_MAP) {
+      let item = CATEGORY_MAP[type]
       if (item.settingName && getSetting(settings[item.settingName]) === true) {
         categoryNames.push(item.categoryName)
       }
@@ -216,7 +217,7 @@ module.exports.init = function (initialState) {
   ipcMain.on(messages.SYNC_READY, module.exports.onSyncReady.bind(null,
     !initialState.seed && !initialState.deviceId))
   ipcMain.on(messages.SYNC_DEBUG, (e, msg) => {
-    console.log('sync-client:', msg)
+    log(msg)
   })
 }
 

js/actions/appActions.js
@@ -116,14 +116,16 @@ const appActions = {
    * @param {string} originalSiteDetail - If specified, the original site detail to edit / overwrite.
    * @param {boolean} destinationIsParent - Whether or not the destinationDetail should be considered the new parent.
    *   The details of the old entries will be modified if this is set, otherwise only the tag will be added.
+   * @param {boolean} skipSync - Set true if a site isn't eligible for Sync (e.g. if addSite was triggered by Sync)
    */
-  addSite: function (siteDetail, tag, originalSiteDetail, destinationDetail) {
+  addSite: function (siteDetail, tag, originalSiteDetail, destinationDetail, skipSync) {
     AppDispatcher.dispatch({
       actionType: appConstants.APP_ADD_SITE,
       siteDetail,
       tag,
       originalSiteDetail,
-      destinationDetail
+      destinationDetail,
+      skipSync
     })
   },
 
@@ -140,12 +142,14 @@ const appActions = {
    * Removes a site from the site list
    * @param {Object} siteDetail - Properties of the site in question
    * @param {string} tag - A tag to associate with the site. e.g. bookmarks.
+   * @param {boolean} skipSync - Set true if a site isn't eligible for Sync (e.g. if this removal was triggered by Sync)
    */
-  removeSite: function (siteDetail, tag) {
+  removeSite: function (siteDetail, tag, skipSync) {
     AppDispatcher.dispatch({
       actionType: appConstants.APP_REMOVE_SITE,
       siteDetail,
-      tag
+      tag,
+      skipSync
     })
   },
 

test/unit/state/syncUtilTest.js
@@ -1,0 +1,29 @@
+/* global describe, it */
+
+const syncUtil = require('../../../js/state/syncUtil')
+const assert = require('assert')
+
+describe('syncUtil', () => {
+  describe('ipcSafeObject()', () => {
+    it('does nothing with objects already safe', () => {
+      const object = {chill: true, time: 42, nest: {egg: 'tree'}}
+      assert.deepEqual(syncUtil.ipcSafeObject(object), object)
+    })
+
+    it('does nothing with Arrays', () => {
+      const object = {arr: [1, 2, 3]}
+      assert.deepEqual(syncUtil.ipcSafeObject(object), object)
+      const deepObject = {deep: {arr: [1, 2, 3]}}
+      assert.deepEqual(syncUtil.ipcSafeObject(deepObject), deepObject)
+    })
+
+    it('converts Uint8Array to Array', () => {
+      const object = {chill: true, arr: new Uint8Array([1, 2, 3])}
+      const expected = {chill: true, arr: [1, 2, 3]}
+      assert.deepEqual(syncUtil.ipcSafeObject(object), expected)
+      const deepObject = {chill: true, deep: {arr: new Uint8Array([1, 2, 3])}}
+      const deepExpected = {chill: true, deep: {arr: [1, 2, 3]}}
+      assert.deepEqual(syncUtil.ipcSafeObject(deepObject), deepExpected)
+    })
+  })
+})

commit 7ee10721d99894b4c39e27961fcd0a7800d30565

move setObjectId from syncUtil to siteUtil to avoid dependency problems
7abab4ba6235b12cafdc826a2a6202537c468562 caused node to throw 'module not found' errors when siteUtil (which depended on syncUtil) was imported into renderer components, i think. This avoids the problem by not having siteUtil depend on syncUtil.

Auditors: @ayumi

Test Plan:
1. npm start
2. you should not see any 'module not found' errors in the terminal

js/state/siteUtil.js
@@ -8,7 +8,6 @@ const siteTags = require('../constants/siteTags')
 const settings = require('../constants/settings')
 const getSetting = require('../settings').getSetting
 const UrlUtil = require('../lib/urlutil')
-const syncUtil = require('./syncUtil')
 const urlParse = require('url').parse
 
 const isBookmark = (tags) => {
@@ -189,7 +188,7 @@ module.exports.addSite = function (sites, siteDetail, tag, originalSiteDetail, s
 
   let site = mergeSiteDetails(oldSite, siteDetail, tag, folderId)
   if (syncCallback) {
-    site = syncUtil.setObjectId(site)
+    site = module.exports.setObjectId(site)
     syncCallback(site)
   }
   if (index === -1) {
@@ -584,4 +583,20 @@ module.exports.getOrigin = function (location) {
   } else {
     return null
   }
+}
+
+/**
+ * Sets object id on a state entry.
+ * @param {Immutable.Map} item
+ * @returns {Immutable.map}
+ */
+module.exports.setObjectId = (item) => {
+  if (!item || !item.toJS) {
+    return
+  }
+  if (item.get('objectId')) {
+    return item
+  }
+  const crypto = require('crypto')
+  return item.set('objectId', new Immutable.List(crypto.randomBytes(16)))
 }

js/state/syncUtil.js
@@ -168,22 +168,6 @@ module.exports.getObjectById = (objectId, category) => {
 }
 
 /**
- * Sets object id on a state entry.
- * @param {Immutable.Map} item
- * @returns {Immutable.map}
- */
-module.exports.setObjectId = (item) => {
-  if (!item || !item.toJS) {
-    return
-  }
-  if (item.get('objectId')) {
-    return item
-  }
-  const crypto = require('crypto')
-  return item.set('objectId', new Immutable.List(crypto.randomBytes(16)))
-}
-
-/**
  * Gets current time in seconds
  */
 module.exports.now = () => {

js/stores/appStore.js
@@ -10,7 +10,6 @@ const AppDispatcher = require('../dispatcher/appDispatcher')
 const appConfig = require('../constants/appConfig')
 const settings = require('../constants/settings')
 const siteUtil = require('../state/siteUtil')
-const syncUtil = require('../state/syncUtil')
 const siteSettings = require('../state/siteSettings')
 const appUrlUtil = require('../lib/appUrlUtil')
 const electron = require('electron')
@@ -596,7 +595,7 @@ const handleAppAction = (action) => {
         let propertyName = action.temporary ? 'temporarySiteSettings' : 'siteSettings'
         let newSiteSettings = siteSettings.mergeSiteSetting(appState.get(propertyName), action.hostPattern, action.key, action.value)
         if (!action.temporary) {
-          let syncObject = syncUtil.setObjectId(newSiteSettings.get(action.hostPattern))
+          let syncObject = siteUtil.setObjectId(newSiteSettings.get(action.hostPattern))
           syncActions.updateSiteSetting(action.hostPattern, syncObject)
           newSiteSettings = newSiteSettings.set(action.hostPattern, syncObject)
         }
@@ -609,7 +608,7 @@ const handleAppAction = (action) => {
         let newSiteSettings = siteSettings.removeSiteSetting(appState.get(propertyName),
           action.hostPattern, action.key)
         if (!action.temporary) {
-          let syncObject = syncUtil.setObjectId(newSiteSettings.get(action.hostPattern))
+          let syncObject = siteUtil.setObjectId(newSiteSettings.get(action.hostPattern))
           syncActions.updateSiteSetting(action.hostPattern, syncObject)
           newSiteSettings = newSiteSettings.set(action.hostPattern, syncObject)
         }