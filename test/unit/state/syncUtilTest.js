8be2e0211e5b54881eb07f843389046344c5bbed
ａｙｕｍｉ  ｙｕ <ayumi@ayumiyu.com>
2017-01-18 18:49:36 -0800

    Fix syncing pinned sites
    Auditors: @diracdeltas

--- test/unit/state/syncUtilTest.js
+++ test/unit/state/syncUtilTest.js
@@ -4,6 +4,56 @@ const syncUtil = require('../../../js/state/syncUtil')
 const assert = require('assert')
 
 describe('syncUtil', () => {
+  describe('createSiteData()', () => {
+    const objectId = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+    const site = {
+      favicon: 'https://calendar.google.com/googlecalendar/images/favicon_v2014_18.ico',
+      lastAccessedTime: 1484792353816,
+      location: 'https://calendar.google.com/calendar/render#main_7',
+      objectId,
+      partitionNumber: 0,
+      tags: [],
+      themeColor: 'rgb(255, 255, 255)',
+      title: 'Google Calendar'
+    }
+    const expectedSite = {
+      name: 'historySite',
+      objectId,
+      value: {
+        location: 'https://calendar.google.com/calendar/render#main_7',
+        title: 'Google Calendar',
+        customTitle: '',
+        lastAccessedTime: 1484792353816,
+        creationTime: 0
+      }
+    }
+
+    it('history sites', () => {
+      assert.deepEqual(syncUtil.createSiteData(site), expectedSite)
+    })
+
+    // TODO: Not explicitly supported; falls back to history item
+    it('pinned sites', () => {
+      const pinnedSite = Object.assign({}, site, {tags: ['pinned']})
+      assert.deepEqual(syncUtil.createSiteData(pinnedSite), expectedSite)
+    })
+
+    it('bookmarks', () => {
+      const bookmark = Object.assign({}, site, {tags: ['bookmark']})
+      const expectedBookmark = {
+        name: 'bookmark',
+        objectId,
+        value: {
+          site: expectedSite.value,
+          isFolder: false,
+          folderId: 0,
+          parentFolderId: 0
+        }
+      }
+      assert.deepEqual(syncUtil.createSiteData(bookmark), expectedBookmark)
+    })
+  })
+
   describe('ipcSafeObject()', () => {
     it('does nothing with objects already safe', () => {
       const object = {chill: true, time: 42, nest: {egg: 'tree'}}
