8b8e2db654b8236d4c6e9ac4bd6417d8c5186c69
Brian R. Bondy <netzen@gmail.com>
2017-01-20 17:38:38 -0500

    Revert "Stop using memoize from underscore"
    This reverts commit 9c55eca41d23d9013324717fcef5ac82f2dfd0e2.

--- js/flash.js
+++ js/flash.js
@@ -11,21 +11,18 @@ const {getOrigin} = require('./state/siteUtil')
 const locale = require('../app/locale')
 const messages = require('./constants/messages')
 const settings = require('./constants/settings')
+const {memoize} = require('underscore')
 
 // set to true if the flash install check has succeeded
 let flashInstalled = false
 
-let pluginPath
-const getPepperFlashPath = () => {
-  if (pluginPath) {
-    return pluginPath
-  }
+const getPepperFlashPath = memoize(() => {
   if (['darwin', 'win32'].includes(process.platform)) {
     return app.getPath('pepperFlashSystemPlugin')
   }
   const basePath = '/usr/lib'
   const plugin = 'libpepflashplayer.so'
-  pluginPath = path.resolve(basePath, 'pepperflashplugin-nonfree', plugin)
+  let pluginPath = path.resolve(basePath, 'pepperflashplugin-nonfree', plugin)
   try {
     fs.statSync(pluginPath)
   } catch (e) {
@@ -38,7 +35,7 @@ const getPepperFlashPath = () => {
     }
   }
   return pluginPath
-}
+})
 
 module.exports.getFlashResourceId = () => {
   return path.basename(getPepperFlashPath())
