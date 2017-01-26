84d0eb5805176c601981d36ff887c00d313f37fa
Brian R. Bondy <netzen@gmail.com>
2017-01-26 15:41:23 -0500

    Use more up to date url when avail
    Auditors: @bsclifton
    
    This goes along with last commit, it's better to use the url provided than what is in our props in case it wasn't updated yet

--- js/components/frame.js
+++ js/components/frame.js
@@ -820,7 +820,7 @@ class Frame extends ImmutableComponent {
         return
       }
       windowActions.onWebviewLoadEnd(this.frame, url)
-      const parsedUrl = urlParse(this.props.location)
+      const parsedUrl = urlParse(url)
       if (!this.allowRunningWidevinePlugin()) {
         this.showWidevineNotification(this.props.location, this.origin, () => {
         }, () => {
@@ -836,8 +836,8 @@ class Frame extends ImmutableComponent {
         appActions.addSite(siteUtil.getDetailFromFrame(this.frame))
       }
 
-      if (this.props.location.startsWith(pdfjsOrigin)) {
-        let displayLocation = UrlUtil.getLocationIfPDF(this.props.location)
+      if (url.startsWith(pdfjsOrigin)) {
+        let displayLocation = UrlUtil.getLocationIfPDF(url)
         windowActions.setSecurityState(this.frame, {
           secure: urlParse(displayLocation).protocol === 'https:',
           runInsecureContent: false
