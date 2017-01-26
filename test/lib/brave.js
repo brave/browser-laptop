23beee3511899af7033a1be7f94a3dcfd9350325
Brian R. Bondy <netzen@gmail.com>
2017-01-26 18:07:34 -0500

    Clear search engine icon on page nav
    Auditors: @bsclifton
    
    Fix #6841

--- test/lib/brave.js
+++ test/lib/brave.js
@@ -398,7 +398,7 @@ var exports = {
             } else {
               ret = val === input
             }
-            logVerbose('Current val: ' + val)
+            logVerbose('Current val (in quotes): "' + val + '"')
             logVerbose('waitForInputText("' + selector + '", "' + input + '") => ' + ret)
             return ret
           })
