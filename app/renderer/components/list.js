80bb03b4a53b4d55f36865560a80191bc1b2a515
Suguru Hirahara <luixxiul@gmail.com>
2017-02-20 11:20:21 +0900

    Refactor download.js with Aphrodite
    including:
    
    - Created <List> and <DownloadList> in list.js
    - Copied itemList.less to commonStyles.js except '&.selected'
    - Added aboutPageSectionMargin to global.js
    
    Closes #7255
    Addresses #7318
    
    Auditors:
    
    Test Plan:
    1. Open about:download
    2. Download a file
    3. Make sure the styles are not broken

---
+++ app/renderer/components/list.js
@@ -0,0 +1,36 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+const React = require('react')
+const ImmutableComponent = require('../../../js/components/immutableComponent')
+const {StyleSheet, css} = require('aphrodite/no-important')
+const globalStyles = require('./styles/global')
+
+class List extends ImmutableComponent {
+  render () {
+    const className = css(
+      this.props['data-isDownload'] && styles.isDownload
+    )
+
+    return <div className={className} {...this.props} />
+  }
+}
+
+class DownloadList extends ImmutableComponent {
+  render () {
+    return <List data-isDownload='true' {...this.props} />
+  }
+}
+
+const styles = StyleSheet.create({
+  isDownload: {
+    marginTop: globalStyles.spacing.aboutPageSectionMargin,
+    overflow: 'hidden'
+  }
+})
+
+module.exports = {
+  List,
+  DownloadList
+}
