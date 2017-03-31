brave/browser-laptop

commit 40fc1079258fe7e8cf41bcdee663c2b7b6c84e59

Fix issue @alexwykoff found while testing 0.14.0 RC2. Fixes are reinforced by unit tests
Auditors: @alexwykoff, @NejcZdovc

js/contextMenus.js
@@ -39,6 +39,7 @@ const {bookmarksToolbarMode} = require('../app/common/constants/settingsEnums')
 const extensionState = require('../app/common/state/extensionState')
 const extensionActions = require('../app/common/actions/extensionActions')
 const appStore = require('./stores/appStoreRenderer')
+const {makeImmutable} = require('../app/common/state/immutableUtil')
 
 const isDarwin = process.platform === 'darwin'
 const isLinux = process.platform === 'linux'
@@ -951,12 +952,15 @@ function addLinkMenu (link, frame) {
 function mainTemplateInit (nodeProps, frame, tab) {
   let template = []
 
+  nodeProps = nodeProps || {}
+  frame = makeImmutable(frame || {})
+
   const isLink = nodeProps.linkURL && nodeProps.linkURL !== ''
   const isImage = nodeProps.mediaType === 'image'
   const isVideo = nodeProps.mediaType === 'video'
   const isAudio = nodeProps.mediaType === 'audio'
   const isInputField = nodeProps.isEditable || nodeProps.inputFieldType !== 'none'
-  const isTextSelected = nodeProps.selectionText.length > 0
+  const isTextSelected = nodeProps.selectionText && nodeProps.selectionText.length > 0
   const isAboutPage = aboutUrls.has(frame.get('location'))
 
   if (isLink) {
@@ -1022,7 +1026,10 @@ function mainTemplateInit (nodeProps, frame, tab) {
       }
     }
 
-    const editableItems = getEditableItems(nodeProps.selectionText, nodeProps.editFlags, true)
+    const editableItems = nodeProps.selectionText
+      ? getEditableItems(nodeProps.selectionText, nodeProps.editFlags, true)
+      : []
+
     template.push(...misspelledSuggestions, {
       label: locale.translation('undo'),
       accelerator: 'CmdOrCtrl+Z',

test/unit/js/contextMenusTest.js
@@ -1,0 +1,61 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+/* global describe, before, after, it */
+
+const mockery = require('mockery')
+const assert = require('assert')
+const sinon = require('sinon')
+let fakeElectronMenu
+let contextMenus
+require('../braveUnit')
+
+describe('Context menu module unit tests', function () {
+  const fakeLocale = {
+    translation: (token) => { return token }
+  }
+
+  before(function () {
+    mockery.enable({
+      warnOnReplace: false,
+      warnOnUnregistered: false,
+      useCleanCache: true
+    })
+    mockery.registerMock('electron', require('../lib/fakeElectron'))
+    mockery.registerMock('../js/l10n', fakeLocale)
+    contextMenus = require('../../../js/contextMenus')
+    fakeElectronMenu = require('../lib/fakeElectronMenu')
+  })
+
+  after(function () {
+    mockery.disable()
+  })
+
+  describe('onMainContextMenu', function () {
+    describe('when calling mainTemplateInit', function () {
+      let menuPopupSpy
+      let menuDestroySpy
+
+      before(function () {
+        menuPopupSpy = sinon.spy(fakeElectronMenu, 'popup')
+        menuDestroySpy = sinon.spy(fakeElectronMenu, 'destroy')
+      })
+
+      after(function () {
+        menuPopupSpy.restore()
+      })
+
+      it('calls menu.popup', function () {
+        menuPopupSpy.reset()
+        contextMenus.onMainContextMenu()
+        assert.equal(menuPopupSpy.calledOnce, true)
+      })
+
+      it('calls menu.destroy', function () {
+        menuDestroySpy.reset()
+        contextMenus.onMainContextMenu()
+        assert.equal(menuDestroySpy.calledOnce, true)
+      })
+    })
+  })
+})

test/unit/lib/fakeElectron.js
@@ -23,6 +23,11 @@ const fakeElectron = {
         isMaximized: () => false,
         webContents: {}
       }
+    },
+    Menu: {
+      buildFromTemplate: (template) => {
+        return require('./fakeElectronMenu')
+      }
     }
   },
   app: {

app/common/state/extensionState.js
@@ -18,9 +18,13 @@ const extensionState = {
   },
 
   getEnabledExtensions: (state) => {
-    return state.get('extensions').filter((installInfo, extensionId) => {
-      return installInfo.get('enabled') === true
-    })
+    const extensions = state.get('extensions')
+    if (extensions) {
+      return extensions.filter((installInfo, extensionId) => {
+        return installInfo.get('enabled') === true
+      })
+    }
+    return Immutable.fromJS([])
   },
 
   getExtensionById: (state, extensionId) => {

test/unit/lib/fakeElectronMenu.js
@@ -1,0 +1,6 @@
+const fakeElectronMenu = {
+  popup: () => {},
+  destroy: () => {}
+}
+
+module.exports = fakeElectronMenu

commit e016cc2e00f7a3cc1b4e28737a2b253a02ceb6d5

Call show paste options in context menu (if clipboard has contents)
This was a regression that I unintentionally introduced with https://github.com/brave/browser-laptop/commit/40fc1079258fe7e8cf41bcdee663c2b7b6c84e59

Fixes https://github.com/brave/browser-laptop/issues/8000

Auditors: @lukemulks, @alexwykoff

Test Plan:
1. Visit a page with a text box (ex: create an issue on browser-laptop)
2. Type text into a textbox
3. Use the mouse to select and cut the text
4. Right click to open a context menu inside the textbox
5. You should now see paste again

js/contextMenus.js
@@ -650,7 +650,7 @@ function getMisspelledSuggestions (selection, isMisspelled, suggestions) {
 }
 
 function getEditableItems (selection, editFlags, hasFormat) {
-  const hasSelection = selection.length > 0
+  const hasSelection = selection && selection.length > 0
   const hasClipboard = clipboard.readText().length > 0
   const template = []
 
@@ -1026,10 +1026,7 @@ function mainTemplateInit (nodeProps, frame, tab) {
       }
     }
 
-    const editableItems = nodeProps.selectionText
-      ? getEditableItems(nodeProps.selectionText, nodeProps.editFlags, true)
-      : []
-
+    const editableItems = getEditableItems(nodeProps.selectionText, nodeProps.editFlags, true)
     template.push(...misspelledSuggestions, {
       label: locale.translation('undo'),
       accelerator: 'CmdOrCtrl+Z',

test/unit/js/contextMenusTest.js
@@ -6,6 +6,7 @@
 const mockery = require('mockery')
 const assert = require('assert')
 const sinon = require('sinon')
+const fakeElectron = require('../lib/fakeElectron')
 let fakeElectronMenu
 let contextMenus
 require('../braveUnit')
@@ -21,7 +22,7 @@ describe('Context menu module unit tests', function () {
       warnOnUnregistered: false,
       useCleanCache: true
     })
-    mockery.registerMock('electron', require('../lib/fakeElectron'))
+    mockery.registerMock('electron', fakeElectron)
     mockery.registerMock('../js/l10n', fakeLocale)
     contextMenus = require('../../../js/contextMenus')
     fakeElectronMenu = require('../lib/fakeElectronMenu')
@@ -33,16 +34,35 @@ describe('Context menu module unit tests', function () {
 
   describe('onMainContextMenu', function () {
     describe('when calling mainTemplateInit', function () {
+      let clipboardReadTextSpy
+      let menuBuildFromTemplateSpy
       let menuPopupSpy
       let menuDestroySpy
 
       before(function () {
+        clipboardReadTextSpy = sinon.spy(fakeElectron.remote.clipboard, 'readText')
+        menuBuildFromTemplateSpy = sinon.spy(fakeElectron.remote.Menu, 'buildFromTemplate')
         menuPopupSpy = sinon.spy(fakeElectronMenu, 'popup')
         menuDestroySpy = sinon.spy(fakeElectronMenu, 'destroy')
       })
 
       after(function () {
+        clipboardReadTextSpy.restore()
+        menuBuildFromTemplateSpy.restore()
         menuPopupSpy.restore()
+        menuDestroySpy.restore()
+      })
+
+      it('calls clipboard.readText', function () {
+        clipboardReadTextSpy.reset()
+        contextMenus.onMainContextMenu()
+        assert.equal(clipboardReadTextSpy.calledOnce, true)
+      })
+
+      it('calls Menu.buildFromTemplate', function () {
+        menuBuildFromTemplateSpy.reset()
+        contextMenus.onMainContextMenu()
+        assert.equal(menuBuildFromTemplateSpy.calledOnce, true)
       })
 
       it('calls menu.popup', function () {

test/unit/lib/fakeElectron.js
@@ -15,6 +15,9 @@ const fakeElectron = {
       on: function () {
       }
     },
+    clipboard: {
+      readText: function () { return '' }
+    },
     getCurrentWindow: function () {
       return {
         on: () => {},