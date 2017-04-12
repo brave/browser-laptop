e169b32ee0b5df9823adfc5b4da7a2be5ce479d4
bridiver <github@brianjohnson.cc>
2017-04-12 16:03:10 -0700

    fix history page auditors @bbondy

--- app/browser/tabs.js
+++ app/browser/tabs.js
@@ -155,7 +155,7 @@ const updateAboutDetails = (tab, tabValue) => {
     })
   } else if (location === 'about:history' && history) {
     appActions.populateHistory()
-    tab.send(messages.HISTORY_UPDATED, aboutHistoryState.toJS())
+    tab.send(messages.HISTORY_UPDATED, history.toJS())
     tab.send(messages.SETTINGS_UPDATED, appSettings.toJS())
   } else if (location === 'about:extensions' && extensions) {
     tab.send(messages.EXTENSIONS_UPDATED, extensionsValue.toJS())
