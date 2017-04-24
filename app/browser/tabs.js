brave/browser-laptop

commit 3dd33dda4d29bd0579b04c4592ff4dcfe9ee777c

Drag and drop improvements

app/browser/tabs.js
@@ -23,17 +23,10 @@ const appStore = require('../../js/stores/appStore')
 const appConfig = require('../../js/constants/appConfig')
 const siteTags = require('../../js/constants/siteTags')
 const {newTabMode} = require('../common/constants/settingsEnums')
+const {cleanupWebContents, currentWebContents, getWebContents, updateWebContents} = require('./webContentsCache')
 
-let currentWebContents = {}
 let currentPartitionNumber = 0
 const incrementPartitionNumber = () => ++currentPartitionNumber
-
-const cleanupWebContents = (tabId) => {
-  if (currentWebContents[tabId]) {
-    delete currentWebContents[tabId]
-    appActions.tabClosed({ tabId })
-  }
-}
 
 const normalizeUrl = function (url) {
   if (isSourceAboutUrl(url)) {
@@ -49,7 +42,7 @@ const normalizeUrl = function (url) {
 }
 
 const getTabValue = function (tabId) {
-  let tab = api.getWebContents(tabId)
+  let tab = getWebContents(tabId)
   if (tab) {
     let tabValue = makeImmutable(tab.tabValue())
     tabValue = tabValue.set('canGoBack', tab.canGoBack())
@@ -353,7 +346,7 @@ const api = {
         console.log('responsive')
       })
 
-      currentWebContents[tabId] = tab
+      updateWebContents(tabId, tab)
       let tabValue = getTabValue(tabId)
       if (tabValue) {
         appActions.tabCreated(tabValue)
@@ -394,14 +387,10 @@ const api = {
     }
   },
 
-  getWebContents: (tabId) => {
-    return currentWebContents[tabId]
-  },
-
   toggleDevTools: (state, action) => {
     action = makeImmutable(action)
     const tabId = action.get('tabId')
-    const tab = api.getWebContents(tabId)
+    const tab = getWebContents(tabId)
     if (tab && !tab.isDestroyed()) {
       if (tab.isDevToolsOpened()) {
         tab.closeDevTools()
@@ -416,7 +405,7 @@ const api = {
     action = makeImmutable(action)
     let frameProps = action.get('frameProps')
     let tabId = frameProps.get('tabId')
-    let tab = api.getWebContents(tabId)
+    let tab = getWebContents(tabId)
     if (tab && !tab.isDestroyed()) {
       tab.setActive(true)
       let tabValue = getTabValue(tabId)
@@ -428,7 +417,7 @@ const api = {
   loadURL: (state, action) => {
     action = makeImmutable(action)
     const tabId = action.get('tabId')
-    const tab = api.getWebContents(tabId)
+    const tab = getWebContents(tabId)
     if (tab && !tab.isDestroyed()) {
       let url = normalizeUrl(action.get('url'))
       // We only allow loading URLs explicitly when the origin is
@@ -451,7 +440,7 @@ const api = {
     action = makeImmutable(action)
     const windowId = action.get('windowId')
     const tabValue = tabState.getActiveTabValue(state, windowId)
-    const tab = tabValue && api.getWebContents(tabValue.get('tabId'))
+    const tab = tabValue && getWebContents(tabValue.get('tabId'))
     if (tab && !tab.isDestroyed()) {
       const url = normalizeUrl(action.get('url'))
       tab.loadURL(url)
@@ -463,7 +452,7 @@ const api = {
     action = makeImmutable(action)
     const muted = action.get('muted')
     const tabId = action.get('tabId')
-    const tab = api.getWebContents(tabId)
+    const tab = getWebContents(tabId)
     if (tab && !tab.isDestroyed()) {
       tab.setAudioMuted(muted)
       const tabValue = getTabValue(tabId)
@@ -480,7 +469,7 @@ const api = {
     if (tabValue && tabValue.get('index') !== undefined) {
       options = options.set('index', tabValue.get('index') + 1)
     }
-    const tab = api.getWebContents(tabId)
+    const tab = getWebContents(tabId)
     if (tab && !tab.isDestroyed()) {
       tab.clone(options.toJS(), (newTab) => {
       })
@@ -492,7 +481,7 @@ const api = {
     action = makeImmutable(action)
     const tabId = action.get('tabId')
     const pinned = action.get('pinned')
-    const tab = api.getWebContents(tabId)
+    const tab = getWebContents(tabId)
     if (tab && !tab.isDestroyed()) {
       const url = tab.getURL()
       // For now we only support 1 tab pin per URL
@@ -520,7 +509,7 @@ const api = {
   },
 
   closeTab: (tabId, forceClose) => {
-    const tab = api.getWebContents(tabId)
+    const tab = getWebContents(tabId)
     try {
       if (tab && !tab.isDestroyed()) {
         if (forceClose) {
@@ -577,14 +566,10 @@ const api = {
     return state
   },
 
-  moveTo: (state, action) => {
-    action = makeImmutable(action)
-    const tabId = action.get('tabId')
-    const frameOpts = action.get('frameOpts')
-    const browserOpts = action.get('browserOpts') || new Immutable.Map()
-    // positionByMouseCursor: true
-    const windowId = action.get('windowId') || -1
-    const tab = api.getWebContents(tabId)
+  moveTo: (state, tabId, frameOpts, browserOpts, windowId) => {
+    frameOpts = makeImmutable(frameOpts)
+    browserOpts = makeImmutable(browserOpts)
+    const tab = getWebContents(tabId)
     if (tab && !tab.isDestroyed()) {
       const tabValue = getTabValue(tabId)
       const guestInstanceId = tabValue && tabValue.get('guestInstanceId')
@@ -596,6 +581,21 @@ const api = {
       if (windowId != null && currentWindowId === windowId) {
         return state
       }
+
+      if (windowId == null || windowId === -1) {
+        // If there's only one tab and we're dragging outside the window, then disallow
+        // a new window to be created.
+        const windowTabCount = tabState.getTabsByWindowId(state, currentWindowId).size
+        if (windowTabCount === 1) {
+          return state
+        }
+      }
+
+      if (tabValue.get('pinned')) {
+        // If the current tab is pinned, then don't allow to drag out
+        return state
+      }
+
       tab.detach(() => {
         if (windowId == null || windowId === -1) {
           appActions.newWindow(makeImmutable(frameOpts), browserOpts)
@@ -615,7 +615,7 @@ const api = {
     const windowId = createProperties.get('windowId')
     const tabData = tabState.getMatchingTab(state, createProperties, windowId, url)
     if (tabData) {
-      const tab = api.getWebContents(tabData.get('id'))
+      const tab = getWebContents(tabData.get('id'))
       if (tab && !tab.isDestroyed()) {
         tab.setActive(true)
       }

app/browser/reducers/tabsReducer.js
@@ -6,6 +6,7 @@
 
 const appConstants = require('../../../js/constants/appConstants')
 const tabs = require('../tabs')
+const {getWebContents} = require('../webContentsCache')
 const tabState = require('../../common/state/tabState')
 const windowConstants = require('../../../js/constants/windowConstants')
 const windowAction = require('../../../js/actions/windowActions.js')
@@ -13,6 +14,9 @@ const {makeImmutable} = require('../../common/state/immutableUtil')
 const {getFlashResourceId} = require('../../../js/flash')
 const {l10nErrorText} = require('../../common/lib/httpUtil')
 const windows = require('../windows')
+const Immutable = require('immutable')
+const dragTypes = require('../../../js/constants/dragTypes')
+const {frameOptsFromFrame} = require('../../../js/state/frameStateUtil')
 
 const tabsReducer = (state, action) => {
   action = makeImmutable(action)
@@ -23,9 +27,14 @@ const tabsReducer = (state, action) => {
     case appConstants.APP_TAB_CREATED:
       state = tabState.maybeCreateTab(state, action)
       break
-    case appConstants.APP_TAB_MOVED:
-      state = tabs.moveTo(state, action)
+    case appConstants.APP_TAB_MOVED: {
+      const tabId = action.get('tabId')
+      const frameOpts = action.get('frameOpts')
+      const browserOpts = action.get('browserOpts') || new Immutable.Map()
+      const windowId = action.get('windowId') || -1
+      state = tabs.moveTo(state, tabId, frameOpts, browserOpts, windowId)
       break
+    }
     case appConstants.APP_CREATE_TAB_REQUESTED:
       state = tabs.createTab(state, action)
       break
@@ -41,7 +50,7 @@ const tabsReducer = (state, action) => {
     case appConstants.APP_ALLOW_FLASH_ONCE:
     case appConstants.APP_ALLOW_FLASH_ALWAYS:
       {
-        const webContents = tabs.getWebContents(action.get('tabId'))
+        const webContents = getWebContents(action.get('tabId'))
         if (webContents && !webContents.isDestroyed() && webContents.getURL() === action.get('url')) {
           webContents.authorizePlugin(getFlashResourceId())
         }
@@ -143,6 +152,23 @@ const tabsReducer = (state, action) => {
         }
       }
       break
+    case appConstants.APP_DRAG_STOPPED: {
+      const dragData = state.get('dragData')
+      if (dragData.get('type') === dragTypes.TAB) {
+        const frame = dragData.get('data')
+        const frameOpts = frameOptsFromFrame(frame).toJS()
+        const browserOpts = { positionByMouseCursor: true }
+        if (dragData) {
+          frameOpts.indexByFrameKey = dragData.getIn(['dragOverData', 'draggingOverKey'])
+          const prependIndexByFrameKey = dragData.getIn(['dragOverData', 'draggingOverLeftHalf'])
+          if (prependIndexByFrameKey === false) {
+            frameOpts.indexByFrameKey++
+          }
+        }
+        state = tabs.moveTo(state, frame.get('tabId'), frameOpts, browserOpts, dragData.get('dropWindowId'))
+      }
+      break
+    }
   }
   return state
 }

app/browser/webContentsCache.js
@@ -1,0 +1,29 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+const appActions = require('../../js/actions/appActions')
+
+const currentWebContents = {}
+
+const cleanupWebContents = (tabId) => {
+  if (currentWebContents[tabId]) {
+    delete currentWebContents[tabId]
+    appActions.tabClosed({ tabId })
+  }
+}
+
+const getWebContents = (tabId) => {
+  return currentWebContents[tabId]
+}
+
+const updateWebContents = (tabId, tab) => {
+  currentWebContents[tabId] = tab
+}
+
+module.exports = {
+  cleanupWebContents,
+  getWebContents,
+  updateWebContents,
+  currentWebContents
+}

app/renderer/components/tabs/tab.js
@@ -18,11 +18,9 @@ const {NotificationBarCaret} = require('../../../../js/components/notificationBa
 
 // Actions
 const windowActions = require('../../../../js/actions/windowActions')
-const appActions = require('../../../../js/actions/appActions')
 
 // Store
 const windowStore = require('../../../../js/stores/windowStore')
-const {frameOptsFromFrame} = require('../../../../js/state/frameStateUtil')
 
 // Constants
 const dragTypes = require('../../../../js/constants/dragTypes')
@@ -88,6 +86,15 @@ class Tab extends ImmutableComponent {
       sourceDragData.get('draggingOverWindowId') === getCurrentWindowId()
   }
 
+  get isDraggingOverSelf () {
+    const draggingOverData = this.props.dragData && this.props.dragData.get('dragOverData')
+    const sourceDragData = dnd.getInterBraveDragData()
+    if (!draggingOverData || !sourceDragData) {
+      return false
+    }
+    return draggingOverData.get('draggingOverKey') === sourceDragData.get('key')
+  }
+
   get isDraggingOverLeft () {
     if (!this.draggingOverData) {
       return false
@@ -127,29 +134,6 @@ class Tab extends ImmutableComponent {
 
   onDragEnd (e) {
     dnd.onDragEnd(dragTypes.TAB, this.frame, e)
-    // If there's no dropWindowId that means the user dropped it outside of Brave completely and we should
-    // create a new window with the tab.
-
-    // TODO(bridiver) - a window with a single tab should not be draggable
-    if (!dnd.isDraggingInsideWindow()) {
-      const frameOpts = frameOptsFromFrame(this.frame).toJS()
-      const browserOpts = { positionByMouseCursor: true }
-      let dropWindowId = -1
-      if (this.props.dragData) {
-        frameOpts.indexByFrameKey = this.props.dragData.getIn(['dragOverData', 'draggingOverKey'])
-        const prependIndexByFrameKey = this.props.dragData.getIn(['dragOverData', 'draggingOverLeftHalf'])
-        if (prependIndexByFrameKey === false) {
-          frameOpts.indexByFrameKey++
-        }
-        dropWindowId = this.props.dragData.get('dropWindowId') || this.props.dragData.getIn(['dragOverData', 'draggingOverWindowId']) || dropWindowId
-      }
-      // Disallow dragging a tab into a new window if the window you're dragging from has only 1 tab
-      // Also dragging out a pin is not cool, so not allowed!
-      if ((dropWindowId === -1 && windowStore.getFrames().size === 1) || this.frame.get('pinnedLocation')) {
-        return
-      }
-      appActions.tabMoved(this.frame.get('tabId'), frameOpts, browserOpts, dropWindowId)
-    }
   }
 
   onDragOver (e) {
@@ -298,8 +282,8 @@ class Tab extends ImmutableComponent {
     return <div
       className={cx({
         tabArea: true,
-        draggingOverLeft: this.isDraggingOverLeft,
-        draggingOverRight: this.isDraggingOverRight,
+        draggingOverLeft: this.isDraggingOverLeft && !this.isDraggingOverSelf,
+        draggingOverRight: this.isDraggingOverRight && !this.isDraggingOverSelf,
         isDragging: this.isDragging,
         isPinned: this.isPinned,
         partOfFullPageSet: this.props.partOfFullPageSet || !!this.props.tabWidth

js/dnd.js
@@ -12,8 +12,6 @@ const siteUtil = require('./state/siteUtil')
 const appStoreRenderer = require('./stores/appStoreRenderer')
 const {getCurrentWindowId} = require('../app/renderer/currentWindow')
 
-let isDraggingInsideWindow = false
-
 module.exports.getInterBraveDragData = () => {
   return appStoreRenderer.state.getIn(['dragData', 'data'])
 }
@@ -21,22 +19,6 @@ module.exports.getInterBraveDragData = () => {
 module.exports.getInterBraveDragType = () => {
   return appStoreRenderer.state.getIn(['dragData', 'type'])
 }
-
-document.addEventListener('dragenter', () => {
-  isDraggingInsideWindow = true
-}, true)
-
-document.addEventListener('dragleave', (e) => {
-  if (!e.clientX && !e.clientY) {
-    isDraggingInsideWindow = false
-    appActions.draggedOver({
-      draggingOverKey: -1,
-      draggingOverWindowId: -1
-    })
-  }
-}, true)
-
-module.exports.isDraggingInsideWindow = () => isDraggingInsideWindow
 
 module.exports.onDragStart = (dragType, data, e) => {
   e.dataTransfer.effectAllowed = 'all'

js/stores/appStore.js
@@ -368,7 +368,8 @@ const handleAppAction = (action) => {
       require('../../app/browser/reducers/downloadsReducer'),
       require('../../app/browser/reducers/flashReducer'),
       // tabs, sites and windows reducers need to stay in that order
-      // until we have a better way to manage dependencies
+      // until we have a better way to manage dependencies.
+      // tabsReducer must be above dragDropReducer.
       require('../../app/browser/reducers/tabsReducer'),
       require('../../app/browser/reducers/sitesReducer'),
       require('../../app/browser/reducers/windowsReducer'),

js/stores/eventStore.js
@@ -9,7 +9,7 @@ const EventEmitter = require('events').EventEmitter
 const Immutable = require('immutable')
 const windowConstants = require('../constants/windowConstants')
 const debounce = require('../lib/debounce')
-const tabs = require('../../app/browser/tabs')
+const {getWebContents} = require('../../app/browser/webContentsCache')
 const {isSourceAboutUrl} = require('../lib/appUrlUtil')
 const {responseHasContent} = require('../../app/common/lib/httpUtil')
 
@@ -49,7 +49,7 @@ let lastActivePageUrl = null
 let lastActiveTabId = null
 
 const addPageView = (url, tabId) => {
-  const tab = tabs.getWebContents(tabId)
+  const tab = getWebContents(tabId)
   const isPrivate = !tab ||
     tab.isDestroyed() ||
     !tab.session.partition.startsWith('persist:')

test/unit/app/browser/reducers/tabsReducerTest.js
@@ -1,0 +1,252 @@
+/* global describe, it, before, after, beforeEach, afterEach */
+const Immutable = require('immutable')
+const assert = require('assert')
+const mockery = require('mockery')
+const sinon = require('sinon')
+const appConstants = require('../../../../../js/constants/appConstants')
+const dragTypes = require('../../../../../js/constants/dragTypes')
+const fakeElectron = require('../../../lib/fakeElectron')
+require('../../../braveUnit')
+
+describe('tabsReducer', function () {
+  let tabsReducer
+  let appActions
+  before(function () {
+    mockery.enable({
+      warnOnReplace: false,
+      warnOnUnregistered: false,
+      useCleanCache: true
+    })
+    this.notPinnedTabIndex = 0
+    this.pinnedTabIndex = 1
+    this.singleTabWindowIndex = 2
+    this.state = Immutable.fromJS({
+      tabs: [{
+        tabId: 1,
+        windowId: 1,
+        pinned: false
+      }, {
+        tabId: 2,
+        pinned: true,
+        windowId: 1
+      }, {
+        tabId: 3,
+        pinned: false,
+        windowId: 2
+      }]
+    })
+    mockery.registerMock('electron', fakeElectron)
+    mockery.registerMock('./webContentsCache', {
+      getWebContents: (tabId) => ({
+        canGoBack: () => true,
+        canGoForward: () => true,
+        session: {
+          partition: 'default'
+        },
+        tabValue: () =>
+          this.state.get('tabs').find((tab) => tab.get('tabId') === tabId),
+        isDestroyed: () => false,
+        detach: (cb) => cb()
+      })
+    })
+    tabsReducer = require('../../../../../app/browser/reducers/tabsReducer')
+    appActions = require('../../../../../js/actions/appActions')
+  })
+
+  after(function () {
+    mockery.disable()
+  })
+
+  beforeEach(function () {
+    this.newWindowSpy = sinon.spy(appActions, 'newWindow')
+    this.newWebContentsAddedSpy = sinon.spy(appActions, 'newWebContentsAdded')
+  })
+
+  afterEach(function () {
+    this.newWindowSpy.restore()
+    this.newWebContentsAddedSpy.restore()
+  })
+
+  describe.skip('APP_SET_STATE', function () {
+    it('initializes a tab', function () {
+      // TODO
+    })
+  })
+
+  describe.skip('APP_TAB_CREATED', function () {
+    it('creates a tab', function () {
+      // TODO
+    })
+  })
+  describe.skip('APP_TAB_MOVED', function () {
+    it('moves a tab', function () {
+      // TODO
+    })
+  })
+
+  describe.skip('APP_CREATE_TAB_REQUESTED', function () {
+    it('creates a new tab', function () {
+      // TODO
+    })
+  })
+
+  describe.skip('APP_CREATE_TAB_REQUESTED', function () {
+    it('creates a tab that does not exist yet', function () {
+      // TODO
+    })
+    it('does not create a tab that already exists', function () {
+      // TODO
+    })
+  })
+
+  // It this one really needed?
+  describe.skip('APP_TAB_UPDATED', function () {
+    it('updates a tab?', function () {
+      // TODO
+    })
+  })
+
+  describe.skip('APP_TAB_CLOSED', function () {
+    it('closes tab?', function () {
+      // TODO
+    })
+  })
+
+  describe.skip('APP_ALLOW_FLASH_ONCE', function () {
+    it('allows flash once', function () {
+      // TODO
+    })
+  })
+
+  describe.skip('APP_ALLOW_FLASH_ALWAYS', function () {
+    it('allows flash always', function () {
+      // TODO
+    })
+  })
+
+  describe.skip('APP_TAB_CLONED', function () {
+    it('clones a tab', function () {
+      // TODO
+    })
+  })
+
+  describe.skip('APP_TAB_PINNED', function () {
+    it('pins a tab', function () {
+      // TODO
+    })
+  })
+
+  describe.skip('WINDOW_SET_AUDIO_MUTED', function () {
+    it('mutes audio', function () {
+      // TODO
+    })
+  })
+
+  describe.skip('WINDOW_SET_ACTIVE_FRAME', function () {
+    it('sets the frame as active', function () {
+      // TODO
+    })
+  })
+
+  describe.skip('APP_TAB_TOGGLE_DEV_TOOLS', function () {
+    it('toggles dev tools for the tab', function () {
+      // TODO
+    })
+  })
+
+  describe.skip('APP_LOAD_URL_REQUESTED', function () {
+    it('loads the specified URL', function () {
+      // TODO
+    })
+  })
+
+  describe.skip('APP_LOAD_URL_IN_ACTIVE_TAB_REQUESTED', function () {
+    it('loads the specified URL in the active tab', function () {
+      // TODO
+    })
+  })
+
+  describe.skip('APP_FRAME_CHANGED', function () {
+    it('updates frame data', function () {
+      // TODO
+    })
+  })
+
+  describe('APP_DRAG_STOPPED', function () {
+    const action = {
+      actionType: appConstants.APP_DRAG_STOPPED
+    }
+    it('moves tab to a new window', function () {
+      const state = this.state.set('dragData', Immutable.fromJS({
+        windowId: 1,
+        type: dragTypes.TAB,
+        data: this.state.getIn(['tabs', this.notPinnedTabIndex]),
+        dropWindowId: -1
+      }))
+      const newState = tabsReducer(state, action)
+      assert.equal(this.newWindowSpy.calledOnce, true)
+      assert.equal(this.newWebContentsAddedSpy.notCalled, true)
+      assert(Immutable.is(newState, state))
+    })
+    it('moves tab to an existing window', function () {
+      const state = this.state.set('dragData', Immutable.fromJS({
+        windowId: 1,
+        type: dragTypes.TAB,
+        data: this.state.getIn(['tabs', this.notPinnedTabIndex]),
+        dropWindowId: 11
+      }))
+      const newState = tabsReducer(state, action)
+      assert.equal(this.newWindowSpy.notCalled, true)
+      assert.equal(this.newWebContentsAddedSpy.calledOnce, true)
+      assert(Immutable.is(newState, state))
+    })
+    it('does not move pinned tabs', function () {
+      const state = this.state.set('dragData', Immutable.fromJS({
+        windowId: 2,
+        type: dragTypes.TAB,
+        data: this.state.getIn(['tabs', this.pinnedTabIndex]),
+        dropWindowId: -1
+      }))
+      const newState = tabsReducer(state, action)
+      assert.equal(this.newWindowSpy.notCalled, true)
+      assert.equal(this.newWebContentsAddedSpy.notCalled, true)
+      assert(Immutable.is(newState, state))
+    })
+    it('does not move pinned tabs to alt window', function () {
+      const state = this.state.set('dragData', Immutable.fromJS({
+        windowId: 2,
+        type: dragTypes.TAB,
+        data: this.state.getIn(['tabs', this.pinnedTabIndex]),
+        dropWindowId: 89
+      }))
+      const newState = tabsReducer(state, action)
+      assert.equal(this.newWindowSpy.notCalled, true)
+      assert.equal(this.newWebContentsAddedSpy.notCalled, true)
+      assert(Immutable.is(newState, state))
+    })
+    it('does not move single tab windows into new window', function () {
+      const state = this.state.set('dragData', Immutable.fromJS({
+        windowId: 1,
+        type: dragTypes.TAB,
+        data: this.state.getIn(['tabs', this.singleTabWindowIndex]),
+        dropWindowId: -1
+      }))
+      const newState = tabsReducer(state, action)
+      assert.equal(this.newWindowSpy.notCalled, true)
+      assert.equal(this.newWebContentsAddedSpy.notCalled, true)
+      assert(Immutable.is(newState, state))
+    })
+    it('allows combining single tab into alt window', function () {
+      const state = this.state.set('dragData', Immutable.fromJS({
+        windowId: 2,
+        type: dragTypes.TAB,
+        data: this.state.getIn(['tabs', this.singleTabWindowIndex]),
+        dropWindowId: 41
+      }))
+      const newState = tabsReducer(state, action)
+      assert.equal(this.newWindowSpy.notCalled, true)
+      assert.equal(this.newWebContentsAddedSpy.calledOnce, true)
+      assert(Immutable.is(newState, state))
+    })
+  })
+})

commit c14021dceea244527c557ee7392440953fc1cab7

Fixes navigation error that was introduced in 3dd33dda4d29bd0579b04c4592ff4dcfe9ee777c
Auditors: @bbondy

Test Plan:
- `app-tests` suit should be green now

app/browser/tabs.js
@@ -627,7 +627,7 @@ const api = {
 
   goBack: (state, action) => {
     action = makeImmutable(action)
-    const tab = api.getWebContents(action.get('tabId'))
+    const tab = getWebContents(action.get('tabId'))
     if (tab && !tab.isDestroyed()) {
       tab.goBack()
     }
@@ -636,7 +636,7 @@ const api = {
 
   goForward: (state, action) => {
     action = makeImmutable(action)
-    const tab = api.getWebContents(action.get('tabId'))
+    const tab = getWebContents(action.get('tabId'))
     if (tab && !tab.isDestroyed()) {
       tab.goForward()
     }
@@ -645,7 +645,7 @@ const api = {
 
   goToIndex: (state, action) => {
     action = makeImmutable(action)
-    const tab = api.getWebContents(action.get('tabId'))
+    const tab = getWebContents(action.get('tabId'))
     if (tab && !tab.isDestroyed()) {
       tab.goToIndex(action.get('index'))
     }
@@ -653,7 +653,7 @@ const api = {
   },
 
   getHistoryEntries: (state, action) => {
-    const tab = api.getWebContents(action.get('tabId'))
+    const tab = getWebContents(action.get('tabId'))
     const sites = state ? state.get('sites') : null
 
     if (tab && !tab.isDestroyed()) {