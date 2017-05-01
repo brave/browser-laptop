6606f99faf1ff27f9af709c9ae553df20880a472
Brian R. Bondy <netzen@gmail.com>
2017-05-01 14:01:07 -0400

    Update digg to https
    Auditors: @bsclifton

--- app/browser/share.js
+++ app/browser/share.js
@@ -15,7 +15,7 @@ const templateUrls = {
   googlePlus: 'https://plus.google.com/share?url={url}',
   linkedIn: 'https://www.linkedin.com/shareArticle?url={url}&title={title}',
   buffer: 'https://buffer.com/add?text={title}&url={url}',
-  digg: 'http://digg.com/submit?url={url}&title={title}',
+  digg: 'https://digg.com/submit?url={url}&title={title}',
   reddit: 'https://reddit.com/submit?url={url}&title={title}'
 }
 
