brave/browser-laptop

commit 1686677bc9779a8eb42a35be6d18590d070f8aaf

Tab tear off and shared pinned tabs

app/common/commonMenu.js
@@ -28,6 +28,15 @@ const ensureAtLeastOneWindow = (frameOpts) => {
   }
 }
 
+const getCurrentWindowId = () => {
+  if (process.type === 'browser') {
+    return BrowserWindow.getActiveWindow().id
+  } else {
+    const {currentWindowId} = require('../renderer/currentWindow')
+    return currentWindowId
+  }
+}
+
 /**
  * Sends a message to the web contents of the focused window.
  * @param {Object} focusedWindow the focusedWindow if any
@@ -59,15 +68,17 @@ module.exports.quitMenuItem = () => ({
   }
 })
 
-module.exports.newTabMenuItem = (parentFrameKey) => {
+module.exports.newTabMenuItem = (openerTabId) => {
   return {
     label: locale.translation('newTab'),
     accelerator: 'CmdOrCtrl+T',
     click: function (item, focusedWindow) {
-      if (!module.exports.sendToFocusedWindow(focusedWindow, [messages.SHORTCUT_NEW_FRAME, undefined, { parentFrameKey }])) {
-        // no active windows
-        appActions.newWindow()
-      }
+      ensureAtLeastOneWindow(Immutable.fromJS({}))
+      appActions.createTabRequested({
+        url: 'about:newtab',
+        windowId: getCurrentWindowId(),
+        openerTabId
+      })
     }
   }
 }
@@ -78,7 +89,11 @@ module.exports.newPrivateTabMenuItem = () => {
     accelerator: 'Shift+CmdOrCtrl+P',
     click: function (item, focusedWindow) {
       ensureAtLeastOneWindow(Immutable.fromJS({ isPrivate: true }))
-      module.exports.sendToFocusedWindow(focusedWindow, [messages.SHORTCUT_NEW_FRAME, undefined, { isPrivate: true }])
+      appActions.createTabRequested({
+        url: 'about:newtab',
+        windowId: getCurrentWindowId(),
+        isPrivate: true
+      })
     }
   }
 }
@@ -169,7 +184,10 @@ module.exports.preferencesMenuItem = () => {
           location: 'about:preferences'
         }))
       } else {
-        module.exports.sendToFocusedWindow(focusedWindow, [messages.SHORTCUT_NEW_FRAME, 'about:preferences', { singleFrame: true }])
+        appActions.maybeCreateTabRequested({
+          url: 'about:preferences',
+          windowId: getCurrentWindowId()
+        })
       }
     }
   }
@@ -185,7 +203,10 @@ module.exports.bookmarksManagerMenuItem = () => {
           location: 'about:bookmarks'
         }))
       } else {
-        module.exports.sendToFocusedWindow(focusedWindow, [messages.SHORTCUT_NEW_FRAME, 'about:bookmarks', { singleFrame: true }])
+        appActions.maybeCreateTabRequested({
+          url: 'about:bookmarks',
+          windowId: getCurrentWindowId()
+        })
       }
     }
   }
@@ -201,7 +222,10 @@ module.exports.historyMenuItem = () => {
           location: 'about:history'
         }))
       } else {
-        module.exports.sendToFocusedWindow(focusedWindow, [messages.SHORTCUT_NEW_FRAME, 'about:history', { singleFrame: true }])
+        appActions.maybeCreateTabRequested({
+          url: 'about:history',
+          windowId: getCurrentWindowId()
+        })
       }
     }
   }
@@ -218,7 +242,10 @@ module.exports.downloadsMenuItem = () => {
         }))
       } else {
         module.exports.sendToFocusedWindow(focusedWindow, [messages.HIDE_DOWNLOADS_TOOLBAR])
-        module.exports.sendToFocusedWindow(focusedWindow, [messages.SHORTCUT_NEW_FRAME, 'about:downloads', { singleFrame: true }])
+        appActions.maybeCreateTabRequested({
+          url: 'about:downloads',
+          windowId: getCurrentWindowId()
+        })
       }
     }
   }
@@ -233,7 +260,10 @@ module.exports.passwordsMenuItem = () => {
           location: 'about:passwords'
         }))
       } else {
-        module.exports.sendToFocusedWindow(focusedWindow, [messages.SHORTCUT_NEW_FRAME, 'about:passwords', { singleFrame: true }])
+        appActions.maybeCreateTabRequested({
+          url: 'about:passwords',
+          windowId: getCurrentWindowId()
+        })
       }
     }
   }
@@ -269,8 +299,10 @@ module.exports.submitFeedbackMenuItem = () => {
   return {
     label: locale.translation('submitFeedback'),
     click: function (item, focusedWindow) {
-      module.exports.sendToFocusedWindow(focusedWindow,
-                                         [messages.SHORTCUT_NEW_FRAME, communityURL])
+      appActions.maybeCreateTabRequested({
+        url: communityURL,
+        windowId: getCurrentWindowId()
+      })
     }
   }
 }
@@ -302,7 +334,10 @@ module.exports.aboutBraveMenuItem = () => {
   return {
     label: locale.translation('aboutApp'),
     click: (item, focusedWindow) => {
-      module.exports.sendToFocusedWindow(focusedWindow, [messages.SHORTCUT_NEW_FRAME, 'about:brave', { singleFrame: true }])
+      appActions.maybeCreateTabRequested({
+        url: 'about:brave',
+        windowId: getCurrentWindowId()
+      })
     }
   }
 }
@@ -325,7 +360,10 @@ module.exports.braveryGlobalMenuItem = () => {
           location: 'about:preferences#shields'
         }))
       } else {
-        module.exports.sendToFocusedWindow(focusedWindow, [messages.SHORTCUT_NEW_FRAME, 'about:preferences#shields', { singleFrame: true }])
+        appActions.maybeCreateTabRequested({
+          url: 'about:preferences#shields',
+          windowId: getCurrentWindowId()
+        })
       }
     }
   }
@@ -344,7 +382,10 @@ module.exports.braveryPaymentsMenuItem = () => {
           location: 'about:preferences#payments'
         }))
       } else {
-        module.exports.sendToFocusedWindow(focusedWindow, [messages.SHORTCUT_NEW_FRAME, 'about:preferences#payments', { singleFrame: true }])
+        appActions.maybeCreateTabRequested({
+          url: 'about:preferences#payments',
+          windowId: getCurrentWindowId()
+        })
       }
     }
   }

commit a90576b5a10e0622eea7269434ad7c90a81df219

Fix issue on macOS where creating new tab fails
Unintentionally introduced with https://github.com/brave/browser-laptop/commit/1686677bc9779a8eb42a35be6d18590d070f8aaf
- appActions.createTabRequested is only called if there is one window (ex: ensureAtLeastOneWindow returns true)
- ensureAtLeastOneWindow is now called for session tabs

Fixes https://github.com/brave/browser-laptop/issues/8575

Auditors: @bbondy

Test Plan:
1. Launch Brave on macOS and close all open windows
2. From the File menu, pick `New Tab` and confirm new window is created w/ new tab
3. Close the window
4. From the File menu, pick `New Private Tab` and confirm new window is created w/ new private tab
5. Close the window
6. From the File menu, pick `New Session Tab` > `New Session Tab 5` and confirm new window is created w/ session tab
7. With a window open, pick the options specified in 2, 4, and 6 and make sure they still work as expected

app/common/commonMenu.js
@@ -25,16 +25,19 @@ if (process.type === 'browser') {
 const ensureAtLeastOneWindow = (frameOpts) => {
   if (BrowserWindow.getAllWindows().length === 0) {
     appActions.newWindow(frameOpts)
+    return false
   }
+  return true
 }
 
 const getCurrentWindowId = () => {
   if (process.type === 'browser') {
-    return BrowserWindow.getActiveWindow().id
-  } else {
-    const currentWindow = require('../renderer/currentWindow')
-    return currentWindow.getCurrentWindowId()
+    const activeWindow = BrowserWindow.getActiveWindow()
+    return activeWindow && activeWindow.id
   }
+
+  const currentWindow = require('../renderer/currentWindow')
+  return currentWindow.getCurrentWindowId()
 }
 
 /**
@@ -73,11 +76,12 @@ module.exports.newTabMenuItem = (openerTabId) => {
     label: locale.translation('newTab'),
     accelerator: 'CmdOrCtrl+T',
     click: function (item, focusedWindow) {
-      ensureAtLeastOneWindow(Immutable.fromJS({}))
-      appActions.createTabRequested({
-        windowId: getCurrentWindowId(),
-        openerTabId
-      })
+      if (ensureAtLeastOneWindow(Immutable.fromJS({}))) {
+        appActions.createTabRequested({
+          windowId: getCurrentWindowId(),
+          openerTabId
+        })
+      }
     }
   }
 }
@@ -87,12 +91,13 @@ module.exports.newPrivateTabMenuItem = () => {
     label: locale.translation('newPrivateTab'),
     accelerator: 'Shift+CmdOrCtrl+P',
     click: function (item, focusedWindow) {
-      ensureAtLeastOneWindow(Immutable.fromJS({ isPrivate: true }))
-      appActions.createTabRequested({
-        url: 'about:newtab',
-        windowId: getCurrentWindowId(),
-        isPrivate: true
-      })
+      if (ensureAtLeastOneWindow(Immutable.fromJS({ isPrivate: true }))) {
+        appActions.createTabRequested({
+          url: 'about:newtab',
+          windowId: getCurrentWindowId(),
+          isPrivate: true
+        })
+      }
     }
   }
 }
@@ -101,9 +106,11 @@ module.exports.newPartitionedTabMenuItem = () => {
   const newPartitionedMenuItem = (partitionNumber) => ({
     label: `${locale.translation('newSessionTab')} ${partitionNumber}`,
     click: (item, focusedWindow) => {
-      appActions.createTabRequested({
-        partitionNumber
-      })
+      if (ensureAtLeastOneWindow(Immutable.fromJS({ partitionNumber }))) {
+        appActions.createTabRequested({
+          partitionNumber
+        })
+      }
     }
   })
 