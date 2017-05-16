c1c44372e79784ab0cc099df6226d9f499d30c05
Suguru Hirahara <luixxiul@gmail.com>
2017-05-16 17:16:08 +0900

    Remove addEditBookmark.less
    Closes #8881
    
    Auditors:
    
    Test Plan:
    1. npm run watch
    2. npm start
    3. Make sure there is no error

--- js/entry.js
+++ js/entry.js
@@ -16,7 +16,6 @@ require('../less/dialogs.less')
 require('../less/downloadBar.less')
 require('../less/bookmarksToolbar.less')
 require('../less/notificationBar.less')
-require('../less/addEditBookmark.less')
 require('../node_modules/font-awesome/css/font-awesome.css')
 
 const React = require('react')
