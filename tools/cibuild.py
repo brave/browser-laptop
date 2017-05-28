a3bb0657def604e5376f91c3184a4afa605d3ee3
Brian R. Bondy <netzen@gmail.com>
2017-05-28 12:47:02 -0400

    Upgrade to chromedriver 2.29

--- tools/cibuild.py
+++ tools/cibuild.py
@@ -5,7 +5,7 @@ import subprocess
 import sys
 import os.path
 MUON_VERSION = '3.2.101'
-CHROMEDRIVER_VERSION = '2.27'
+CHROMEDRIVER_VERSION = '2.29'
 SOURCE_ROOT = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 TARGET_ARCH= os.environ['TARGET_ARCH'] if os.environ.has_key('TARGET_ARCH') else 'x64'
 os.environ['npm_config_arch'] = TARGET_ARCH
