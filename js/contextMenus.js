3ff6f0f4f135f483f121caf1b58725b7e28808f4
Brian Clifton <brian@clifton.me>
2017-06-08 23:26:15 -0700

    Revert "Revert "Remove destroy from popup menus""
    This reverts commit b5743024e71bbea811ba3e97b274af390217e457.
    
    Required since Muon was updated in Master with https://github.com/brave/browser-laptop/commit/f018b74ebcf2bb0abad0ee80956ccca45c9030d9
    
    cc: @bridiver, @bbondy

--- js/contextMenus.js
+++ js/contextMenus.js
@@ -1339,21 +1339,18 @@ function onMainContextMenu (nodeProps, frame, tab, contextMenuType) {
   } else {
     const mainMenu = Menu.buildFromTemplate(mainTemplateInit(nodeProps, frame, tab))
     mainMenu.popup(getCurrentWindow())
-    mainMenu.destroy()
   }
 }
 
 function onFlashContextMenu (nodeProps, frameProps) {
   const flashMenu = Menu.buildFromTemplate(flashTemplateInit(frameProps))
   flashMenu.popup(getCurrentWindow())
-  flashMenu.destroy()
 }
 
 function onTabContextMenu (frameProps, e) {
   e.stopPropagation()
   const tabMenu = Menu.buildFromTemplate(tabTemplateInit(frameProps))
   tabMenu.popup(getCurrentWindow())
-  tabMenu.destroy()
 }
 
 function onNewTabContextMenu (target) {
@@ -1365,14 +1362,12 @@ function onNewTabContextMenu (target) {
   ]
   const menu = Menu.buildFromTemplate(menuTemplate)
   menu.popup(getCurrentWindow())
-  menu.destroy()
 }
 
 function onTabsToolbarContextMenu (bookmarkTitle, bookmarkLink, closestDestinationDetail, isParent, e) {
   e.stopPropagation()
   const tabsToolbarMenu = Menu.buildFromTemplate(tabsToolbarTemplateInit(bookmarkTitle, bookmarkLink, closestDestinationDetail, isParent))
   tabsToolbarMenu.popup(getCurrentWindow())
-  tabsToolbarMenu.destroy()
 }
 
 function onDownloadsToolbarContextMenu (downloadId, downloadItem, e) {
@@ -1381,14 +1376,12 @@ function onDownloadsToolbarContextMenu (downloadId, downloadItem, e) {
   }
   const downloadsToolbarMenu = Menu.buildFromTemplate(downloadsToolbarTemplateInit(downloadId, downloadItem))
   downloadsToolbarMenu.popup(getCurrentWindow())
-  downloadsToolbarMenu.destroy()
 }
 
 function onTabPageContextMenu (framePropsList, e) {
   e.stopPropagation()
   const tabPageMenu = Menu.buildFromTemplate(tabPageTemplateInit(framePropsList))
   tabPageMenu.popup(getCurrentWindow())
-  tabPageMenu.destroy()
 }
 
 function onUrlBarContextMenu (e) {
@@ -1398,14 +1391,12 @@ function onUrlBarContextMenu (e) {
   const activeFrame = getActiveFrame(windowState)
   const inputMenu = Menu.buildFromTemplate(urlBarTemplateInit(searchDetail, activeFrame, e))
   inputMenu.popup(getCurrentWindow())
-  inputMenu.destroy()
 }
 
 function onFindBarContextMenu (e) {
   e.stopPropagation()
   const findBarMenu = Menu.buildFromTemplate(findBarTemplateInit(e))
   findBarMenu.popup(getCurrentWindow())
-  findBarMenu.destroy()
 }
 
 function onSiteDetailContextMenu (siteDetail, activeFrame, e) {
@@ -1414,7 +1405,6 @@ function onSiteDetailContextMenu (siteDetail, activeFrame, e) {
   }
   const menu = Menu.buildFromTemplate(siteDetailTemplateInit(siteDetail, activeFrame))
   menu.popup(getCurrentWindow())
-  menu.destroy()
 }
 
 function onLedgerContextMenu (location, hostPattern) {
@@ -1430,7 +1420,6 @@ function onLedgerContextMenu (location, hostPattern) {
   ]
   const menu = Menu.buildFromTemplate(template)
   menu.popup(getCurrentWindow())
-  menu.destroy()
 }
 
 function onShowBookmarkFolderMenu (bookmarks, bookmark, activeFrame, e) {
