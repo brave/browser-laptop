2918560830be0863c0b6c9ac43e156b04958d883
Brian Clifton <brian@clifton.me>
2017-06-13 22:38:52 -0700

    Merge pull request #9432 from brave/fix/over-sorting-sites
    Don't sort sites when adding or removing them

--- app/browser/reducers/sitesReducer.js
+++ app/browser/reducers/sitesReducer.js
@@ -76,13 +76,11 @@ const sitesReducer = (state, action, immutableAction) => {
           state = syncUtil.updateSiteCache(state, action.destinationDetail || action.siteDetail)
         }
       }
-      state = state.set('sites', state.get('sites').sort(siteUtil.siteSort))
       state = updateActiveTabBookmarked(state)
       break
     case appConstants.APP_REMOVE_SITE:
       const removeSiteSyncCallback = action.skipSync ? undefined : syncActions.removeSite
       state = siteUtil.removeSite(state, action.siteDetail, action.tag, true, removeSiteSyncCallback)
-      state = state.set('sites', state.get('sites').sort(siteUtil.siteSort))
       if (syncEnabled()) {
         state = syncUtil.updateSiteCache(state, action.siteDetail)
       }
