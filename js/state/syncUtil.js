146a32432e4bc56708b08c3e590e54a88dd5492d
Brian R. Bondy <bbondy@gmail.com>
2017-07-07 11:20:01 -0400

    Merge pull request #9784 from brave/feature/sync-pausable
    Pending/confirmed sync records to allow pause/resume

--- js/state/syncUtil.js
+++ js/state/syncUtil.js
@@ -4,6 +4,8 @@
 'use strict'
 
 const Immutable = require('immutable')
+const appActions = require('../actions/appActions')
+const crypto = require('crypto')
 const writeActions = require('../constants/sync/proto').actions
 const siteTags = require('../constants/siteTags')
 const siteUtil = require('./siteUtil')
@@ -416,8 +418,6 @@ module.exports.isSyncable = (type, item) => {
  * @returns {Array.<number>}
  */
 module.exports.newObjectId = (objectPath) => {
-  const crypto = require('crypto')
-  const appActions = require('../actions/appActions')
   const objectId = new Immutable.List(crypto.randomBytes(16))
   appActions.setObjectId(objectId, objectPath)
   return objectId.toJS()
@@ -564,6 +564,7 @@ const deepArrayify = (sourceObject) => {
   }
   return object
 }
+module.exports.deepArrayify = deepArrayify
 
 /**
  * @param {Object}
