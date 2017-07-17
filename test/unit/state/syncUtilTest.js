e44519a60fee35ca862f134f9691d213c8657401
Brian Clifton <brian@clifton.me>
2017-07-17 00:37:11 -0700

    Add more unit tests for syncUtil
    Statement coverage from 39.06% to 39.84%
    Branch coverage 33.91% to 35.63%
    Function coverage from 29.03% to 32.26%
    Line coverage from 39.75% to 40.57%
    
    Auditors: @ayumi, @diracdeltas
    
    Test Plan: `npm run unittest -- --grep="syncUtil"`

--- test/unit/state/syncUtilTest.js
+++ test/unit/state/syncUtilTest.js
@@ -1,11 +1,196 @@
-/* global describe, it */
+/* global describe, before, after, it */
 
-const syncUtil = require('../../../js/state/syncUtil')
+const mockery = require('mockery')
 const assert = require('assert')
+const sinon = require('sinon')
 const Immutable = require('immutable')
+const writeActions = require('../../../js/constants/sync/proto').actions
 
 describe('syncUtil', () => {
-  describe('createSiteData()', () => {
+  let syncUtil
+  let appAction
+  let crypto
+  before(function () {
+    mockery.enable({
+      warnOnReplace: false,
+      warnOnUnregistered: false,
+      useCleanCache: true
+    })
+    appAction = require('../../../js/actions/appActions')
+    crypto = require('crypto')
+    mockery.registerMock('../actions/appActions', appAction)
+    mockery.registerMock('crypto', crypto)
+    syncUtil = require('../../../js/state/syncUtil')
+  })
+  after(function () {
+    mockery.disable()
+  })
+
+  describe('getSiteDataFromRecord', function () {
+    const objectId = [96, 46, 213, 0, 13, 111, 180, 184, 65, 66, 173, 27, 207, 29, 32, 108]
+    const records = [{
+      action: writeActions.UPDATE,
+      bookmark: {
+        isFolder: true,
+        parentFolderObjectId: null,
+        site: {
+          creationTime: 0,
+          customTitle: '',
+          favicon: '',
+          lastAccessedTime: 0,
+          location: '',
+          title: 'Folder1'
+        }
+      },
+      deviceId: [1],
+      objectData: 'bookmark',
+      objectId,
+      syncTimestamp: 1499736988267
+    }]
+    const existingObject = {
+      lastAccessedTime: 0,
+      tags: ['bookmark-folder'],
+      objectId,
+      order: 9,
+      folderId: 2,
+      customTitle: 'Folder1', // XXX: Android uses title whereas laptop uses customTitle
+      parentFolderId: 1
+    }
+    const appState = {
+      sites: {
+        '2': existingObject
+      },
+      sync: {
+        objectsById: {
+          [objectId.join('|')]: ['sites', '2']
+        }
+      }
+    }
+
+    describe('with action', function () {
+      describe('when NOT writeActions.CREATE', function () {
+        it('looks up / returns existing object', function () {
+          const result = syncUtil.getSiteDataFromRecord(records[0],
+            Immutable.fromJS(appState), Immutable.fromJS(records))
+          assert.deepEqual(result.existingObjectData.toJS(), existingObject)
+        })
+      })
+
+      describe('when writeActions.CREATE', function () {
+        it('does not set existingObjectData', function () {
+          const recordsCopy = Object.assign({}, records[0])
+          recordsCopy.action = writeActions.CREATE
+          const result = syncUtil.getSiteDataFromRecord(recordsCopy,
+            Immutable.fromJS(appState), Immutable.fromJS([recordsCopy]))
+          assert.equal(result.existingObjectData, undefined)
+        })
+      })
+    })
+
+    describe('with customTitle', function () {
+      it('keeps field if assigned value', function () {
+        // deep copy and assign a different custom title
+        const recordsCopy = Object.assign({}, records[0])
+        recordsCopy.bookmark = Object.assign({}, records[0].bookmark)
+        recordsCopy.bookmark.site = Object.assign({}, records[0].bookmark.site)
+        recordsCopy.bookmark.site.customTitle = 'demo value'
+        const result = syncUtil.getSiteDataFromRecord(recordsCopy,
+          Immutable.fromJS(appState), Immutable.fromJS([recordsCopy]))
+        assert.equal(result.siteDetail.has('customTitle'), true)
+        assert.equal(result.siteDetail.get('customTitle'), 'demo value')
+      })
+
+      it('deletes field if empty', function () {
+        const result = syncUtil.getSiteDataFromRecord(records[0],
+          Immutable.fromJS(appState), Immutable.fromJS(records))
+        assert.equal(result.siteDetail.has('customTitle'), false)
+      })
+    })
+
+    describe('with objectData', function () {
+      describe('when "bookmark"', function () {
+        it('update bookmark parent folder to null -> overrides existing parent folder', () => {
+          const result = syncUtil.getSiteDataFromRecord(records[0],
+            Immutable.fromJS(appState), Immutable.fromJS(records))
+          assert.equal(result.tag, 'bookmark-folder')
+          assert.deepEqual(result.siteDetail.toJS(),
+            {
+              objectId,
+              title: 'Folder1',
+              favicon: '',
+              location: '',
+              parentFolderId: 0,
+              folderId: 2,
+              tags: ['bookmark-folder'],
+              lastAccessedTime: 0,
+              creationTime: 0
+            }
+          )
+          assert.deepEqual(result.existingObjectData.toJS(), existingObject)
+        })
+      })
+
+      describe('when not "bookmark"', function () {
+
+      })
+    })
+  })
+
+  describe('applySyncRecords', function () {
+  })
+
+  describe('getExistingObject', function () {
+  })
+
+  describe('createSiteCache', function () {
+  })
+
+  describe('updateSiteCache', function () {
+  })
+
+  describe('now', function () {
+    let clock
+    before(function () {
+      clock = sinon.useFakeTimers()
+      clock.tick(8800)
+    })
+    after(function () {
+      clock.restore()
+    })
+    it('returns seconds (truncating milliseconds)', function () {
+      assert.equal(syncUtil.now(), 8)
+    })
+  })
+
+  describe('isSyncable', function () {
+  })
+
+  describe('newObjectId', function () {
+    let randomBytesSpy
+    let setObjectIdStub
+    let result
+    before(function () {
+      setObjectIdStub = sinon.stub(appAction, 'setObjectId')
+      randomBytesSpy = sinon.spy(crypto, 'randomBytes')
+      result = syncUtil.newObjectId(['path'])
+    })
+    after(function () {
+      setObjectIdStub.restore()
+      randomBytesSpy.restore()
+    })
+    it('generates a 16 byte id', function () {
+      assert.equal(randomBytesSpy.withArgs(16).calledOnce, true)
+    })
+    it('calls appActions.setObjectId', function () {
+      assert.equal(setObjectIdStub.calledOnce, true)
+    })
+    it('returns the 16 byte id', function () {
+      assert(Array.isArray(result))
+      assert.equal(result.length, 16)
+    })
+  })
+
+  describe('createSiteData', function () {
     const objectId = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
     const site = {
       favicon: 'https://calendar.google.com/googlecalendar/images/favicon_v2014_18.ico',
@@ -129,87 +314,43 @@ describe('syncUtil', () => {
     })
   })
 
-  describe('ipcSafeObject()', () => {
+  describe('createSiteSettingsData', function () {
+  })
+
+  describe('deepArrayify', function () {
     it('does nothing with objects already safe', () => {
       const object = {chill: true, time: 42, nest: {egg: 'tree'}}
-      assert.deepEqual(syncUtil.ipcSafeObject(object), object)
+      assert.deepEqual(syncUtil.deepArrayify(object), object)
     })
 
     it('does nothing with Arrays', () => {
       const object = {arr: [1, 2, 3]}
-      assert.deepEqual(syncUtil.ipcSafeObject(object), object)
+      assert.deepEqual(syncUtil.deepArrayify(object), object)
       const deepObject = {deep: {arr: [1, 2, 3]}}
-      assert.deepEqual(syncUtil.ipcSafeObject(deepObject), deepObject)
+      assert.deepEqual(syncUtil.deepArrayify(deepObject), deepObject)
     })
 
     it('converts Uint8Array to Array', () => {
       const object = {chill: true, arr: new Uint8Array([1, 2, 3])}
       const expected = {chill: true, arr: [1, 2, 3]}
-      assert.deepEqual(syncUtil.ipcSafeObject(object), expected)
+      assert.deepEqual(syncUtil.deepArrayify(object), expected)
       const deepObject = {chill: true, deep: {arr: new Uint8Array([1, 2, 3])}}
       const deepExpected = {chill: true, deep: {arr: [1, 2, 3]}}
-      assert.deepEqual(syncUtil.ipcSafeObject(deepObject), deepExpected)
+      assert.deepEqual(syncUtil.deepArrayify(deepObject), deepExpected)
     })
   })
 
-  describe('getSiteDataFromRecord()', () => {
-    it('update bookmark parent folder to null -> overrides existing parent folder', () => {
-      const objectId = [96, 46, 213, 0, 13, 111, 180, 184, 65, 66, 173, 27, 207, 29, 32, 108]
-      const records = [{
-        action: 1,
-        bookmark: {
-          isFolder: true,
-          parentFolderObjectId: null,
-          site: {
-            creationTime: 0,
-            customTitle: '',
-            favicon: '',
-            lastAccessedTime: 0,
-            location: '',
-            title: 'Folder1'
-          }
-        },
-        deviceId: [1],
-        objectData: 'bookmark',
-        objectId,
-        syncTimestamp: 1499736988267
-      }]
-      const existingObject = {
-        lastAccessedTime: 0,
-        tags: ['bookmark-folder'],
-        objectId,
-        order: 9,
-        folderId: 2,
-        customTitle: 'Folder1', // XXX: Android uses title whereas laptop uses customTitle
-        parentFolderId: 1
-      }
-      const appState = {
-        sites: {
-          '2': existingObject
-        },
-        sync: {
-          objectsById: {
-            [objectId.join('|')]: ['sites', '2']
-          }
-        }
-      }
-      const result = syncUtil.getSiteDataFromRecord(records[0],
-        Immutable.fromJS(appState), Immutable.fromJS(records))
-      assert.equal(result.tag, 'bookmark-folder')
-      assert.deepEqual(result.siteDetail.toJS(),
-        {
-          objectId,
-          title: 'Folder1',
-          favicon: '',
-          location: '',
-          parentFolderId: 0,
-          folderId: 2,
-          tags: ['bookmark-folder'],
-          lastAccessedTime: 0,
-          creationTime: 0
-        }
-      )
-      assert.deepEqual(result.existingObjectData.toJS(), existingObject)
+  describe('ipcSafeObject', function () {
+    let deepArrayifySpy
+    before(function () {
+      deepArrayifySpy = sinon.spy(syncUtil, 'deepArrayify')
+    })
+    after(function () {
+      deepArrayifySpy.restore()
+    })
+    it('calls deepArrayify', function () {
+      syncUtil.deepArrayify({chill: true, time: 42, nest: {egg: 'tree'}})
+      assert.equal(deepArrayifySpy.calledOnce, true)
     })
   })
 })
