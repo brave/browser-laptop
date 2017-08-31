32ef086a26ba064fc9eac0563038d73b62830f88
Brian R. Bondy <netzen@gmail.com>
2017-08-31 19:08:26 -0400

    Fix change of having last tab detached destroyed
    Auditors: @bridiver
    
    Fix #10436
    
    This is covered by a test already but the bug is a very intermittent race
    condition.

--- app/browser/tabs.js
+++ app/browser/tabs.js
@@ -491,7 +491,7 @@ const api = {
         // webcontents is still the embedder.
         const tabValue = getTabValue(tabId)
         const windowId = tabValue.get('windowId')
-        tab.once('will-attach', () => {
+        tab.once('did-attach', () => {
           appActions.tabStripEmpty(windowId)
         })
       })
