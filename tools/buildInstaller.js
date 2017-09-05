cb05e3a3d1e23af6911fd900c49e8132b141f364
Brian Clifton <brian@clifton.me>
2017-09-05 15:29:50 -0700

    Revert "Temp work-around so that we can build 0.19.x."
    This reverts commit 0d08051856a9bc7abeb5481acc5b9023cc54759c.

--- tools/buildInstaller.js
+++ tools/buildInstaller.js
@@ -51,18 +51,18 @@ if (isDarwin) {
     raiseError('IDENTIFIER needs to be set to the certificate organization')
   }
 
-  // const wvBundle = buildDir + '/Brave.app/Contents/Frameworks/Brave Framework.framework/Brave Framework'
-  // const wvBundleSig = buildDir + '/Brave.app/Contents/Frameworks/Widevine Resources.bundle/Contents/Resources/Brave Framework.sig'
-  // const wvPlugin = buildDir + '/Brave.app/Contents/Frameworks/Brave Framework.framework/Libraries/WidevineCdm/_platform_specific/mac_x64/widevinecdmadapter.plugin'
+  const wvBundle = buildDir + '/Brave.app/Contents/Frameworks/Brave Framework.framework/Brave Framework'
+  const wvBundleSig = buildDir + '/Brave.app/Contents/Frameworks/Widevine Resources.bundle/Contents/Resources/Brave Framework.sig'
+  const wvPlugin = buildDir + '/Brave.app/Contents/Frameworks/Brave Framework.framework/Libraries/WidevineCdm/_platform_specific/mac_x64/widevinecdmadapter.plugin'
   cmds = [
     // Remove old
     'rm -f ' + outDir + '/Brave.dmg',
 
     // sign for widevine
-    // 'codesign --deep --force --strict --verbose --sign $IDENTIFIER "' + wvBundle + '"',
-    // 'codesign --deep --force --strict --verbose --sign $IDENTIFIER "' + wvPlugin + '"',
-    // 'python tools/signature_generator.py --input_file "' + wvBundle + '" --output_file "' + wvBundleSig + '" --flag 1',
-    // 'python tools/signature_generator.py --input_file "' + wvPlugin + '"',
+    'codesign --deep --force --strict --verbose --sign $IDENTIFIER "' + wvBundle + '"',
+    'codesign --deep --force --strict --verbose --sign $IDENTIFIER "' + wvPlugin + '"',
+    'python tools/signature_generator.py --input_file "' + wvBundle + '" --output_file "' + wvBundleSig + '" --flag 1',
+    'python tools/signature_generator.py --input_file "' + wvPlugin + '"',
 
     // Sign it
     'cd ' + buildDir + '/Brave.app/Contents/Frameworks',
