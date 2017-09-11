22b9283f0421724ffd7f63e317dd0bcdc6d6d86a
Cezar Augusto <cezaraugusto@outlook.it>
2017-09-11 06:19:37 -0300

    add new state helpers for the private tab Auditors: @nejczdovc Test plan: npm run test -- --grep="privateState unit tests"

--- app/common/state/tabContentState/privateState.js
+++ app/common/state/tabContentState/privateState.js
@@ -2,4 +2,15 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
- // TODO
+// State
+const frameStateUtil = require('../../../../js/state/frameStateUtil')
+
+module.exports.isPrivateTab = (state, frameKey) => {
+  const frame = frameStateUtil.getFrameByKey(state, frameKey)
+
+  if (frame == null) {
+    return false
+  }
+
+  return !!frame.get('isPrivate')
+}
