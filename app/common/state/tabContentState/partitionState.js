83818cfb95a02ccdad2d5dcb32c3cedffe8661ff
Cezar Augusto <cezaraugusto@outlook.it>
2017-09-11 06:26:42 -0300

    add new state helpers for the partition tab (new session) Auditors: @nejczdovc Test plan: npm run test -- --grep="partitionState unit tests"

--- app/common/state/tabContentState/partitionState.js
+++ app/common/state/tabContentState/partitionState.js
@@ -2,4 +2,47 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
- // TODO
+// State helpers
+const frameStateUtil = require('../../../../js/state/frameStateUtil')
+
+// Constants
+const {tabs} = require('../../../../js/constants/config')
+
+module.exports.isPartitionTab = (state, frameKey) => {
+  const frame = frameStateUtil.getFrameByKey(state, frameKey)
+
+  if (frame == null) {
+    return false
+  }
+
+  return !!frame.get('partitionNumber')
+}
+
+module.exports.getPartitionNumber = (state, frameKey) => {
+  const frame = frameStateUtil.getFrameByKey(state, frameKey)
+
+  if (frame == null) {
+    return 0
+  }
+
+  const partitionNumber = frame.get('partitionNumber')
+  if (typeof partitionNumber === 'string') {
+    return partitionNumber.replace(/^partition-/i, '')
+  }
+  return partitionNumber
+}
+
+module.exports.getMaxAllowedPartitionNumber = (state, frameKey) => {
+  const frame = frameStateUtil.getFrameByKey(state, frameKey)
+
+  if (frame == null) {
+    return 0
+  }
+
+  const partitionNumber = module.exports.getPartitionNumber(state, frameKey)
+
+  if (partitionNumber > tabs.maxAllowedNewSessions) {
+    return tabs.maxAllowedNewSessions
+  }
+  return partitionNumber
+}
