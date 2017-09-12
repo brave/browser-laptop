048b1217c1890fed902580c00313b5eb0a4cc02f
Brian R. Bondy <netzen@gmail.com>
2017-09-12 18:15:45 -0400

    Fix dispatch issues for actions pre windowReady
    Fix #10866
    
    Changes in this PR:
    - Browser process now registers the process right away for the
      dispatcher..
    - Callbacks are registered from the window store.
    - Waits until the window is ready before dispatching, queues up messages
      sent before this.
    - No extensions.createTab will be issued before a window is ready.

--- app/browser/windows.js
+++ app/browser/windows.js
@@ -320,6 +320,7 @@ const api = {
         win.__alreadyPinnedSites = new Immutable.Set()
         updatePinnedTabs(win)
         win.__ready = true
+        win.emit(messages.WINDOW_RENDERER_READY)
       }
     })
   },
--- app/common/actions/extensionActions.js
+++ app/common/actions/extensionActions.js
@@ -3,7 +3,7 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 'use strict'
-const AppDispatcher = require('../../../js/dispatcher/appDispatcher')
+const appDispatcher = require('../../../js/dispatcher/appDispatcher')
 const ExtensionConstants = require('../constants/extensionConstants')
 
 const extensionActions = {
@@ -14,7 +14,7 @@ const extensionActions = {
    * @param {object} browserAction - browser action details
    */
   browserActionRegistered: function (extensionId, browserAction) {
-    AppDispatcher.dispatch({
+    appDispatcher.dispatch({
       actionType: ExtensionConstants.BROWSER_ACTION_REGISTERED,
       extensionId,
       browserAction
@@ -22,7 +22,7 @@ const extensionActions = {
   },
 
   browserActionUpdated: function (extensionId, browserAction, tabId) {
-    AppDispatcher.dispatch({
+    appDispatcher.dispatch({
       actionType: ExtensionConstants.BROWSER_ACTION_UPDATED,
       extensionId,
       browserAction,
@@ -36,7 +36,7 @@ const extensionActions = {
    * @param {string} extensionId - the extension id
    */
   extensionInstalled: function (extensionId, installInfo) {
-    AppDispatcher.dispatch({
+    appDispatcher.dispatch({
       actionType: ExtensionConstants.EXTENSION_INSTALLED,
       extensionId,
       installInfo
@@ -49,7 +49,7 @@ const extensionActions = {
    * @param {string} extensionId - the extension id
    */
   extensionUninstalled: function (extensionId) {
-    AppDispatcher.dispatch({
+    appDispatcher.dispatch({
       actionType: ExtensionConstants.EXTENSION_UNINSTALLED,
       extensionId
     })
@@ -61,7 +61,7 @@ const extensionActions = {
    * @param {string} extensionId - the extension id
    */
   extensionEnabled: function (extensionId) {
-    AppDispatcher.dispatch({
+    appDispatcher.dispatch({
       actionType: ExtensionConstants.EXTENSION_ENABLED,
       extensionId
     })
@@ -73,7 +73,7 @@ const extensionActions = {
    * @param {string} extensionId - the extension id
    */
   extensionDisabled: function (extensionId) {
-    AppDispatcher.dispatch({
+    appDispatcher.dispatch({
       actionType: ExtensionConstants.EXTENSION_DISABLED,
       extensionId
     })
@@ -88,7 +88,7 @@ const extensionActions = {
    * @param {string} icon - 16x16 extension icon
    */
   contextMenuCreated: function (extensionId, menuItemId, properties, icon) {
-    AppDispatcher.dispatch({
+    appDispatcher.dispatch({
       actionType: ExtensionConstants.CONTEXT_MENU_CREATED,
       extensionId,
       menuItemId,
@@ -103,7 +103,7 @@ const extensionActions = {
    * @param {string} extensionId - the extension id
    */
   contextMenuAllRemoved: function (extensionId) {
-    AppDispatcher.dispatch({
+    appDispatcher.dispatch({
       actionType: ExtensionConstants.CONTEXT_MENU_ALL_REMOVED,
       extensionId
     })
@@ -117,7 +117,7 @@ const extensionActions = {
    * @param {object} info - the arg of onclick callback
    */
   contextMenuClicked: function (extensionId, tabId, info) {
-    AppDispatcher.dispatch({
+    appDispatcher.dispatch({
       actionType: ExtensionConstants.CONTEXT_MENU_CLICKED,
       extensionId,
       tabId,
