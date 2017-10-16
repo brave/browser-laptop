3113e670ef8f8aa2ab2e366761b0c1c0cb07459c
Brian R. Bondy <netzen@gmail.com>
2017-10-16 16:49:44 -0400

    Only run dapp detection if metamask is not installed
    Fix #11569
    
    Auditors: @aekeus

--- app/extensions/brave/content/scripts/dappListener.js
+++ app/extensions/brave/content/scripts/dappListener.js
@@ -2,45 +2,44 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const script =
-  `
-  function __insertWeb3Installed() {
-    if (!window.alreadyInserted) {
-      const meta = document.createElement('meta')
-      meta.name = 'web3-installed'
-      document.head.appendChild(meta)
-      window.alreadyInserted = true
-    }
-  }
-  if (window.web3) {
-    if (!window.web3.currentProvider || !window.web3.currentProvider.isMetaMask) {
-      __insertWeb3Installed()
-    }
-  } else {
-    var oldWeb3 = window.web3
-    Object.defineProperty(window, 'web3', {
-      configurable: true,
-      set: function (val) {
-        __insertWeb3Installed()
-        oldWeb3 = val
-      },
-      get: function () {
-        __insertWeb3Installed()
-        return oldWeb3
-      }
-    })
-  }`
-
 if (chrome.contentSettings.dappDetection == 'allow') {
-  executeScript(script)
-}
+  const script =
+    `
+    function __insertWeb3Installed() {
+      if (!window.alreadyInserted) {
+        const meta = document.createElement('meta')
+        meta.name = 'web3-installed'
+        document.head.appendChild(meta)
+        window.alreadyInserted = true
+      }
+    }
+    if (window.web3) {
+      if (!window.web3.currentProvider || !window.web3.currentProvider.isMetaMask) {
+        __insertWeb3Installed()
+      }
+    } else {
+      var oldWeb3 = window.web3
+      Object.defineProperty(window, 'web3', {
+        configurable: true,
+        set: function (val) {
+          __insertWeb3Installed()
+          oldWeb3 = val
+        },
+        get: function () {
+          __insertWeb3Installed()
+          return oldWeb3
+        }
+      })
+    }`
 
-setTimeout(function () {
-  const isDapp = document.querySelector('meta[name="web3-installed"]')
-  if (isDapp) {
-    chrome.ipcRenderer.send('dispatch-action', JSON.stringify([{
-      actionType: 'app-dapp-available',
-      location: window.location.href
-    }]))
-  }
-}, 3000)
+  executeScript(script)
+  setTimeout(function () {
+    const isDapp = document.querySelector('meta[name="web3-installed"]')
+    if (isDapp) {
+      chrome.ipcRenderer.send('dispatch-action', JSON.stringify([{
+        actionType: 'app-dapp-available',
+        location: window.location.href
+      }]))
+    }
+  }, 3000)
+}
