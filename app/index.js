ab29856c9e7fb8ce1d884d3cf3b4d5a18270a4c9
Henrique Ferreiro <hferreiro@igalia.com>
2017-11-16 10:27:37 +0100

    Remove 'overridable' from the 'certificate-error' event
    This parameter was removed from
    ContentBrowserClient::AllowCertificateError in
    https://crrev.com/9cc660cd8f17

--- app/index.js
+++ app/index.js
@@ -135,7 +135,7 @@ const notifyCertError = (webContents, url, error, cert) => {
 }
 
 app.on('ready', () => {
-  app.on('certificate-error', (e, webContents, url, error, cert, resourceType, overridable, strictEnforcement, expiredPreviousDecision, muonCb) => {
+  app.on('certificate-error', (e, webContents, url, error, cert, resourceType, strictEnforcement, expiredPreviousDecision, muonCb) => {
     let host = urlParse(url).host
     if (host && acceptCertDomains[host] === true) {
       // Ignore the cert error
