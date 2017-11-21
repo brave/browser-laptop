brave/browser-laptop

commit 685380896390719b5800ce3d84a6677df5143e8a

Improve SortableTable
- Replace headerClassNames with headerStyles
- Replace columnClassNames with columnStyles on ledgerTable
- Replace bodyClassNames with bodyStyles
- Remove customCellClassesStr
- Remove .th-inner
- Set white-space: nowrap to ledgerTable.js

Auditors:

Test Plan:
1. Run `npm run add-simulated-synopsis-visits 100`
2. Open about:preferences#payments
3. Pin some entries
4. Make sure the table is properly displayed

app/renderer/components/preferences/payment/ledgerTable.js
@@ -112,15 +112,15 @@ class LedgerTable extends ImmutableComponent {
     }
   }
 
-  get columnClassNames () {
+  get columnStyles () {
     return [
-      css(styles.column_verified), // verified
-      css(styles.column_sites), // sites
-      css(styles.alignCenter),  // include
-      css(styles.alignRight), // views
-      css(styles.alignRight), // time spent
-      css(styles.alignRight, styles.column_percentage), // percentage
-      css(styles.alignCenter)   // actions
+      styles.column_verified,
+      styles.column_sites,
+      null,  // include
+      [styles.column_nowrap, styles.alignRight], // views
+      [styles.column_nowrap, styles.alignRight], // time spent
+      [styles.alignRight, styles.column_percentage],
+      styles.alignCenter // actions
     ]
   }
 
@@ -184,12 +184,14 @@ class LedgerTable extends ImmutableComponent {
             small
             disabled
             checkedOn
+            switchClassName={css(styles.switchControl_center)}
             indicatorClassName={css(styles.pinnedToggle)}
             testId='pinnedDisabled'
             onClick={() => {}}
           />
           : <SiteSettingCheckbox
             small
+            switchClassName={css(styles.switchControl_center)}
             hostPattern={this.getHostPattern(synopsis)}
             defaultValue={defaultAutoInclude}
             prefKey='ledgerPayments'
@@ -204,7 +206,7 @@ class LedgerTable extends ImmutableComponent {
         value: duration
       },
       {
-        html: <span className={css(styles.column_percentage__percentageValue)} data-test-id='percentageValue'>
+        html: <span data-test-id='percentageValue'>
           {
             pinned
             ? <PinnedInput
@@ -291,10 +293,10 @@ class LedgerTable extends ImmutableComponent {
         headings={['', 'publisher', 'include', 'views', 'timeSpent', 'percentage', 'actions']}
         defaultHeading='percentage'
         defaultHeadingSortOrder='desc'
-        headerClassNames={css(styles.header)}
-        columnClassNames={this.columnClassNames}
+        headerStyles={styles.header}
+        columnStyles={this.columnStyles}
         rowClassNames={this.rowClassNames(pinnedRows, unPinnedRows)}
-        bodyClassNames={[css(unPinnedRows.size > 0 && styles.pinnedBody), '']}
+        bodyStyles={[unPinnedRows.size > 0 && styles.pinnedBody, '']}
         onContextMenu={aboutActions.contextMenu}
         contextMenuName='synopsis'
         rowObjects={[
@@ -369,9 +371,12 @@ const styles = StyleSheet.create({
   },
 
   column_verified: {
-    // Set the same padding with the padding-top and padding-bottom
-    paddingRight: `calc(${globalStyles.sortableTable.cell.small.padding} / 2) !important`,
-    paddingLeft: `calc(${globalStyles.sortableTable.cell.small.padding} / 2) !important`
+    paddingRight: 0,
+    paddingLeft: 0
+  },
+
+  column_nowrap: {
+    whiteSpace: 'nowrap'
   },
 
   column_sites: {
@@ -380,12 +385,9 @@ const styles = StyleSheet.create({
   },
 
   column_percentage: {
-    minWidth: '3ch'
-  },
-
-  column_percentage__percentageValue: {
-    // To cancel the global color setting
-    color: '#656565'
+    minWidth: '3ch',
+    paddingRight: `calc(${globalStyles.sortableTable.cell.small.padding} + .2rem)`,
+    paddingLeft: `calc(${globalStyles.sortableTable.cell.small.padding} + .2rem)`
   },
 
   pinnedBody: {
@@ -414,6 +416,10 @@ const styles = StyleSheet.create({
     padding: '0 6px'
   },
 
+  switchControl_center: {
+    justifyContent: 'center'
+  },
+
   hideExcludedSites: {
     display: 'grid',
     alignItems: 'center',
@@ -435,8 +441,7 @@ const styles = StyleSheet.create({
   },
 
   alignCenter: {
-    display: 'flex',
-    justifyContent: 'center'
+    textAlign: 'center'
   },
 
   actionIcons: {

app/renderer/about/bookmarks/bookmarkTitleHeader.js
@@ -23,7 +23,7 @@ class BookmarkTitleHeader extends ImmutableComponent {
     windowActions.addBookmark(newBookmark)
   }
   render () {
-    return <div className='th-inner'>
+    return <div>
       <span data-l10n-id={this.props.heading} />
       <span className='addBookmark'
         data-l10n-id='addBookmark'

app/renderer/components/common/sortableTable.js
@@ -254,10 +254,6 @@ class SortableTable extends React.Component {
     return this.props.columnClassNames &&
       this.props.columnClassNames.length === this.props.headings.length
   }
-  get hasBodyClassNames () {
-    return this.props.bodyClassNames &&
-      this.props.bodyClassNames.length === this.props.rows.length
-  }
   get hasContextMenu () {
     return typeof this.props.onContextMenu === 'function' &&
       typeof this.props.contextMenuName === 'string'
@@ -389,12 +385,10 @@ class SortableTable extends React.Component {
         const cell = typeof item === 'object' ? item.cell : item
 
         let hasColumnClassNames = (this.hasColumnClassNames ? this.props.columnClassNames[j] : '')
-        let customCellClassesStr = (this.props.customCellClasses ? this.props.customCellClasses : '')
 
         return <td className={cx({
           [hasColumnClassNames]: true,
-          [customCellClassesStr]: true,
-          [css(styles.table__tbody__tr__td, this.props.smallRow && styles.table__tbody__tr_smallRow__td)]: true
+          [css(styles.table__tbody__tr__td, this.props.smallRow && styles.table__tbody__tr_smallRow__td, this.props.columnStyles && this.props.columnStyles[j])]: true
         })}
           data-sort={value}
           data-td-index={`${j}`}
@@ -442,7 +436,7 @@ class SortableTable extends React.Component {
       return this.props.rows.map((rows, i) => {
         const content = this.generateTableRows(rows, i)
         return (content.length > 0)
-          ? <tbody className={this.hasBodyClassNames ? this.props.bodyClassNames[i] : undefined} data-tbody-index={`${i}`}>
+          ? <tbody className={css(this.props.bodyStyles && this.props.bodyStyles[i])} data-tbody-index={`${i}`}>
             {content}
           </tbody>
         : null
@@ -487,6 +481,7 @@ class SortableTable extends React.Component {
             const isString = typeof heading === 'string'
             const sortMethod = this.sortingDisabled ? 'none' : (dataType === 'number' ? 'number' : undefined)
             if (isString) headerClasses['heading-' + heading] = true
+
             return <th className={cx(headerClasses)}
               data-sort-method={sortMethod}
               data-sort-order={this.props.defaultHeadingSortOrder}
@@ -497,11 +492,11 @@ class SortableTable extends React.Component {
             >
               {
                 isString
-                  ? <div className={cx({
-                    'th-inner': true,
-                    [css(styles.table__th__inner, this.props.smallRow && styles.table__th__inner_smallRow)]: true,
-                    [this.props.headerClassNames]: !!this.props.headerClassNames
-                  })} data-l10n-id={heading} />
+                  ? <div className={css(
+                    styles.table__th__inner,
+                    this.props.smallRow && styles.table__th__inner_smallRow,
+                    this.props.headerStyles
+                  )} data-l10n-id={heading} />
                   : heading
               }
             </th>
@@ -585,12 +580,18 @@ const styles = StyleSheet.create({
 
   table__tbody__tr__td: {
     boxSizing: 'border-box',
-    padding: `calc(${globalStyles.sortableTable.cell.normal.padding} / 2.25) ${globalStyles.sortableTable.cell.normal.padding}`
+    paddingTop: `calc(${globalStyles.sortableTable.cell.normal.padding} / 2.25)`,
+    paddingBottom: `calc(${globalStyles.sortableTable.cell.normal.padding} / 2.25)`,
+    paddingRight: `${globalStyles.sortableTable.cell.normal.padding}`,
+    paddingLeft: `${globalStyles.sortableTable.cell.normal.padding}`
   },
 
   // Add 'smallRow' to SortableTable to decrease padding values.
   table__tbody__tr_smallRow__td: {
-    padding: `calc(${globalStyles.sortableTable.cell.small.padding} / 2.25) ${globalStyles.sortableTable.cell.small.padding}`,
+    paddingTop: `calc(${globalStyles.sortableTable.cell.small.padding} / 2.25)`,
+    paddingBottom: `calc(${globalStyles.sortableTable.cell.small.padding} / 2.25)`,
+    paddingRight: `${globalStyles.sortableTable.cell.small.padding}`,
+    paddingLeft: `${globalStyles.sortableTable.cell.small.padding}`,
     height: '1.5rem'
   }
 })

app/renderer/components/preferences/payment/pinnedInput.js
@@ -62,9 +62,6 @@ const styles = StyleSheet.create({
     // include border width
     width: 'calc(5ch + 2px)',
 
-    // To cancel the global color setting
-    color: '#656565',
-
     ':focus': {
       background: '#fff',
       borderColor: globalStyles.color.highlightBlue

less/sortableTable.less
@@ -25,7 +25,6 @@ table.sortableTable {
     }
 
     td {
-      color: @mediumGray;
       -webkit-font-smoothing: antialiased;
       position: relative;
       width: auto;

commit c245c917e2e38f95cdb1ac945085afcadda786ae

Fix switch placement of pinned site on ledgerTable
This is a follow-up to #10789

Regression caused by https://github.com/brave/browser-laptop/commit/685380896390719b5800ce3d84a6677df5143e8a#diff-e168dd994d1e4de3f340af83899a20f5R187

Auditors: @cezaraugusto

Test Plan:
1. Run `npm run add-simulated-synopsis-visits 100`
2. Pin a site on about:preferences#payments
3. Make sure the switch appears at the center of the column

app/renderer/components/preferences/payment/ledgerTable.js
@@ -184,7 +184,7 @@ class LedgerTable extends ImmutableComponent {
             small
             disabled
             checkedOn
-            switchClassName={css(styles.switchControl_center)}
+            customWrapperClassName={css(styles.switchControl_center)}
             indicatorClassName={css(styles.pinnedToggle)}
             testId='pinnedDisabled'
             onClick={() => {}}