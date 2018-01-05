85d43229e15e7da7a2772121953ecf0a73b81e27
Brian Clifton <brian@clifton.me>
2018-01-05 10:17:04 -0700

    Merge pull request #12518 from brave/fix/siteisolation-timing
    fix site isolation not working in packaged builds

--- app/index.js
+++ app/index.js
@@ -104,6 +104,11 @@ let loadAppStatePromise = SessionStore.loadAppState()
 loadAppStatePromise.then((initialImmutableState) => {
   const {HARDWARE_ACCELERATION_ENABLED, SMOOTH_SCROLL_ENABLED, SEND_CRASH_REPORTS, SITE_ISOLATION_ENABLED} = require('../js/constants/settings')
   const initialSettings = initialImmutableState.get('settings')
+  if (getSetting(SITE_ISOLATION_ENABLED, initialSettings) === true) {
+    // This needs to happen as early as possible :O
+    app.commandLine.appendSwitch('site-per-process')
+  }
+
   CrashHerald.init(getSetting(SEND_CRASH_REPORTS, initialSettings))
 
   telemetry.setCheckpointAndReport('state-loaded')
@@ -113,10 +118,6 @@ loadAppStatePromise.then((initialImmutableState) => {
 
   if (getSetting(SMOOTH_SCROLL_ENABLED, initialSettings) === false) {
     app.commandLine.appendSwitch('disable-smooth-scrolling')
-  }
-
-  if (getSetting(SITE_ISOLATION_ENABLED, initialSettings) === true) {
-    app.commandLine.appendSwitch('site-per-process')
   }
 })
 
