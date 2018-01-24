a7a2c0578b4555a77227be0243b300038f813092
Brian Clifton <brian@clifton.me>
2018-01-24 09:08:45 -0700

    Merge pull request #12814 from brave/issue-12813
    handle null origins

--- app/filtering.js
+++ app/filtering.js
@@ -452,6 +452,10 @@ function registerPermissionHandler (session, partition) {
       tempSettings = siteSettings.getSiteSettingsForURL(appState.get('temporarySiteSettings'), origin)
     }
 
+    if (origin == null) {
+      muonCb(false)
+    }
+
     let response = []
     for (let i = 0; i < permissionTypes.length; i++) {
       const responseSizeThisIteration = response.length
