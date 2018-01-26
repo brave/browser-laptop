822cbc429235dba6ef27bc9d8b716ff877a8e712
Brian Clifton <brian@clifton.me>
2018-01-25 22:07:17 -0700

    Merge pull request #12855 from brave/issue-12813-follow-up-up
    don't create with full permission length

--- app/filtering.js
+++ app/filtering.js
@@ -453,9 +453,10 @@ function registerPermissionHandler (session, partition) {
       tempSettings = siteSettings.getSiteSettingsForURL(appState.get('temporarySiteSettings'), origin)
     }
 
-    let response = new Array(permissionTypes.length)
+    let response = []
 
     if (origin == null) {
+      response = new Array(permissionTypes.length)
       response.fill(false, 0, permissionTypes.length)
       muonCb(response)
       return
