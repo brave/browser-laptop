2874650befce8d4641d1592201ddae191eeb36e2
NejcZdovc <nejc.zdovc@3zsistemi.si>
2018-02-13 08:13:34 +0100

    Adds native notifications

--- js/stores/appStore.js
+++ js/stores/appStore.js
@@ -235,6 +235,7 @@ const handleAppAction = (action) => {
       require('../../app/browser/reducers/siteSettingsReducer'),
       require('../../app/browser/reducers/pageDataReducer'),
       require('../../app/browser/reducers/userModelReducer'),
+      require('../../app/browser/reducers/nativeNotificationReducer'),
       ledgerReducer,
       require('../../app/browser/menu')
     ]
--- js/stores/windowStore.js
+++ js/stores/windowStore.js
@@ -28,6 +28,7 @@ const bookmarkFoldersState = require('../../app/common/state/bookmarkFoldersStat
 const bookmarksState = require('../../app/common/state/bookmarksState')
 const tabState = require('../../app/common/state/tabState')
 const bookmarkUtil = require('../../app/common/lib/bookmarkUtil')
+const notificationUtil = require('../../app/renderer/lib/notificationUtil')
 
 let windowState = Immutable.fromJS({
   activeFrameKey: null,
@@ -811,6 +812,11 @@ const doAction = (action) => {
       const sourceFrameTabId = sourceFrame.get('tabId')
       appActions.tabIndexChangeRequested(sourceFrameTabId, destinationFrameIndex)
       break
+    case windowConstants.WINDOW_ON_NATIVE_NOTIFICATION_OPEN:
+      {
+        notificationUtil.createNotification(action.title, action.options)
+        break
+      }
     default:
       break
   }
