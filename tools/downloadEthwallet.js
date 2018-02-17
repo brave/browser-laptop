9e09bb3b3d9def326319fb7b26dfc0319b64507b
eV <ev@7pr.xyz>
2018-02-17 07:18:28 +0000

    Add tooling for upgrading geth, bundling

--- tools/downloadEthwallet.js
+++ tools/downloadEthwallet.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const fs = require('fs')
 const path = require('path')
 const {removeSync, ensureDirSync, copySync} = require('fs-extra')
 
@@ -9,3 +10,14 @@ removeSync(targetDirectory)
 ensureDirSync(targetDirectory)
 require('meteor-dapp-wallet-prebuilt').init(targetDirectory)
 copySync(path.resolve('img/ethereum/'), targetDirectory)
+
+const Manager = require('ethereum-client-binaries').Manager
+const clientBinaries = fs.readFileSync('gethBinaries.json')
+const config = JSON.parse(clientBinaries)
+const mgr = new Manager(config)
+mgr.init()
+.then(() => {
+  mgr.download('Geth', {
+    downloadFolder: './app/'
+  })
+})
