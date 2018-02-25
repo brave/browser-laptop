5652166309998d2ae955bec4574d11dff2fc63de
Brian Clifton <brian@clifton.me>
2018-02-25 07:28:36 +0000

    Merge pull request #13292 from NejcZdovc/hotfix/#12666-fines
    Fixes low amounts display

--- app/common/lib/ledgerUtil.js
+++ app/common/lib/ledgerUtil.js
@@ -76,7 +76,7 @@ const formatCurrentBalance = (ledgerData) => {
   balance = balance.toFixed(2)
 
   if (converted > 0 && converted < 0.01) {
-    converted = '<.01'
+    converted = '< 0.01'
   } else {
     converted = converted.toFixed(2)
   }
