8e1176606e384c017ae4e451290f9efccc70f188
Andrey Okonetchnikov <andrey@okonet.ru>
2018-03-15 12:43:28 +0100

    Implement time spent table

--- app/renderer/about/history/historyTimeSpent.js
+++ app/renderer/about/history/historyTimeSpent.js
@@ -5,8 +5,8 @@
 // Note that these are webpack requires, not CommonJS node requiring requires
 const React = require('react')
 const Immutable = require('immutable')
+const {subMilliseconds, distanceInWordsStrict} = require('date-fns')
 const messages = require('../../../../js/constants/messages')
-// const SortableTable = require('../../app/renderer/components/common/sortableTable')
 
 const ipc = window.chrome.ipcRenderer
 
@@ -17,18 +17,48 @@ class HistoryTimeSpent extends React.Component {
       ledgerData: Immutable.Map()
     }
     ipc.on(messages.LEDGER_UPDATED, (e, ledgerData) => {
-      console.log(ledgerData) // Logs nothing
-      this.setState({ ledgerData: Immutable.fromJS(ledgerData) })
+      console.log(ledgerData)
+      this.setState({ledgerData: Immutable.fromJS(ledgerData)})
     })
-    ipc.on(messages.HISTORY_UPDATED, (e, history) => {
-      console.log(history) // Logs nothing
-    })
+  }
+
+  renderRow = (row, index) => {
+    const {duration, percentage, publisherKey, publisherURL, views} = row.toJS()
+    return <tr key={publisherKey}>
+      <td>{index + 1}</td>
+      <td>{publisherURL}</td>
+      <td>{views}</td>
+      <td>{distanceInWordsStrict(subMilliseconds(new Date(), duration), new Date())}</td>
+      <td>{percentage}</td>
+    </tr>
   }
 
   render () {
     const {ledgerData} = this.state
-    console.log(ledgerData.toJS()) // is always empty
-    return <h1>{ledgerData.size}</h1>
+    const synopsis = ledgerData.get('synopsis')
+    if (!synopsis || synopsis.size === 0) {
+      return <h2>No data available</h2>
+    }
+    const sortedPublishers = synopsis.sortBy(publisher => publisher.get('score') * -1)
+    console.log(sortedPublishers.toJS()) // is always empty
+
+    return <section>
+      <h1>Time spent</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Rank</th>
+            <th>Site</th>
+            <th>Views</th>
+            <th>Time spent</th>
+            <th>%</th>
+          </tr>
+        </thead>
+        <tbody>
+          {sortedPublishers.map(this.renderRow)}
+        </tbody>
+      </table>
+    </section>
   }
 }
 
