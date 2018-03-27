2b507c7680a82c761a6af2fb4da6aa998cd93948
yan <yan@mit.edu>
2018-03-27 15:35:43 -0700

    Workaround browser-context-created not being emitted for tor tabs

--- app/browser/tabs.js
+++ app/browser/tabs.js
@@ -29,7 +29,7 @@ const {newTabMode} = require('../common/constants/settingsEnums')
 const {tabCloseAction} = require('../common/constants/settingsEnums')
 const webContentsCache = require('./webContentsCache')
 const {FilterOptions} = require('ad-block')
-const {isResourceEnabled} = require('../filtering')
+const {isResourceEnabled, initPartition} = require('../filtering')
 const autofill = require('../autofill')
 const bookmarksState = require('../common/state/bookmarksState')
 const bookmarkFoldersState = require('../common/state/bookmarkFoldersState')
@@ -1049,6 +1049,11 @@ const api = {
         }
         extensions.createTab(createProperties, (tab) => {
           cb && cb(tab)
+          // XXX: Workaround for 'browser-context-created' not emitted for Tor
+          // browsing context
+          if (createProperties.isTor) {
+            initPartition(appConfig.tor.partition)
+          }
         })
       }
 
