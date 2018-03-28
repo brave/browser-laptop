cf9ee27b72b2750baacb9b1365576cf29e3dc16d
Brian Clifton <brian@clifton.me>
2018-03-28 10:20:42 -0700

    Merge pull request #13638 from bsclifton/revert-hsts
    Revert "Merge pull request #13437 from brave/hsts-fingerprinting"

--- test/unit/app/sessionStoreTest.js
+++ test/unit/app/sessionStoreTest.js
@@ -12,9 +12,7 @@ const compareVersions = require('compare-versions')
 require('../braveUnit')
 
 describe('sessionStore unit tests', function () {
-  let filtering
   let sessionStore
-
   let shutdownClearHistory = false
   let shutdownClearAutocompleteData = false
   let shutdownClearAutofillData = false
@@ -40,7 +38,6 @@ describe('sessionStore unit tests', function () {
     }
   }
   const fakeFiltering = {
-    clearHSTSData: () => {},
     clearStorageData: () => {},
     clearCache: () => {},
     clearHistory: () => {}
@@ -93,7 +90,6 @@ describe('sessionStore unit tests', function () {
       }
     })
     mockery.registerMock('./filtering', fakeFiltering)
-    filtering = require('./filtering')
     sessionStore = require('../../../app/sessionStore')
   })
 
@@ -788,7 +784,6 @@ describe('sessionStore unit tests', function () {
     let localeInitSpy
     let backupSessionStub
     let runImportDefaultSettings
-    let clearHSTSDataSpy
 
     before(function () {
       runPreMigrationsSpy = sinon.spy(sessionStore, 'runPreMigrations')
@@ -798,7 +793,6 @@ describe('sessionStore unit tests', function () {
       localeInitSpy = sinon.spy(fakeLocale, 'init')
       backupSessionStub = sinon.stub(sessionStore, 'backupSession')
       runImportDefaultSettings = sinon.spy(sessionStore, 'runImportDefaultSettings')
-      clearHSTSDataSpy = sinon.spy(filtering, 'clearHSTSData')
     })
 
     after(function () {
@@ -808,27 +802,6 @@ describe('sessionStore unit tests', function () {
       runPostMigrationsSpy.restore()
       localeInitSpy.restore()
       backupSessionStub.restore()
-      clearHSTSDataSpy.restore()
-    })
-
-    describe('check clearHSTSData invocations', function () {
-      describe('if lastAppVersion is 0.23', function () {
-        it('clearHSTSData is not invoked', function () {
-          let exampleState = sessionStore.defaultAppState()
-          exampleState.lastAppVersion = '0.23'
-          sessionStore.runPreMigrations(exampleState)
-          assert.equal(clearHSTSDataSpy.notCalled, true)
-        })
-      })
-
-      describe('if lastAppVersion is 0.21', function () {
-        it('clearHSTSData is calledOnce', function () {
-          let exampleState = sessionStore.defaultAppState()
-          exampleState.lastAppVersion = '0.21'
-          sessionStore.runPreMigrations(exampleState)
-          assert.equal(clearHSTSDataSpy.calledOnce, true)
-        })
-      })
     })
 
     describe('when reading the session file', function () {
