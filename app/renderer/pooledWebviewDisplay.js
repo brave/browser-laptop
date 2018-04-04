737d9dcd8914c8e347a0ada6188365e08fb215a0
petemill <miller.pete+commit@gmail.com>
2018-04-03 17:32:57 -0700

    Correct variable names for WebviewDisplay now that we're passing tabId not guestInstanceId

--- app/renderer/pooledWebviewDisplay.js
+++ app/renderer/pooledWebviewDisplay.js
@@ -94,7 +94,7 @@ module.exports = class WebviewDisplay {
       // do not steal focus if is new tab page, or if we're about to
       // switch away
       const isNewTabPage = getBaseUrl(e.url) === getTargetAboutUrl('about:newtab')
-      const isSwitchingAway = this.attachingToGuestInstanceId && this.attachingToGuestInstanceId !== this.activeGuestInstanceId
+      const isSwitchingAway = this.attachingToTabId && this.attachingToTabId !== this.activeTabId
       if (!isNewTabPage && !isSwitchingAway) {
         webview.focus()
       }
@@ -126,7 +126,7 @@ module.exports = class WebviewDisplay {
       this.debugLog('webview tab-detached-at')
       webview.detachGuest()
     })
-    const debugEvents = ['tab-replaced-at', 'tab-id-changed', 'will-attach', 'did-attach', 'did-detach', 'will-detach', 'console-message']
+    const debugEvents = ['tab-replaced-at', 'tab-id-changed', 'will-attach', 'did-attach', 'did-detach', 'will-detach']
     for (const event of debugEvents) {
       webview.addEventListener(event, () => {
         this.debugLog(`<webview> event ${event}`, webview)
@@ -138,22 +138,22 @@ module.exports = class WebviewDisplay {
   attachActiveTab (tabId) {
     this.debugLog(`attachActiveTab`, tabId)
     if (tabId == null) {
-      throw new Error('guestInstanceId is not valid')
+      throw new Error('tabId is not valid')
     }
     // do nothing if repeat call to same guest Id as attached or attaching
     if (
-      (!this.attachingToGuestInstanceId && tabId === this.activeGuestInstanceId) ||
-      tabId === this.attachingToGuestInstanceId
+      (!this.attachingToTabId && tabId === this.activeTabId) ||
+      tabId === this.attachingToTabId
     ) {
-      this.debugLog('already attaching this guest, nothing to do.')
+      this.debugLog('already attaching this tab, nothing to do.')
       return
     }
     // are we waiting to attach to something different already?
-    if (this.attachingToGuestInstanceId != null) {
+    if (this.attachingToTabId != null) {
       // wait for that attach to finish, and queue this one up to then display
       // if we have something already in the queue, then remove it
-      this.debugLog('attach already in progress, queuing replacement guest')
-      this.attachingToGuestInstanceId = tabId
+      this.debugLog('attach already in progress, queuing replacement tab')
+      this.attachingToTabId = tabId
       return
     }
     // fresh attach
@@ -166,7 +166,7 @@ module.exports = class WebviewDisplay {
     }
     this.debugLog(`swapWebviewOnAttach: Using webview #${toAttachWebview.dataset.webviewReplaceCount}`)
 
-    this.attachingToGuestInstanceId = tabId
+    this.attachingToTabId = tabId
     const t0 = window.performance.now()
     let timeoutHandleBumpView = null
     const fnEnd = () => {
@@ -192,23 +192,23 @@ module.exports = class WebviewDisplay {
     const showAttachedView = async () => {
       // if we have decided to show a different guest in the time it's taken to attach and show
       // then do not show the intermediate, instead detach it and wait for the next attach
-      if (tabId !== this.attachingToGuestInstanceId) {
-        this.debugLog('swapWebviewOnAttach: detaching guest from just attached view because it was not the desired guest anymore')
+      if (tabId !== this.attachingToTabId) {
+        this.debugLog('swapWebviewOnAttach: detaching tab from just-attached view because it was not the desired tab anymore')
         remote.unregisterEvents(tabId, tabEventHandler)
         await toAttachWebview.detachGuest()
         // if it happens to be the webview which is already being shown
-        if (this.attachingToGuestInstanceId === this.activeGuestInstanceId) {
+        if (this.attachingToTabId === this.activeTabId) {
           // release everything and do not continue
           this.webviewPool.push(toAttachWebview)
-          this.attachingToGuestInstanceId = null
+          this.attachingToTabId = null
           this.debugLog('swapWebviewOnAttach: Asked to show already-attached view, so leaving that alone and returning new attached to pool')
           fnEnd()
           return
         }
         // start again, but with different guest
-        this.debugLog('swapWebviewOnAttach: Asked to show a different guest than the one we just attached, continuing with that one')
+        this.debugLog('swapWebviewOnAttach: Asked to show a different tab than the one we just attached, continuing with that one')
         fnEnd()
-        this.swapWebviewOnAttach(this.attachingToGuestInstanceId, toAttachWebview, lastAttachedWebview)
+        this.swapWebviewOnAttach(this.attachingToTabId, toAttachWebview, lastAttachedWebview)
         return
       }
       this.debugLog(`swapWebviewOnAttach: webview waiting for paint ${window.performance.now() - t0}ms`)
@@ -239,16 +239,16 @@ module.exports = class WebviewDisplay {
       }
       // check again if there's a pending view
       // since we may have done something async
-      const pendingGuestInstanceId = this.attachingToGuestInstanceId
+      const pendingTabId = this.attachingToTabId
       // reset state
-      this.activeGuestInstanceId = tabId
+      this.activeTabId = tabId
       this.attachedWebview = toAttachWebview
-      this.attachingToGuestInstanceId = null
+      this.attachingToTabId = null
       // perform next attach if there's one waiting
-      if (pendingGuestInstanceId !== tabId) {
+      if (pendingTabId !== tabId) {
         this.debugLog('swapWebviewOnAttach - another attach pending, continuing with that attach request')
         fnEnd()
-        this.swapWebviewOnAttach(pendingGuestInstanceId, this.getPooledWebview(), this.attachedWebview)
+        this.swapWebviewOnAttach(pendingTabId, this.getPooledWebview(), this.attachedWebview)
       } else {
         if (this.shouldFocusOnAttach) {
           this.shouldFocusOnAttach = false
@@ -262,25 +262,25 @@ module.exports = class WebviewDisplay {
     const onDestroyedInsteadOfAttached = () => {
       this.debugLog('swapWebviewOnAttach: destroyed instead of attached...')
       // continue with next attach request if we've had one in the meantime
-      if (this.attachingToGuestInstanceId !== tabId) {
+      if (this.attachingToTabId !== tabId) {
         // if it happens to be the webview which is already being shown
-        if (this.attachingToGuestInstanceId === this.activeGuestInstanceId) {
+        if (this.attachingToTabId === this.activeTabId) {
           // release everything and do not continue
           this.webviewPool.push(toAttachWebview)
-          this.attachingToGuestInstanceId = null
+          this.attachingToTabId = null
           this.debugLog('swapWebviewOnAttach: Asked to show already-attached view, so leaving that alone and returning new attached to pool')
           fnEnd()
           return
         }
-        // start again, but with different guest
-        this.debugLog('swapWebviewOnAttach: Asked to show a different guest than the one we just attached, continuing with that one')
+        // start again, but with different tab
+        this.debugLog('swapWebviewOnAttach: Asked to show a different tab than the one we just attached, continuing with that one')
         fnEnd()
-        this.swapWebviewOnAttach(this.attachingToGuestInstanceId, toAttachWebview, lastAttachedWebview)
+        this.swapWebviewOnAttach(this.attachingToTabId, toAttachWebview, lastAttachedWebview)
         return
       }
       // reset state and do not continue
       this.webviewPool.push(toAttachWebview)
-      this.attachingToGuestInstanceId = null
+      this.attachingToTabId = null
       fnEnd()
     }
 
@@ -333,7 +333,7 @@ module.exports = class WebviewDisplay {
       this.debugLog('swapWebviewOnAttach: attaching active guest instance ', guestInstanceId, 'to webview', toAttachWebview)
       // setImmediate is a bit of a hacky way to ensure that registerEvents handler is registered
       setImmediate(() => toAttachWebview.attachGuest(guestInstanceId, webContents))
-      this.debugLog('swapWebviewOnAttach: Waiting....')
+      this.debugLog(`swapWebviewOnAttach: Waiting.... ${window.performance.now() - t0}ms`)
       // another workaround for not getting did-attach on webview, set a timeout and then hide / show view
       timeoutHandleBumpView = window.setTimeout(ensurePaintWebviewFirstAttach.bind(null, toAttachWebview), 2000)
     })
@@ -344,9 +344,9 @@ module.exports = class WebviewDisplay {
   }
 
   focusActiveWebview () {
-    if (!this.attachingToGuestInstanceId && this.attachedWebview) {
+    if (!this.attachingToTabId && this.attachedWebview) {
       this.attachedWebview.focus()
-    } else if (this.attachingToGuestInstanceId) {
+    } else if (this.attachingToTabId) {
       this.shouldFocusOnAttach = true
     }
   }
