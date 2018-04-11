cd75af0f2b6d8fedb1eee4a9ea4c33c608d114d5
Brian Clifton <brian@clifton.me>
2018-04-11 16:04:18 -0700

    Merge pull request #13807 from brave/fix/13805
    Move webrtc options from advanced to security prefs

--- js/about/preferences.js
+++ js/about/preferences.js
@@ -35,6 +35,7 @@ const appConfig = require('../constants/appConfig')
 const preferenceTabs = require('../constants/preferenceTabs')
 const messages = require('../constants/messages')
 const settings = require('../constants/settings')
+const webrtcConstants = require('../constants/webrtcConstants')
 const {changeSetting} = require('../../app/renderer/lib/settingsUtil')
 const {passwordManagers, extensionIds} = require('../constants/passwordManagers')
 const {startsWithOption, newTabMode, bookmarksToolbarMode, fullscreenOption, autoplayOption} = require('../../app/common/constants/settingsEnums')
@@ -430,6 +431,30 @@ class SecurityTab extends ImmutableComponent {
       <SettingsList>
         <SettingCheckbox dataL10nId='useSiteIsolation' prefKey={settings.SITE_ISOLATION_ENABLED} settings={this.props.settings} onChangeSetting={this.props.onChangeSetting} />
       </SettingsList>
+      <DefaultSectionTitle data-l10n-id='webrtcPolicy' />
+      <SettingsList>
+        <SettingDropdown
+          value={(
+            getSetting(settings.WEBRTC_POLICY, this.props.settings)
+          )}
+          onChange={changeSetting.bind(
+            null,
+            this.props.onChangeSetting,
+            settings.WEBRTC_POLICY
+          )}>
+          {
+            Object.keys(webrtcConstants)
+              .map((policy) => <option data-l10n-id={policy} value={webrtcConstants[policy]} />)
+          }
+        </SettingDropdown>
+        <div
+          className={css(styles.link)}
+          data-l10n-id='webrtcPolicyExplanation'
+          onClick={aboutActions.createTabRequested.bind(null, {
+            url: 'https://cs.chromium.org/chromium/src/content/public/common/webrtc_ip_handling_policy.h'
+          })}
+        />
+      </SettingsList>
       <SitePermissionsPage siteSettings={this.props.siteSettings} names={permissionNames} />
       <div data-l10n-id='requiresRestart' className={css(commonStyles.requiresRestart)} />
     </div>
@@ -754,6 +779,12 @@ const styles = StyleSheet.create({
   sortableTable_searchTab: {
     width: '704px',
     marginBottom: globalStyles.spacing.settingsListContainerMargin // See syncTab.js for use cases
+  },
+  link: {
+    cursor: 'pointer',
+    fontSize: '14px',
+    lineHeight: '3em',
+    textDecoration: 'underline'
   }
 })
 
