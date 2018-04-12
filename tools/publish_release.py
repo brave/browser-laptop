e92b743735ad04264390387b70b0f5428bf8bd20
Ryan Gerstenkorn <ryan_gerstenkorn@fastmail.fm>
2018-04-12 10:13:40 -0500

    Add MPL2 licenses and add tool tests to travis

--- tools/publish_release.py
+++ tools/publish_release.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
-# for more info see https://developer.github.com/v3/repos/releases/
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import json
 import os
+import sys
+
 from lib.github import GitHub
 from lib.helpers import *
 import requests
