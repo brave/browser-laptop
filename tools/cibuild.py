a6490ac12c5486589c2f5967051d646a0b1c209c
Brian Clifton <brian@clifton.me>
2018-04-11 23:51:28 -0700

    missed a spot

--- tools/cibuild.py
+++ tools/cibuild.py
@@ -5,7 +5,7 @@ import subprocess
 import sys
 import os.path
 MUON_VERSION = '5.2.0'
-CHROMEDRIVER_VERSION = '2.35'
+CHROMEDRIVER_VERSION = '2.36'
 SOURCE_ROOT = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 TARGET_ARCH= os.environ['TARGET_ARCH'] if os.environ.has_key('TARGET_ARCH') else 'x64'
 os.environ['npm_config_arch'] = TARGET_ARCH
