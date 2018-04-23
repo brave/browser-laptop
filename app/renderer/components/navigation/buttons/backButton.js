ae3520b5c5601a49ef787d94d9195b005ea7dd23
pgodbout <pgodbout@bitbucket.org>
2018-04-22 21:47:13 -0400

    WORK IN PROGRESS (WIP), bugfix7645 for Brave Browser

--- app/renderer/components/navigation/buttons/backButton.js
+++ app/renderer/components/navigation/buttons/backButton.js
@@ -56,7 +56,8 @@ class BackButton extends React.Component {
     const rect = target.parentNode.getBoundingClientRect()
     appActions.onGoBackLong(this.props.activeTabId, {
       left: rect.left,
-      bottom: rect.bottom
+      bottom: rect.bottom,
+      type: false
     })
   }
 
