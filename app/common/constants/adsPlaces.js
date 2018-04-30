c02d72e22b0c4f16327bdd6d9668c389cc042b21
NejcZdovc <nejc.zdovc@3zsistemi.si>
2018-04-30 19:36:20 +0200

    Adds dropdown for the place

---
+++ app/common/constants/adsPlaces.js
@@ -0,0 +1,17 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+const adsPlaces = {
+  UNDISCLOSED: 'Undisclosed',
+  HOME: 'Home',
+  WORK: 'Work',
+  TRANSIT: 'Transit',
+  OTHER: 'Other',
+  SHELL_BEACH: 'Shell Beach',
+  METROPOLIS: 'Metropolis',
+  ISS: 'ISS',
+  MIR: 'Mir'
+}
+
+module.exports = adsPlaces
