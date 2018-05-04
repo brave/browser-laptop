31e46806d038f0ee75fb0d104f0acee79b293421
Nejc Zdovc <NejcZdovc@users.noreply.github.com>
2018-05-04 18:33:29 +0200

    Merge pull request #14022 from NejcZdovc/fix/#14018-contribution
    Fixes contribution process

--- app/renderer/components/preferences/payment/enabledContent.js
+++ app/renderer/components/preferences/payment/enabledContent.js
@@ -377,7 +377,7 @@ class EnabledContent extends ImmutableComponent {
 
   render () {
     const ledgerData = this.props.ledgerData
-    const walletStatusText = walletStatus(ledgerData)
+    const walletStatusText = walletStatus(ledgerData, this.props.settings)
     const contributionAmount = ledgerState.getContributionAmount(null, ledgerData.get('contributionAmount'), this.props.settings)
     const amountList = ledgerData.get('monthlyAmounts') || ledgerUtil.defaultMonthlyAmounts
 
