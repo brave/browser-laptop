c317749534894529115ec26f71c3f4fbab573f97
NejcZdovc <nejc.zdovc@3zsistemi.si>
2018-05-04 19:05:26 +0200

    Adds clear ads data

--- js/constants/settings.js
+++ js/constants/settings.js
@@ -50,6 +50,7 @@ const settings = {
   SHUTDOWN_CLEAR_AUTOFILL_DATA: 'shutdown.clear-autofill-data',
   SHUTDOWN_CLEAR_SITE_SETTINGS: 'shutdown.clear-site-settings',
   SHUTDOWN_CLEAR_PUBLISHERS: 'shutdown.clear-publishers',
+  SHUTDOWN_CLEAR_ADS: 'shutdown.clear-ads',
   FLASH_INSTALLED: 'security.flash.installed',
   FULLSCREEN_CONTENT: 'security.fullscreen.content',
   AUTOPLAY_MEDIA: 'security.autoplay.media',
