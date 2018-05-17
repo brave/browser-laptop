0820b3d410ad3b41a7cf8ae7aa6bdf618ce65f62
yan <yan@mit.edu>
2018-05-16 20:19:17 -0700

    Open link in new private tab should use tor if enabled
    fix https://github.com/brave/browser-laptop/issues/14103
    
    Test Plan:
    1. make sure tor tabs is enabled
    2. go to a link and right-click 'open in new private tab'
    3. in the new private tab, go to check.torproject.org to make sure Tor
       is enabled
    4. right click on the onion image, select 'open image in new tab'
    5. in the new tab, go to check.torproect.org to make sure Tor is enabled
    
    auditors: @darkdh

--- app/browser/tabs.js
+++ app/browser/tabs.js
@@ -521,6 +521,7 @@ const api = {
       if (ses) {
         isPrivate = ses.isOffTheRecord()
       }
+      const isTor = isPrivate && settingsStore.getSetting(settings.USE_TOR_PRIVATE_TABS)
 
       const frameOpts = {
         location,
@@ -531,6 +532,7 @@ const api = {
         guestInstanceId: newTab.guestInstanceId,
         isPinned: !!newTabValue.get('pinned'),
         isPrivate,
+        isTor,
         openerTabId,
         disposition,
         index,
