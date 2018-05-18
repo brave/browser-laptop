bce4a402a8d4a7cc08584f01be9467df92b4721d
Taylor `Riastradh' Campbell <taylor@brave.com>
2018-05-18 02:45:45 +0000

    Copying notice on torTest.js too.

--- test/unit/app/torTest.js
+++ test/unit/app/torTest.js
@@ -1,4 +1,8 @@
-/* global describe, before, after, it */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/* global after, afterEach, before, beforeEach, describe, it */
 
 const assert = require('assert')
 const child_process = require('child_process') // eslint-disable-line camelcase
