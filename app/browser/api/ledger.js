f109a6f325f97eac768f9db1a2147a74dbf0b4f6
ryanml <rlanese@asu.edu>
2018-05-22 15:23:47 -0700

    Exporting callback

--- app/browser/api/ledger.js
+++ app/browser/api/ledger.js
@@ -3358,7 +3358,8 @@ const getMethods = () => {
     setNewTimeUntilReconcileCallback,
     fetchReferralHeadersCallback,
     getPaymentInfo,
-    fetchReferralHeaders
+    fetchReferralHeaders,
+    callback
   }
 
   let privateMethods = {}
