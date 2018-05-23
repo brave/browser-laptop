308be5b33447c95d62afdc7b91972a2c36377982
ryanml <rlanese@asu.edu>
2018-05-22 20:58:18 -0700

    Immutable safetey check, callback export

--- app/browser/api/ledger.js
+++ app/browser/api/ledger.js
@@ -1194,6 +1194,7 @@ const onWalletRecovery = (state, error, result) => {
     state = logError(state, error.toString(), 'recoveryWallet')
     state = aboutPreferencesState.setRecoveryStatus(state, false)
   } else {
+    result = makeImmutable(result)
     // convert buffer to Uint8Array
     let seed = result && result.getIn(['properties', 'wallet', 'keyinfo', 'seed'])
     if (seed) {
@@ -1210,7 +1211,7 @@ const onWalletRecovery = (state, error, result) => {
       state = ledgerState.setAboutProp(state, 'status', '')
     }
 
-    callback(error, result)
+    module.exports.callback(error, result)
 
     if (balanceTimeoutId) {
       clearTimeout(balanceTimeoutId)
