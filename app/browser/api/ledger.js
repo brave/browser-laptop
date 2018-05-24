aaaa16a46bb0724d5f80d9cd5c353605a88325f2
Jason Sadler <jsadler@brave.com>
2018-05-24 17:50:39 -0400

    returning state on 'run' for ledger-spec

--- app/browser/api/ledger.js
+++ app/browser/api/ledger.js
@@ -2586,6 +2586,7 @@ const run = (state, delayTime) => {
   if (client.isReadyToReconcile(synopsis, onFuzzing)) {
     client.reconcile(uuid.v4().toLowerCase(), callback)
   }
+  return state
 }
 
 const networkConnected = () => {
