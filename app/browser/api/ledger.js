8e9e06c836eea8e7d0c8c255190937948bba5f5f
Jason Sadler <jsadler@brave.com>
2018-05-30 16:30:58 -0400

    Mock out onFuzzing

--- app/browser/api/ledger.js
+++ app/browser/api/ledger.js
@@ -2482,7 +2482,7 @@ const onInitRead = (state, parsedData) => {
     // enables it again -> reconcileStamp is in the past.
     // In this case reset reconcileStamp to the future.
     try {
-      timeUntilReconcile = client.timeUntilReconcile(synopsis, onFuzzing)
+      timeUntilReconcile = client.timeUntilReconcile(synopsis, module.exports.onFuzzing)
     } catch (ex) {}
 
     let ledgerWindow = (ledgerState.getSynopsisOption(state, 'numFrames') - 1) * ledgerState.getSynopsisOption(state, 'frameSize')
@@ -2600,7 +2600,7 @@ const run = (state, delayTime) => {
   }
 
   const publishers = ledgerState.getAboutProp(state, 'synopsis') || Immutable.List()
-  if (isList(publishers) && publishers.isEmpty() && client.isReadyToReconcile(synopsis, onFuzzing)) {
+  if (isList(publishers) && publishers.isEmpty() && client.isReadyToReconcile(synopsis, module.exports.onFuzzing)) {
     setNewTimeUntilReconcile()
   }
 
@@ -2661,7 +2661,7 @@ const run = (state, delayTime) => {
 
   if (delayTime === 0) {
     try {
-      delayTime = client.timeUntilReconcile(synopsis, onFuzzing)
+      delayTime = client.timeUntilReconcile(synopsis, module.exports.onFuzzing)
     } catch (ex) {
       delayTime = false
     }
@@ -2695,7 +2695,7 @@ const run = (state, delayTime) => {
     return
   }
 
-  if (client.isReadyToReconcile(synopsis, onFuzzing)) {
+  if (client.isReadyToReconcile(synopsis, module.exports.onFuzzing)) {
     client.reconcile(uuid.v4().toLowerCase(), callback)
   }
   return state
@@ -3385,7 +3385,11 @@ const getMethods = () => {
     fetchReferralHeaders,
     callback,
     onLedgerQRGeneratedCallback,
-    qrWriteImage
+    qrWriteImage,
+    onFuzzing,
+    getClient: () => {
+      return client
+    }
   }
 
   let privateMethods = {}
