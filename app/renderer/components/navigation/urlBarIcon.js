d316c1f1db36b9f0d384f43c7fdfacb2d8924345
Ross Moody <fiftyfivehis@gmail.com>
2018-05-30 11:37:10 -0700

    Icon SVG updates and very small padding tweaks (#14224)
    - Updated navigationBar icon sizing and svg code
    - Updated Browser window icons (bookmarks, history, settings)
    - Preferences page icon and styling tweaks

--- app/renderer/components/navigation/urlBarIcon.js
+++ app/renderer/components/navigation/urlBarIcon.js
@@ -157,7 +157,7 @@ class UrlBarIcon extends React.Component {
   }
 }
 
-const searchIconSize = 16
+const searchIconSize = 14
 const iconSize = 20
 const styles = StyleSheet.create({
   urlBarIcon: {
