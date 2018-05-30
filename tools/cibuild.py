5ba40bd4576fae3c54eda636a035aa3a569f0573
Brian Clifton <brian@clifton.me>
2018-05-29 23:38:03 -0700

    Update to Muon 7.0.1
    Auditors: @bridiver

--- tools/cibuild.py
+++ tools/cibuild.py
@@ -4,7 +4,7 @@ import os
 import subprocess
 import sys
 import os.path
-MUON_VERSION = '7.0.0'
+MUON_VERSION = '7.0.1'
 CHROMEDRIVER_VERSION = '2.37'
 SOURCE_ROOT = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 TARGET_ARCH= os.environ['TARGET_ARCH'] if os.environ.has_key('TARGET_ARCH') else 'x64'
