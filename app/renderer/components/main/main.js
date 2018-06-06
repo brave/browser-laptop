31f1d055d41d9209e95e232a16650956054aeaab
yan <yan@mit.edu>
2018-06-06 15:29:17 -0700

    Add tor loading progress UI
    Fix https://github.com/brave/browser-laptop/issues/14043
    
    Test Plan:
    
    Success case:
    1. start brave
    2. immediately open a tor private tab
    3. you should briefly see a loading message in the URLbar
    4. once loading is finished, you should be able to type in the URLbar
    
    Fail case:
    1. start an HTTP listener on port 9250 (so that Tor cannot bind to it.
       this causes Tor to not be able to connect).
    2. start brave
    3. open tor tab and wait for 20s
    4. you should see an error message pop up
    5. the button and link in the error message should work as expected

--- app/renderer/components/main/main.js
+++ app/renderer/components/main/main.js
@@ -539,6 +539,8 @@ class Main extends React.Component {
     const widevinePanelDetail = currentWindow.get('widevinePanelDetail', Immutable.Map())
     const loginRequiredDetails = basicAuthState.getLoginRequiredDetail(state, activeTabId)
     const focused = isFocused(state)
+    const isTor = frameStateUtil.isTor(activeFrame)
+    const torConnectionError = state.getIn(['tor', 'initializationError'])
 
     const props = {}
     // used in renderer
@@ -553,8 +555,9 @@ class Main extends React.Component {
     props.captionButtonsVisible = isWindows
     props.showContextMenu = currentWindow.has('contextMenuDetail')
     props.showPopupWindow = currentWindow.has('popupWindowDetail')
-    props.showSiteInfo = currentWindow.getIn(['ui', 'siteInfo', 'isVisible']) &&
-      !isSourceAboutUrl(activeFrame.get('location'))
+    props.showSiteInfo = (currentWindow.getIn(['ui', 'siteInfo', 'isVisible']) &&
+      !isSourceAboutUrl(activeFrame.get('location'))) ||
+      (torConnectionError && isTor)
     props.showBravery = shieldState.braveShieldsEnabled(activeFrame) &&
       !!currentWindow.get('braveryPanelDetail')
     props.showClearData = currentWindow.getIn(['ui', 'isClearBrowsingDataPanelVisible'], false)
