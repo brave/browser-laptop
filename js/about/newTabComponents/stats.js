605a01b63b230e70a468ae39dc33a0fbea7f7003
多多.王 <kingdom1028@163.com>
2018-06-07 20:52:41 +0800

    更新浏览器的显示名称

--- js/about/newTabComponents/stats.js
+++ js/about/newTabComponents/stats.js
@@ -1,6 +1,9 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * You can obtain one at http://mozilla.org/MPL/2.0/. 
+ *   
+ * New Tab
+ * */
 
 const React = require('react')
 const ImmutableComponent = require('../../../app/renderer/components/immutableComponent')
