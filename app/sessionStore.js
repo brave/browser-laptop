b3724870d280eafb6e575047600052b49ece822d
Pranjal <pranjal@brave.com>
2018-06-07 15:03:10 -0700

    Avoid crash if the quarantine attribute is not present

--- app/sessionStore.js
+++ app/sessionStore.js
@@ -851,7 +851,7 @@ module.exports.runPreMigrations = (data) => {
     if (process.platform === 'darwin' && (compareVersions(data.lastAppVersion, '0.22.13') === 0 || data.quarantineNeeded)) {
       const unQuarantine = (appPath) => {
         try {
-          execSync(`xattr -d com.apple.quarantine "${appPath}"`)
+          execSync(`xattr -d com.apple.quarantine "${appPath}" || true`)
           console.log(`Quarantine attribute has been removed from ${appPath}`)
         } catch (e) {
           console.error(`Failed to remove quarantine attribute from ${appPath}: `, e)
