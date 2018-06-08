2eab7a5768e7e2ce62efa59b2c793f18a5d1b9c3
NejcZdovc <nejc.zdovc@3zsistemi.si>
2018-06-08 08:10:15 +0200

    Adds captcha block overlay
    Resolves #14356
    
    Auditors:
    
    Test Plan:

--- app/locale.js
+++ app/locale.js
@@ -289,6 +289,8 @@ var rendererIdentifiers = function () {
     'promotionClaimedErrorMessage',
     'promotionClaimedErrorText',
     'promotionClaimedErrorTitle',
+    'promotionCaptchaBlockTitle',
+    'promotionCaptchaBlockMessage',
     'corruptedOverlayTitle',
     'corruptedOverlayMessage',
     'corruptedOverlayText',
