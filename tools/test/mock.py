e95b60f96661481624339b04bdae539205bf5d24
Ryan Gerstenkorn <ryan_gerstenkorn@fastmail.fm>
2018-06-14 21:19:03 -0500

    Avoid depending on requests module in python unittests
    Fixes #14430

--- tools/test/mock.py
+++ tools/test/mock.py
@@ -3,6 +3,10 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
+class MockImport(object):
+ def __import__(self):
+     pass
+
 class Repo():
   def __init__(self):
     self.releases = self.Releases()
