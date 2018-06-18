c863a816281a7de5f10776742dc1f2ef71301070
Marshall T. Rose <mrose17@gmail.com>
2018-06-18 11:54:13 -0700

    Look for expiration status
    `userModelState.getUserModelValue(state, 'status’)`

--- js/actions/appActions.js
+++ js/actions/appActions.js
@@ -2004,11 +2004,12 @@ const appActions = {
     })
   },
 
-  onUserModelUploadLogs: function (stamp, retryIn) {
+  onUserModelUploadLogs: function (stamp, retryIn, result) {
     dispatch({
       actionType: appConstants.APP_ON_USERMODEL_UPLOAD_LOGS,
       stamp,
-      retryIn
+      retryIn,
+      result
     })
   },
 
