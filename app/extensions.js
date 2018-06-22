291ffaf5ff3bd1ca6fc6c502f53ca2debb76895d
yan <yan@mit.edu>
2018-06-22 16:49:54 -0700

    fix tor-related webtorrent issues
    * re-enable webtorrent in private tabs
    * stop page loading indicator after torrent warning is shown in a tor
    tab
    
    fix https://github.com/brave/browser-laptop/issues/14472
    fix https://github.com/brave/browser-laptop/issues/14524
    
    Test Plan:
    1. open brave and make sure webtorrent is enabled
    2. go to https://webtorrent.io/torrents/sintel.torrent in a private tab.
       it should load webtorrent.
    3. go to https://webtorrent.io/torrents/sintel.torrent in a tor tab. it
       should show a privacy warning.
    4. click 'ok'. the page loading indicator should stop.

--- app/extensions.js
+++ app/extensions.js
@@ -289,7 +289,7 @@ let generateTorrentManifest = () => {
       48: 'img/webtorrent-48.png',
       16: 'img/webtorrent-16.png'
     },
-    incognito: 'not_allowed',
+    incognito: 'split',
     key: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAyWl+wMvL0wZX3JUs7GeZAvxMP+LWEh2bwMV1HyuBra/lGZIq3Fmh0+AFnvFPXz1NpQkbLS3QWyqhdIn/lepGwuc2ma0glPzzmieqwctUurMGSGManApGO1MkcbSPhb+R1mx8tMam5+wbme4WoW37PI3oATgOs2NvHYuP60qol3U7b/zB3IWuqtwtqKe2Q1xY17btvPuz148ygWWIHneedt0jwfr6Zp+CSLARB9Heq/jqGXV4dPSVZ5ebBHLQ452iZkHxS6fm4Z+IxjKdYs3HNj/s8xbfEZ2ydnArGdJ0lpSK9jkDGYyUBugq5Qp3FH6zV89WqBvoV1dqUmL9gxbHsQIDAQAB'
   }
 }
