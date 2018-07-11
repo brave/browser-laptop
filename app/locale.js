6efebe7a54dcfe6bed158d4cd20905797d614646
Taylor `Riastradh' Campbell <taylor@brave.com>
2018-07-11 18:04:58 +0000

    Block loading .onion sites in non-Tor tabs.
    fix #14431
    
    Auditors: @diracdeltas @bsclifton
    
    Test Plan:
    I:
    	1. Open a tab _without_ Tor (private or nonprivate).
    	2. Enter: https://nyttips4bmquxfzw.onion/
    	3. Confirm that Brave blocks loading the URL.
    
    II:
    	1. Open a tab _without_ Tor (private or nonprivate).
    	2. Enter: https://nyttips4bmquxfzw.onion:12345/
    	3. Confirm that Brave blocks loading the URL.
    
    III:
    	1. Open a private tab with Tor.
    	2. Enter: https://nyttips4bmquxfzw.onion/
    	3. Confirm that the NYT SecureDrop page loads.
    	4. Bookmark it.
    	5. Open a tab _without_ Tor (private or nonprivate).
    	6. Try to load the bookmark.
    	7. Confirm that Brave blocks loading the bookmark.

--- app/locale.js
+++ app/locale.js
@@ -270,6 +270,7 @@ var rendererIdentifiers = function () {
     'noDownloads',
     'torrentDesc',
     'urlBlockedInTor',
+    'urlBlockedOutsideTor',
     'urlWarningOk',
     'multiSelectionBookmarks',
     // Caption buttons in titlebar (min/max/close - Windows only)
