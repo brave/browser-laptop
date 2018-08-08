7aa0c2cb0a858eaa605505163261ca32e6e4ca1c
Brian Clifton <brian@clifton.me>
2018-08-07 23:56:07 -0700

    Merge pull request #14970 from brave/fix/media-device-access
    Add content setting for media device access

--- js/state/contentSettings.js
+++ js/state/contentSettings.js
@@ -90,6 +90,10 @@ const getDefaultUserPrefContentSettings = (braveryDefaults, appSettings, appConf
       setting: getSetting(settings.DO_NOT_TRACK, appSettings) ? 'allow' : 'block',
       primaryPattern: '*'
     }],
+    mediaPermission: [{
+      setting: 'block',
+      primaryPattern: '*'
+    }],
     passwordManager: getDefaultPasswordManagerSettings(braveryDefaults, appSettings, appConfig),
     javascript: [{
       setting: braveryDefaults.get('noScript') ? 'block' : 'allow',
@@ -280,10 +284,12 @@ const siteSettingsToContentSettings = (currentSiteSettings, defaultContentSettin
         }
       })
     }
-    if (typeof siteSetting.get('runInsecureContent') === 'boolean') {
-      contentSettings = addContentSettings(contentSettings, 'runInsecureContent', primaryPattern, '*',
-        siteSetting.get('runInsecureContent') ? 'allow' : 'block')
-    }
+    ['runInsecureContent', 'autoplay', 'mediaPermission'].forEach((permission) => {
+      if (typeof siteSetting.get(permission) === 'boolean') {
+        contentSettings = addContentSettings(contentSettings, permission, primaryPattern, '*',
+          siteSetting.get(permission) ? 'allow' : 'block')
+      }
+    })
     if (siteSetting.get('cookieControl')) {
       if (siteSetting.get('cookieControl') === 'block3rdPartyCookie') {
         contentSettings = addContentSettings(contentSettings, 'cookies', primaryPattern, '*', 'block')
@@ -322,9 +328,6 @@ const siteSettingsToContentSettings = (currentSiteSettings, defaultContentSettin
     }
     if (typeof siteSetting.get('widevine') === 'number' && braveryDefaults.get('widevine')) {
       contentSettings = addContentSettings(contentSettings, 'plugins', primaryPattern, '*', 'allow', appConfig.widevine.resourceId)
-    }
-    if (typeof siteSetting.get('autoplay') === 'boolean') {
-      contentSettings = addContentSettings(contentSettings, 'autoplay', primaryPattern, '*', siteSetting.get('autoplay') ? 'allow' : 'block')
     }
   })
   // On the second pass we consider only shieldsUp === false settings since we want those to take precedence.
